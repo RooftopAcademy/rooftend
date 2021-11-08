5440e3fd6b0db374cd839ace088ba42d8584136a
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-08 18:11:00 -0300

    Create new platform module

---
+++ src/platform/platform.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PlatformService } from './services/platform.service';
+import { PlatformController } from './controllers/platform.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Platform } from './entities/platform.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Platform])],
+  providers: [PlatformService],
+  controllers: [PlatformController],
+})
+export class PlatformModule {}
