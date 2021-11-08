53788fecf9eadd69f65495cc5bfc06f8b17afb41
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-08 17:29:36 -0300

    Create account Status modele file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountStatusModule } from './account-status/account-status.module';
 
 @Module({
-  imports: [],
+  imports: [AccountStatusModule],
   controllers: [AppController],
   providers: [AppService],
 })
