ea01a7930c7349d119811217e78880213d9cb454
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-08 17:37:19 -0300

    App module update to accept service controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlatformService } from './platform/services/platform.service';
+import { PlatformController } from './platform/controllers/platform.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PlatformController],
+  providers: [AppService, PlatformService],
 })
 export class AppModule {}
