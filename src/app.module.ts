a428b6643514eb8f70591625db63b88b19f466a8
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-08 17:28:56 -0300

    missing line in the app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhotosModule } from './photos/photos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
