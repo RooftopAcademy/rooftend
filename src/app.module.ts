fe84f08e54b95a0b41a2e3354dd04f582f21df83
agustintosco <agustintosco.92@gmail.com>
2021-11-08 22:34:31 +0100

    updated app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PaymentMethodsController } from './paymentMethod.controller';
+import { PaymentMethodsService } from './paymentMethod.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PaymentMethodsController],
+  providers: [AppService, PaymentMethodsService],
 })
 export class AppModule {}
