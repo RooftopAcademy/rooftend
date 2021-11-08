86e98ca7d96da081e51245a2e95fc671d60747bc
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-08 17:02:09 -0300

    Add import for TypeOrmModule and Favorites Service and Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [],
+  imports: [FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
