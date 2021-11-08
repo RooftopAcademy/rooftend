f7c116eef8d31fb1052f4ea833fde9fee0481fef
GSaucedo <g.saucedo.kd@gmail.com>
2021-11-08 14:41:36 -0600

    Deleted

--- src/telefonos/controllers/telefonos.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('telefonos')
-export class TelefonosController {}
--- src/telefonos/telefonos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TelefonosService } from './services/telefonos.service';
-import { TelefonosController } from './controllers/telefonos.controller';
-
-@Module({
-  providers: [TelefonosService],
-  controllers: [TelefonosController],
-})
-export class TelefonosModule {}
