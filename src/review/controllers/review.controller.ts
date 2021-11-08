76ea07c2e9fbf4e06c91f30c1702fe694e03aaca
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-08 17:19:41 -0300

    add status codes

--- src/review/controllers/review.controller.ts
+++ src/review/controllers/review.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
 import { ReviewService } from '../services/review.service';
 
 @Controller('review')
@@ -6,26 +15,31 @@ export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
   @Get()
+  @HttpCode(200)
   findAll() {
     return this.reviewService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(200)
   findOne(@Param('id') id: string) {
     return this.reviewService.findOne(id);
   }
 
   @Post()
+  @HttpCode(201)
   create() {
     return this.reviewService.create();
   }
 
   @Patch(':id')
+  @HttpCode(200)
   update(@Param('id') id: string) {
     return this.reviewService.update(id);
   }
 
   @Delete(':id')
+  @HttpCode(204)
   delete(@Param('id') id: string) {
     return this.reviewService.delete(id);
   }
