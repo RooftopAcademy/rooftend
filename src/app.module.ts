321358b73dae8f682f86fca4b367b468546033df
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-08 17:03:50 -0300

    import review in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [],
+  imports: [ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
