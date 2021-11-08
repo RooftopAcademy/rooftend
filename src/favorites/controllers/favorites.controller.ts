2b3c96d6b21f99d3ab1452bc2c273fdd1afa8ade
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-08 17:16:28 -0300

    Added http status code in favorites.controller.ts

--- src/favorites/controllers/favorites.controller.ts
+++ src/favorites/controllers/favorites.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Post,
   Put,
@@ -15,26 +16,31 @@ export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
   @Get()
+  @HttpCode(200)
   getAll() {
     return this.favoritesService.getAll();
   }
 
   @Get()
+  @HttpCode(200)
   getById(@Param('id') id: number) {
     return this.favoritesService.getById(id);
   }
 
   @Post()
+  @HttpCode(201)
   create(@Body() body: any) {
     return this.favoritesService.create(body);
   }
 
   @Put(':id')
+  @HttpCode(204)
   update(@Param('id') id: number, @Body() body: any) {
     return this.favoritesService.update(id, body);
   }
 
   @Delete(':id')
+  @HttpCode(200)
   delete(@Param('id') id: number) {
     return this.favoritesService.delete(id);
   }
