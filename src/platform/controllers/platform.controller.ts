a00d5cd1a066d893288dbd49d26e186e79a2e188
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-08 17:37:52 -0300

    Add httpcodes to platform controller

--- src/platform/controllers/platform.controller.ts
+++ src/platform/controllers/platform.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Patch,
   Post,
@@ -10,31 +11,36 @@ import {
 
 import { PlatformService } from '../services/platform.service';
 
-@Controller('platform')
+@Controller('platforms')
 export class PlatformController {
   constructor(private platformService: PlatformService) {}
 
   @Get()
+  @HttpCode(200)
   async findAll() {
     this.platformService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(200)
   async findOneById(@Param('id') id: string) {
     this.platformService.findOneById(id);
   }
 
   @Post()
+  @HttpCode(201)
   async create(@Body() body) {
     this.platformService.create(body);
   }
 
   @Patch(':id')
+  @HttpCode(200)
   async update(@Param('id') id: string, @Body() body) {
     this.platformService.update(id, body);
   }
 
   @Delete(':id')
+  @HttpCode(204)
   async remove(@Param('id') id: string) {
     this.platformService.remove(id);
   }
