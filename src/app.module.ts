54ed57aacbe7532dc00f970e740993e9a6ff60c8
ChachoPacho <bordonnet@hotmail.com>
2021-11-08 17:27:46 -0300

    ShippingMethods's Module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ShippingMethodsModule } from './shipping-methods/shipping-methods.module';
 
 @Module({
-  imports: [],
+  imports: [ShippingMethodsModule],
   controllers: [AppController],
   providers: [AppService],
 })
