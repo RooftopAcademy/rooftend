3ba2e4303ea9d0ddc5780aa0dad6e077b293168e
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-08 18:12:03 -0300

    Edit platform service to the repository access

--- src/platform/services/platform.service.ts
+++ src/platform/services/platform.service.ts
@@ -1,35 +1,43 @@
 import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Platform } from '../entities/platform.entity';
 
 @Injectable()
 export class PlatformService {
-  // constructor(@InjectRepository(Platform) private platformRepository: Repository<Platform>){
+  constructor(
+    @InjectRepository(Platform)
+    private platformRepository: Repository<Platform>,
+  ) {}
 
-  findAll(): string {
-    // this.platformRepository.find();
-    return 'This action returns all the platforms';
+  findAll(): Promise<Platform[]> {
+    return this.platformRepository.find();
   }
 
-  findOneById(id: string) {
-    // this.platformRepository.findOne(id);
-    return `This action returns the platform with id = ${id}`;
+  findOneById(id: number): Promise<Platform> {
+    return this.platformRepository.find({ id })[0];
   }
 
-  create(body: Body): string {
-    console.log(body);
-    // this.platformRepository.save(body);
-    return 'This action creates a new platform';
+  create(platform: Platform): boolean {
+    try {
+      this.platformRepository.save(platform);
+      return true;
+    } catch {
+      return false;
+    }
   }
 
-  update(id: string, body: Body): string {
-    console.log(body);
-    // this.platformRepository.findOneAndUpdate(id, body);
-    return `This action updates the platform with id = ${id}`;
+  update(id: number, platform: Platform): boolean {
+    try {
+      this.platformRepository.update(id, platform);
+      return true;
+    } catch {
+      return false;
+    }
   }
 
-  remove(id: string): string {
-    // this.platformRepository.findByIdAndDelete(id);
-    return `This action removes the platform with id = ${id}`;
+  async remove(id: number) {
+    const platform: Platform = await this.findOneById(id);
+    this.platformRepository.remove(platform);
   }
 }
