3e8a4c5189218d6d34ab15ca35f6b327dc101666
Ayrton <ayrtoncravero26@gmail.com>
2021-11-08 17:20:10 -0300

    all the logic was added for the controllers and the services

--- src/notification/controllers/notification.controller.ts
+++ src/notification/controllers/notification.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
+import { NotificationService } from '../services/notification.service';
 
-@Controller('notification')
-export class NotificationController {}
+@Controller('/notification')
+export class NotificationController {
+    constructor(private notificationServices: NotificationService) { };
+
+    @Get()
+    getAll() {
+        return this.notificationServices.findAll();
+    };
+
+    @Get(':id')
+    getOne(@Param('id') id: number) {
+        return this.notificationServices.findOne(id);
+    };
+
+    @Post()
+    create(@Body() body: any) {
+        return this.notificationServices.create(body);
+    };
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() body: any) {
+        return this.notificationServices.update(id, body);
+    };
+
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+        return this.notificationServices.delete(id);
+    };
+}
--- src/notification/services/notification.service.ts
+++ src/notification/services/notification.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+    constructor(@InjectRepository(Notification) private notificationRepo: Repository<Notification>) { };
+
+    findAll() {
+        return this.notificationRepo.find();
+    };
+
+    findOne(id: number) {
+        return this.notificationRepo.findOne(id);
+    };
+
+    create(body: any) {
+        const notification = this.notificationRepo.create(body);
+
+        return this.notificationRepo.save(notification);
+    };
+
+    async update(id: number, body: any) {
+        const notofication = await this.notificationRepo.findOne(id);
+
+        if(!notofication) {
+            throw new Error('Notification not found.');
+        };
+
+        this.notificationRepo.merge(notofication, body);
+
+        return this.notificationRepo.save(notofication);
+    };
+
+    async delete(id: number) {
+        await this.notificationRepo.delete(id);
+
+        return true;
+    };
+}
