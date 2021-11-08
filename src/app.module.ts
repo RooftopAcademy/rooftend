3889db8e3e69aa6e5dc75a42cfb9d6ef11a85de2
Ayrton <ayrtoncravero26@gmail.com>
2021-11-08 17:30:04 -0300

    deleted file with confit of database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationModule } from './notification/notification.module';
-import { config } from './orm.config';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     NotificationModule,
-    TypeOrmModule.forRoot(config),
   ],
   controllers: [AppController],
   providers: [AppService],
