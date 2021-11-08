4377db28e1c271fbad1692baea851a62f34106c3
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-08 17:37:39 -0300

    added some code statuses

--- src/photos/controllers/photos.controller.ts
+++ src/photos/controllers/photos.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Post,
   Delete,
+  HttpCode,
 } from '@nestjs/common';
 import { PhotosService } from '../services/photos.service';
 
@@ -14,26 +15,31 @@ export class PhotosController {
   constructor(private readonly photosService: PhotosService) {}
 
   @Post()
+  @HttpCode(201)
   create(@Body() photo: any) {
     return this.photosService.create(photo);
   }
 
   @Get()
+  @HttpCode(200)
   findAll() {
     return this.photosService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(200)
   findOne(@Param('id') id: number) {
     return this.photosService.findOne(id);
   }
 
   @Patch(':id')
+  @HttpCode(200)
   update(@Param('id') id: number, @Body() body: any) {
     return this.photosService.update(id, body);
   }
 
   @Delete(':id')
+  @HttpCode(200)
   delete(@Param('id') id: number) {
     return this.photosService.delete(id);
   }
