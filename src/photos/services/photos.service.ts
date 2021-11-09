9b28826a8514646fcc6ea54aaf36a1159a41c513
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-09 10:57:21 -0300

    injected a repository from PhotosEntity and updated all functions which now return Observables

--- src/photos/services/photos.service.ts
+++ src/photos/services/photos.service.ts
@@ -1,35 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { PhotosEntity } from '../models/photos.entity';
+import { PhotosInterface } from '../models/photos.interface';
+import { Observable, from } from 'rxjs';
 
 @Injectable()
 export class PhotosService {
-  create(photo: any) {
-    return `Photo ${photo} created`;
+  constructor(
+    @InjectRepository(PhotosEntity)
+    private readonly photosRepository: Repository<PhotosEntity>,
+  ) {}
+
+  create(photo: PhotosInterface): Observable<PhotosInterface> {
+    return from(this.photosRepository.save(photo));
   }
 
-  findAll() {
-    return [1, 2, 3, 4];
+  findAll(): Observable<PhotosEntity[]> {
+    return from(this.photosRepository.find());
   }
 
-  findOne(id: number) {
-    return `{
-      id: ${id},
-      created_at: Date,
-      url: varchar (200),
-      width: smallint,
-      height: smallint,
-      size: integer,
-      subject: integer,
-      subject_type: varchar (200),
-      redirect_url: varchar(200)
-    }
-    `;
+  findOne(id: number): Observable<PhotosEntity> {
+    return from(this.photosRepository.findOne(id));
   }
 
-  update(id: number, body: any) {
-    return `Photo id= ${id} updated as ${body}`;
+  update(id: number, body: any): Observable<UpdateResult> {
+    return from(this.photosRepository.update(id, body));
   }
 
-  delete(id: number) {
-    return `Photo id= ${id} was deleted`;
+  delete(id: number): Observable<DeleteResult> {
+    return from(this.photosRepository.delete(id));
   }
 }
