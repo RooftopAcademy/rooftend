dff3b6ed73c1bf587582d09f531ae9ffc473023c
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-09 19:54:22 -0300

    eslint refactor...

--- src/profile/mock/profiles.ts
+++ src/profile/mock/profiles.ts
@@ -1,22 +1,22 @@
 export default [
-    {
-        "id": 1,
-        "firstName": "Marcos",
-        "lastName": "Galperin",
-        "avatarUrl" : "https://i.pravatar.cc/150?img=3",
-        "identification" : {
-            "type" : "DNI",
-            "number" : "123456789"
-        }
+  {
+    id: 1,
+    firstName: 'Marcos',
+    lastName: 'Galperin',
+    avatarUrl: 'https://i.pravatar.cc/150?img=3',
+    identification: {
+      type: 'DNI',
+      number: '123456789',
     },
-    {
-        "id": 2,
-        "firstName": "Hernan",
-        "lastName": "Kazah",
-        "avatarUrl" : "https://i.pravatar.cc/150?img=3",
-        "identification" : {
-            "type" : "DNI",
-            "number" : "123456789"
-        }
-    }
-]
+  },
+  {
+    id: 2,
+    firstName: 'Hernan',
+    lastName: 'Kazah',
+    avatarUrl: 'https://i.pravatar.cc/150?img=3',
+    identification: {
+      type: 'DNI',
+      number: '123456789',
+    },
+  },
+];
