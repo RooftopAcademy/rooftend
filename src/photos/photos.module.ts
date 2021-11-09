4b8428f176d674e6e1ec0a2d0253d4efa48b9de2
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-09 10:55:46 -0300

    added PhotosEntity in the photosModule

--- src/photos/photos.module.ts
+++ src/photos/photos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PhotosController } from './controllers/photos.controller';
+import { PhotosEntity } from './models/photos.entity';
 import { PhotosService } from './services/photos.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PhotosEntity])],
   controllers: [PhotosController],
   providers: [PhotosService],
 })
