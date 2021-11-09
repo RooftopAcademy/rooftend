a5deebcc6b2be85047df2c26408fd429c7741077
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-08 23:03:35 -0300

    change returned values

--- src/review/services/review.service.ts
+++ src/review/services/review.service.ts
@@ -3,7 +3,29 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ReviewService {
   findAll() {
-    return [];
+    return [
+      {
+        id: 1,
+        title: 'title',
+        content: 'content',
+        user: {},
+        item: {},
+      },
+      {
+        id: 2,
+        title: 'title 2',
+        content: 'content 2',
+        user: {},
+        item: {},
+      },
+      {
+        id: 3,
+        title: 'title 3',
+        content: 'content 3',
+        user: {},
+        item: {},
+      },
+    ];
   }
 
   findOne(id: string) {
@@ -17,14 +39,28 @@ export class ReviewService {
   }
 
   create(body: any) {
-    return 'Review created.';
+    // it would return the created entity
+    return {
+        id: 4,
+        title: 'title 4',
+        content: 'content 4',
+        user: {},
+        item: {},
+    };
   }
 
   update(id: string, body: any) {
-    return `Review ${id} updated`;
+    // it would return the updated entity
+    return {
+        id: 1,
+        title: 'updated title',
+        content: 'updated content',
+        user: {},
+        item: {},
+    };
   }
 
   delete(id: string) {
-    return `Review ${id} deleted.`;
+    return;
   }
 }
