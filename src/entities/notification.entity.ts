a9c592eb6e720d8a70884e86965ad7deec2893a9
Ayrton <ayrtoncravero26@gmail.com>
2021-11-09 16:17:23 -0300

    adding columns in the entity

--- src/entities/notification.entity.ts
+++ src/entities/notification.entity.ts
@@ -1,7 +1,34 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Notification {
     @PrimaryGeneratedColumn()
-    id: number;
+    id: Number;
+
+    @Column()
+    user_id: Number
+
+    @Column()
+    action_url: String
+
+    @Column()
+    title: String
+
+    @Column()
+    description: String
+
+    @Column()
+    image_url: String
+
+    @Column()
+    created_at: Date
+
+    @Column()
+    sent_at: Date
+
+    @Column()
+    read_at: Date
+
+    @Column()
+    deleted_at: Date
 }
