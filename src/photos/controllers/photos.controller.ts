1f3c5a4f0d1381bff46207c7d93ea91ed0061123
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-09 10:52:57 -0300

    updated the retun values

--- src/photos/controllers/photos.controller.ts
+++ src/photos/controllers/photos.controller.ts
@@ -6,41 +6,42 @@ import {
   Patch,
   Post,
   Delete,
-  HttpCode,
 } from '@nestjs/common';
+import { PhotosInterface } from '../models/photos.interface';
 import { PhotosService } from '../services/photos.service';
+import { PhotosEntity } from '../models/photos.entity';
+import { UpdateResult, DeleteResult } from 'typeorm';
+import { Observable } from 'rxjs';
 
 @Controller('photos')
 export class PhotosController {
   constructor(private readonly photosService: PhotosService) {}
 
   @Post()
-  @HttpCode(201)
-  create(@Body() photo: any) {
+  create(@Body() photo: PhotosInterface): Observable<PhotosInterface> {
     return this.photosService.create(photo);
   }
 
   @Get()
-  @HttpCode(200)
-  findAll() {
+  findAll(): Observable<PhotosEntity[]> {
     return this.photosService.findAll();
   }
 
   @Get(':id')
-  @HttpCode(200)
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id') id: number): Observable<PhotosEntity> {
     return this.photosService.findOne(id);
   }
 
   @Patch(':id')
-  @HttpCode(200)
-  update(@Param('id') id: number, @Body() body: any) {
+  update(
+    @Param('id') id: number,
+    @Body() body: PhotosInterface,
+  ): Observable<UpdateResult> {
     return this.photosService.update(id, body);
   }
 
   @Delete(':id')
-  @HttpCode(200)
-  delete(@Param('id') id: number) {
+  delete(@Param('id') id: number): Observable<DeleteResult> {
     return this.photosService.delete(id);
   }
 }
