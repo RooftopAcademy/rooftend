6afa50a8d4d4c123efe26340ca2432f7e2e387b6
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-09 19:53:48 -0300

    added eslint requirements

--- src/profile/controllers/profiles.controller.ts
+++ src/profile/controllers/profiles.controller.ts
@@ -1,40 +1,49 @@
-import { 
-    Controller, Get, Post, Patch, Body, Delete, 
-    Req, HttpCode, Header, Redirect, Param, Res 
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Body,
+  Delete,
+  Req,
+  Param,
+  Res,
 } from '@nestjs/common';
-import { Request } from 'express'
-import profiles from '../mock/profiles'
-import {ProfileService} from '../services/profile/profile.service'
+import { Request } from 'express';
+import { ProfileService } from '../services/profile/profile.service';
 
 @Controller('profiles')
 export class ProfilesController {
-    public constructor(private readonly service : ProfileService) {}
+  public constructor(private readonly service: ProfileService) {}
+  @Get()
+  public index(@Req() req: Request) {
+    return this.service.all();
+  }
 
-    @Get()
-    public index(@Req() req : Request) {
-        return this.service.all()
-    }
+  @Get(':id')
+  public find(@Param('id') id, @Res() res) {
+    this.service
+      .find(id)
+      .then((data) => {
+        return data;
+      })
+      .catch((err) => {
+        return res.status(404).end(err.message);
+      });
+  }
 
-    @Get(':id')
-    public find(@Param('id') id, @Res() res) {
-        this.service.find(id)
-        .then(data => {
-            return data
-        }).catch(err => {
-            return res.status(404).end(err.message)
-        })
-    }
+  @Patch(':id')
+  public edit(@Body() body, @Res() res) {
+    return;
+  }
 
-    @Patch(':id')
-    public edit(@Body() body, @Res() res) {}
+  @Delete(':id')
+  public delete(@Param('id') id, @Res() res) {
+    res.send('ok');
+  }
 
-    @Delete(':id')
-    public delete(@Param('id') id, @Res() res) {
-        res.send('ok')
-    }
-
-    @Post()
-    public create(@Body() body, @Res() res) {
-        res.send(body)
-    }
+  @Post()
+  public create(@Body() body, @Res() res) {
+    res.send(body);
+  }
 }
