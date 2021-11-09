8a26b64afeacff04f617b43177ae40c51d6adc45
alejandro <info@alezvi.com.ar>
2021-11-09 17:46:28 -0300

    created profile modules as example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfileModule } from './profile/profile.module';
+import { ProfilesController } from './profile/controllers/profiles.controller';
+import {ProfileService} from './profile/services/profile/profile.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type : 'postgres',
+      host : process.env.DB_HOST,
+      port : Number(process.env.DB_PORT),
+      username : process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database : process.env.DB_NAME,
+      autoLoadEntities : true,
+      // synchronize: true,
+    }),
+    ProfileModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
