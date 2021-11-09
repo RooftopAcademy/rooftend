308bdd84a4c481f8c229f348df6b971c749720e4
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-09 19:35:36 -0300

    new PhotosInterface

--- src/photos/models/photos.interface.ts
+++ src/photos/models/photos.interface.ts
@@ -1,11 +1,11 @@
 export interface PhotosInterface {
   id?: number;
-  created_at?: Date;
+  createdAt?: Date;
   url: string;
   width: number;
   height: number;
   size: number;
-  subject_id: number;
-  subject_type: string;
-  redirect_url?: string;
+  subjectId: number;
+  subjectType: string;
+  redirectUrl?: string;
 }
