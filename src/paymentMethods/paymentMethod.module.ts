f90bc10462fd73acf84086f88c1b7948fdb4f93a
agustintosco <agustintosco.92@gmail.com>
2021-11-10 18:04:13 +0100

    updated payment method module

--- src/paymentMethods/paymentMethod.module.ts
+++ src/paymentMethods/paymentMethod.module.ts
@@ -1,12 +1,16 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { PaymentMethodsController } from './paymentMethod.controller';
-import { PaymentMethodsService } from './paymentMethod.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import PaymentMethodsController from './controllers/paymentMethod.controller';
+import PaymentMethod from './paymentMethod.entity';
+import PaymentMethodsService from './services/paymentMethod.service';
 
 @Module({
     providers: [PaymentMethodsService],
     controllers: [PaymentMethodsController],
-    imports: []
+    imports: [
+        TypeOrmModule.forFeature([PaymentMethod])
+    ]
 })
 
 export class PaymentMethdosModule {}
