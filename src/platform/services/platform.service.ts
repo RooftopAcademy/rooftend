d3cf8b95b6a45af1bc9035bce3a3333e68454c54
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-10 10:24:03 -0300

    Rearrange entity folder

--- src/platform/services/platform.service.ts
+++ src/platform/services/platform.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Platform } from '../entities/platform.entity';
+import { Platform } from '../platform.entity';
 
 @Injectable()
 export class PlatformService {
