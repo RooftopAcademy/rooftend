edac327355950733bca6264e4bc671190d35310b
MariaDelCarmenLuque <maritcarmn@gmail.com>
2021-11-10 15:41:58 -0500

    Added pagination to categories controller

--- src/categories/categories/controllers/categories.controller.ts
+++ src/categories/categories/controllers/categories.controller.ts
@@ -10,47 +10,57 @@ import {
   Patch,
   Req,
   Res,
+  DefaultValuePipe,
+  ParseIntPipe,
+  Query
 } from '@nestjs/common';
 import { Request } from 'express';
 import categories from '../mock/categories';
+import {Category} from 'src/categories/categories.entity';
 import { CategoriesService } from '../services/categories.service';
-
+import  {  Pagination  } from 'nestjs-typeorm-paginate' ;
 
 @Controller('categories')
 export class CategoriesController {
 
    public constructor (private readonly categoriesService: CategoriesService) {}
-    @Get()
-    public index(@Req() req : Request) {
-    return this.categoriesService.getAll()
-    }
-   
+    
+   @Get()
+   getAll(){
+       return this.categoriesService.getAll();
+   }
+
+   @Get('')
+    async index(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
+  ): Promise <Pagination<Category>>{
+    limit = limit > 100 ? 100 : limit;
+    return this.categoriesService.paginate({
+      page,
+      limit,
+      route: '/categories',
+    });
+  }
     @Get(':id')
     @HttpCode(201)
-    public findOneById(@Param('id') id:number | string){
-        
-        // this.categoriesService.findOneById(id)
-        // .then(data=>{
-        //     return data
-        // }).catch(err => {
-        //     return res.status(404).end(err.message)
-        // })
-    }
-
+    public findOneById(@Param('id') id:number){}
+    
     @Post()
     @HttpCode(201)
-    create(@Body() body){
+    create(@Body() body: any) {
         return this.categoriesService.create(body);
     }
 
     @Patch(':id')
     @HttpCode(200)
-    update(@Param('id') id:number, @Body() body:any){
-        return this.categoriesService.update(id,body)
+    update(@Param('id') id: number, @Body() body: any) {
+        return this.categoriesService.update(id, body);
     }
+    
     @Delete(':id')
     @HttpCode(204)
-    delete(@Param('id') id:number,@Res() res){
-        res.send('ok')
+    delete(@Param('id') id: number) {
+        return this.categoriesService.delete(id);
     }
 }
