77c7638ea4f391de42f8450c985279af7a3edf7e
GSaucedo <g.saucedo.kd@gmail.com>
2021-11-10 12:42:57 -0600

    added TypeOrm

--- src/phones/phones.module.ts
+++ src/phones/phones.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PhonesService } from './services/phones.service';
 import { PhonesController } from './controllers/phones.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Phone } from './entities/phone.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Phone])],
   providers: [PhonesService],
   controllers: [PhonesController],
 })
-export class TelefonosModule {}
+export class PhonesModule {}
