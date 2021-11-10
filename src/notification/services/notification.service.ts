011be8757e152acc95be5c34a670da1fc00e3203
Ayrton <ayrtoncravero26@gmail.com>
2021-11-10 16:33:18 -0300

    changes in the methods

--- src/notification/services/notification.service.ts
+++ src/notification/services/notification.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { from } from 'rxjs';
 import { Repository } from 'typeorm';
+import { paginate, Pagination, IPaginationOptions } from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class NotificationService {
