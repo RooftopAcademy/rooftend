cd40057bebf3c3e83ba581d16fbfcce2fbf29086
GSaucedo <g.saucedo.kd@gmail.com>
2021-11-10 12:43:50 -0600

    implemented ORM

--- src/phones/services/phones.service.ts
+++ src/phones/services/phones.service.ts
@@ -1,24 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, UpdateResult } from 'typeorm';
+import { Phone } from '../entities/phone.entity';
 
 @Injectable()
 export class PhonesService {
-  findAll(): string {
-    return 'Phones list';
+  constructor(
+    @InjectRepository(Phone) private phoneRepository: Repository<Phone>,
+  ) {}
+
+  findAll(): Promise<Phone[]> {
+    return this.phoneRepository.find();
   }
 
-  findOne(id: number): string {
-    return `Phone with id: ${id}`;
+  findOne(id: number): Promise<Phone> {
+    return this.phoneRepository.findOne(id);
   }
 
-  create(phone: any): string {
-    return `Create phone with id: ${phone.id}`;
+  create(phone: any): Promise<Phone> {
+    return this.phoneRepository.save(phone);
   }
 
-  update(id: number, phone: any): string {
-    return `Update phone with id: ${id} and ${phone}`;
+  async update(id: number, phoneChanges: any): Promise<String> {
+    try {
+      const phone = await this.phoneRepository.findOne(id);
+      this.phoneRepository.merge(phone, phoneChanges);
+      this.phoneRepository.save(phone);
+      return 'Phone updated';
+    } catch (e) {
+      return 'Phone not found id: ' + id;
+    }
   }
 
-  delete(id: number): string {
-    return `Delete phone with id: ${id}`;
+  async delete(id: number): Promise<String> {
+    try {
+      await this.phoneRepository.delete(id);
+      return 'Phone deleted';
+    } catch (e) {
+      return 'Phone not found';
+    }
   }
 }
