3cc464baaabbc8506f34976e46dd30d839b6c6a1
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-10 10:52:28 -0300

    more eslint refactoring

--- src/profile/services/profile/profile.service.ts
+++ src/profile/services/profile/profile.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import {Profile} from '../../profile.entity'
+import { Profile } from '../../profile.entity';
 
 @Injectable()
 export class ProfileService {
-    public constructor(
-        @InjectRepository(Profile) private repository : Repository<Profile>
-    ) {}
+  public constructor(
+    @InjectRepository(Profile) private repository: Repository<Profile>,
+  ) {}
 
-    all() {
-        return this.repository.find()
-    }
+  all() {
+    return this.repository.find();
+  }
 
-    find(id : string | number) {
-        return this.repository.findOne(id)
-    }
+  find(id: string | number) {
+    return this.repository.findOne(id);
+  }
 }
