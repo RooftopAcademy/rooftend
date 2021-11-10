1eedae20b2eacb86f85fdad3b309aee6ea990d5d
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-10 17:09:43 -0300

    Add new controller for use paginated result

--- src/favorites/controllers/favorites.controller.ts
+++ src/favorites/controllers/favorites.controller.ts
@@ -7,13 +7,35 @@ import {
   Param,
   Post,
   Put,
+  ParseIntPipe,
+  DefaultValuePipe,
+  Query,
 } from '@nestjs/common';
-
+import { Pagination } from 'nestjs-typeorm-paginate';
 import { FavoritesService } from '../services/favorites.service';
+
+import { Favorite } from '../entities/favorite.entity';
 
 @Controller('favorites')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
+
+  @Get()
+  @HttpCode(200)
+  async index(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+  ): Promise<Pagination<Favorite>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.favoritesService.paginate({
+      page,
+      limit,
+      route: `${process.env.DB_HOST.substring(
+        0,
+        process.env.DB_HOST.length - 1,
+      )}:${Number(process.env.DB_PORT)}/favorites`,
+    });
+  }
 
   @Get()
   @HttpCode(200)
