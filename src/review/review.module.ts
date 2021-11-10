509123d58d113a1be205ad8ad90b0676ef3a556b
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-10 12:06:57 -0300

    inject repo into module

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReviewController } from './controllers/review.controller';
+import { Review } from './review.entity';
 import { ReviewService } from './services/review.service';
 
 @Module({
   controllers: [ReviewController],
   providers: [ReviewService],
+  imports: [TypeOrmModule.forFeature([Review])],
 })
 export class ReviewModule {}
