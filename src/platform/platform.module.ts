60d78e61c2dbd3c880d7fc068c27a72a253976a2
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-10 10:23:52 -0300

    Rearrange entity folder

--- src/platform/platform.module.ts
+++ src/platform/platform.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PlatformService } from './services/platform.service';
 import { PlatformController } from './controllers/platform.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Platform } from './entities/platform.entity';
+import { Platform } from './platform.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Platform])],
