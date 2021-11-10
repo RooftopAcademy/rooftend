437e2ecfb496a499bcc4b65fc8c8a7fef195c66d
gab <gbangertes@gmail.com>
2021-11-10 17:21:54 -0300

    Added pagination to getAll endpoint.

--- src/stores/controller/stores.controller.ts
+++ src/stores/controller/stores.controller.ts
@@ -8,11 +8,16 @@ import {
   HttpCode,
   Body,
   Res,
+  Query,
+  DefaultValuePipe,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { DeleteResult } from 'typeorm';
+import { Pagination } from 'nestjs-typeorm-paginate';
 
 import { StoresService } from './stores.service';
 import { StoresInterface } from '../models/stores.interface';
+import { StoresEntity } from '../models/stores.entity';
 
 @Controller('stores')
 export class StoresController {
@@ -20,8 +25,16 @@ export class StoresController {
 
   @Get()
   @HttpCode(200)
-  getAll(): Promise<StoresInterface[]> {
-    return this.storesService.getAll();
+  async getAll(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
+  ): Promise<Pagination<StoresEntity>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.storesService.paginate({
+      page,
+      limit,
+      route: '/stores',
+    });
   }
 
   @Get()
