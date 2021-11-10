bc18abe0fc55a4aa0071c56e62d5eb80bbebf6c4
agustintosco <agustintosco.92@gmail.com>
2021-11-10 16:53:09 +0100

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,40 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+<<<<<<< HEAD
 import { PaymentMethodsController } from './paymentMethod.controller';
 import { PaymentMethodsService } from './paymentMethod.service';
 
 @Module({
-  imports: [],
+  imports: [PaymentMethdosModuleModule],
   controllers: [AppController, PaymentMethodsController],
   providers: [AppService, PaymentMethodsService],
+=======
+import { ProfileModule } from './profile/profile.module';
+import { ProfilesController } from './profile/controllers/profiles.controller';
+import {ProfileService} from './profile/services/profile/profile.service'
+import { PaymentMethdosModuleModule } from './payment-methdos-module/payment-methdos-module.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type : 'postgres',
+      host : process.env.DB_HOST,
+      port : Number(process.env.DB_PORT),
+      username : process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database : process.env.DB_NAME,
+      autoLoadEntities : true,
+      // synchronize: true,
+    }),
+    ProfileModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+>>>>>>> 8fa7bfaef7fd2d45b87679e48a2f29927df654bd
 })
 export class AppModule {}
