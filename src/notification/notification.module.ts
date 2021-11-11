9866ae42dbced7717882a68fc3de3291d7e89dcf
Ayrton <ayrtoncravero26@gmail.com>
2021-11-11 15:23:59 -0300

    import module notification

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './services/notification.service';
 import { NotificationController } from './controllers/notification.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from 'src/entities/notification.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Notification])
+  ],
   providers: [NotificationService],
   controllers: [NotificationController],
 })
