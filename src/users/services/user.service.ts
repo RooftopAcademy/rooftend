cf446cec86a40457b2f2a67dbc6222c8a67d94d4
pabloroque1 <pabloroque1995@gmail.com>
2021-11-11 17:15:40 -0300

    Add paginate to service

--- src/users/services/user.service.ts
+++ src/users/services/user.service.ts
@@ -3,13 +3,23 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 
+import {
+    paginate,
+    Pagination,
+    IPaginationOptions,
+  } from 'nestjs-typeorm-paginate';
+
 @Injectable()
 export class UserService {
     constructor(@InjectRepository(User) private userRepo: Repository<User>){
 
     }
 
-    findOne(id: number){
+    async paginate(options: IPaginationOptions ): Promise<Pagination<User>> {
+        return paginate<User>(this.userRepo, options);
+      }
+
+    findOne(id: number): Promise<User>{
         return this.userRepo.findOne(id);
     }
 
@@ -22,7 +32,7 @@ export class UserService {
         return this.userRepo.save(newUser);
     }
 
-    async update(id: number, body: any){
+    async update(id: number, body: any): Promise<User>{
         const user = await this.userRepo.findOne(id);
         this.userRepo.merge(user,body);
         return this.userRepo.save(user);
