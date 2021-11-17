89d9d3fc63cbd916b1ddfd6f818412f4aa3a64ab
agustintosco <agustintosco.92@gmail.com>
2021-11-17 20:15:29 +0100

    Added initial test for PM service

--- src/paymentMethods/services/paymentMethod.service.spec.ts
+++ src/paymentMethods/services/paymentMethod.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import PaymentMethod from '../paymentMethod.entity';
 import PaymentMethodsService from './paymentMethod.service';
 
 describe('PaymentMethodsService', () => {
   let service: PaymentMethodsService;
 
+  let mockPaymentMethodRepo = {}
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PaymentMethodsService],
+      providers: [PaymentMethodsService, {
+        provide: getRepositoryToken(PaymentMethod),
+        useValue: mockPaymentMethodRepo
+      }],
     }).compile();
 
     service = module.get<PaymentMethodsService>(PaymentMethodsService);
