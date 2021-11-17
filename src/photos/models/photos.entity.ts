f3d0caeccfbda630ea9d19efae64bfcc7e2a76b4
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-17 19:49:17 -0300

    added nullable option to photos.entity

--- src/photos/models/photos.entity.ts
+++ src/photos/models/photos.entity.ts
@@ -9,27 +9,38 @@ export class PhotosEntity {
     type: 'timestamp with time zone',
     name: 'created_at',
     default: () => 'CURRENT_TIMESTAMP',
+    nullable: true,
   })
   createdAt: Date;
 
-  @Column({ type: 'varchar', length: 200 })
+  @Column({ type: 'varchar', length: 200, nullable: false })
   url: string;
 
-  @Column({ type: 'smallint' })
+  @Column({ type: 'smallint', nullable: false })
   width: number;
 
-  @Column({ type: 'smallint' })
+  @Column({ type: 'smallint', nullable: false })
   height: number;
 
-  @Column({ type: 'smallint' })
+  @Column({ type: 'smallint', nullable: false })
   size: number;
 
-  @Column({ type: 'integer', name: 'subject_id' })
+  @Column({ type: 'integer', name: 'subject_id', nullable: false })
   subjectId: number;
 
-  @Column({ type: 'varchar', length: 100, name: 'subject_type' })
+  @Column({
+    type: 'varchar',
+    length: 100,
+    name: 'subject_type',
+    nullable: false,
+  })
   subjectType: string;
 
-  @Column({ type: 'varchar', length: 200, name: 'redirect_url' })
+  @Column({
+    type: 'varchar',
+    length: 200,
+    name: 'redirect_url',
+    nullable: true,
+  })
   redirectUrl: string;
 }
