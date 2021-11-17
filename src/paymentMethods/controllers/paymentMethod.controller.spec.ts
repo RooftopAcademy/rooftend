3a2b824eecc0ecdeadb22d443f7a629535d6b36e
agustintosco <agustintosco.92@gmail.com>
2021-11-17 17:58:18 +0100

    Added initial test to PM controller

--- src/paymentMethods/controllers/paymentMethod.controller.spec.ts
+++ src/paymentMethods/controllers/paymentMethod.controller.spec.ts
@@ -1,13 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import PaymentMethodsService from '../services/paymentMethod.service';
 import PaymentMethodsController from './paymentMethod.controller';
 
 describe('PaymentMethodsController', () => {
   let controller: PaymentMethodsController;
 
+  const PaymentMethodsUserService = {
+
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PaymentMethodsController],
-    }).compile();
+      providers: [PaymentMethodsService]
+    })
+      .overrideProvider(PaymentMethodsService)
+      .useValue(PaymentMethodsUserService)
+      .compile();
 
     controller = module.get<PaymentMethodsController>(PaymentMethodsController);
   });
