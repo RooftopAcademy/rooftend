c8a2c02124571031ed4ca6707008ea87d67dad5a
agustintosco <agustintosco.92@gmail.com>
2021-11-17 17:34:26 +0100

    Modified entity properties

--- src/paymentMethods/paymentMethod.entity.ts
+++ src/paymentMethods/paymentMethod.entity.ts
@@ -1,27 +1,46 @@
 /* eslint-disable prettier/prettier */
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity({
     name: 'payment_methods'
 })
 export default class PaymentMethod {
-  @PrimaryGeneratedColumn({
-    unsigned: true,
-    type: 'bigint'
-  })
-  id: number;
+    @PrimaryGeneratedColumn({
+        unsigned: true,
+        type: 'bigint'
+    })
+    id: number;
 
-  @Column({
-      name: 'name',
-      type: 'character varying',
-      length: 15
-  })
-  name: string;
+    @Column({
+        name: 'name',
+        type: 'character varying',
+        length: 15,
+        nullable: false
+    })
+    name: string;
 
-  @Column({
-    name: 'type',
-    type: 'character varying',
-    length: 15
-})
-  type: string;
+    @Column({
+        name: 'type',
+        type: 'character varying',
+        length: 15,
+        nullable: false
+    })
+    type: string;
+
+    @CreateDateColumn({
+        name: 'created_at',
+        type: 'timestamptz',
+        default: () => 'CURRENT_TIMESTAMP',
+        nullable: false
+    })
+    created_at : Date;
+
+    @UpdateDateColumn({
+        name: 'updated_at',
+        type: 'timestamptz',
+        default: () => 'CURRENT_TIMESTAMP',
+        nullable: false
+    })
+    updated_at : Date;
+
 }
