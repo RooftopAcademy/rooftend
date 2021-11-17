0b55bdc69661eb493ed3479d9a835af71ccc64da
agustintosco <agustintosco.92@gmail.com>
2021-11-17 19:51:55 +0100

    Added create PM test for controller

--- src/paymentMethods/controllers/paymentMethod.controller.spec.ts
+++ src/paymentMethods/controllers/paymentMethod.controller.spec.ts
@@ -5,8 +5,13 @@ import PaymentMethodsController from './paymentMethod.controller';
 describe('PaymentMethodsController', () => {
   let controller: PaymentMethodsController;
 
-  const PaymentMethodsUserService = {
-
+  const mockPaymentMethodsService = {
+    create: jest.fn(dto => {
+      return {
+        id: Date.now(),
+        ...dto
+      }
+    })
   }
 
   beforeEach(async () => {
@@ -15,7 +20,7 @@ describe('PaymentMethodsController', () => {
       providers: [PaymentMethodsService]
     })
       .overrideProvider(PaymentMethodsService)
-      .useValue(PaymentMethodsUserService)
+      .useValue(mockPaymentMethodsService)
       .compile();
 
     controller = module.get<PaymentMethodsController>(PaymentMethodsController);
@@ -24,4 +29,18 @@ describe('PaymentMethodsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a payment method', () => {
+    const dto = {name: 'Cash', type: 'CASH'};
+    expect(controller.create(dto))
+    .toEqual({
+      id: expect.any(Number),
+      name: 'Cash',
+      type: 'CASH'
+    })
+  
+  expect(mockPaymentMethodsService.create).toHaveBeenCalledWith(dto);
+
+  })
+
 });
