4a8b524e1a706936d29fdb5f986292c09b7a7746
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-18 12:05:17 -0300

    Fix, imports needed for module

--- src/account-status/account-status.module.ts
+++ src/account-status/account-status.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AccountStatusController } from './controllers/account-status.controller';
 import { AccountStatusService } from './services/account-status.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountStatusEntity } from './models/account-status.entity';
 
 @Module({
   providers: [AccountStatusService],
-  controllers: [AccountStatusController]
+  controllers: [AccountStatusController],
+  imports: [
+    TypeOrmModule.forFeature([AccountStatusEntity])
+  ]
 })
 export class AccountStatusModule { }
