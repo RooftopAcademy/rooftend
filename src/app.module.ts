c7e23ef661f0139c9134edb642cb641c119db97b
gbangertes <gbangertes@gmail.com>
2021-11-21 17:38:34 -0300

    Added missing lines in app.module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoresModule } from './stores/stores.module';
 
 @Module({
-  imports: [StoresModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      // synchronize: true,
+    }),
+    StoresModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
