808b50870cbd5fa189638ded3b1b3a566023daeb
ChachoPacho <bordonnet@hotmail.com>
2021-11-23 17:43:59 -0300

    removed extra folder

--- src/cart-item/cart-item.module.ts
+++ src/cart-item/cart-item.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { CartItemService } from './services/cart-item/cart-item.service';
-import { CartItemController } from './controllers/cart-item/cart-item.controller';
+import { CartItemService } from './services/cart-item.service';
+import { CartItemController } from './controllers/cart-item.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { cartItem } from './models/cart-item.entity';
 
