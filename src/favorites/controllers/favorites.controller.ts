188c7760a498b6234ed6621c289c5911e340fca7
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-11-23 17:22:56 -0300

    Added swagger to favorites.controller

--- src/favorites/controllers/favorites.controller.ts
+++ src/favorites/controllers/favorites.controller.ts
@@ -12,18 +12,24 @@ import {
   Query,
   Res,
 } from '@nestjs/common';
+
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
 import { Response } from 'express';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { FavoritesService } from '../services/favorites.service';
 
 import { Favorite } from '../entities/favorite.entity';
 
+@ApiTags('favorites')
 @Controller('favorites')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
   @Get()
   @HttpCode(200)
+  @ApiOperation({ summary: 'Get a result page with 10 records' })
+  @ApiResponse({ status: 200, description: 'OK.' })
   public async index(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
@@ -37,6 +43,8 @@ export class FavoritesController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get the record according to the id entered' })
+  @ApiResponse({ status: 200, description: 'OK.', type: Favorite })
   public async getById(@Param('id') id: number | string, @Res() res: Response) {
     const response = await this.favoritesService.getById(id);
     if (response) return res.status(200).send(response).end();
@@ -45,18 +53,24 @@ export class FavoritesController {
 
   @Post()
   @HttpCode(201)
+  @ApiOperation({ summary: 'Create Favorite' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   public create(@Body() body: any) {
     return this.favoritesService.create(body);
   }
 
   @Put(':id')
   @HttpCode(204)
+  @ApiOperation({ summary: 'Update Favorite' })
+  @ApiResponse({ status: 200, description: 'OK.' })
   public update(@Param('id') id: number | string, @Body() body: any) {
     return this.favoritesService.update(id, body);
   }
 
   @Delete(':id')
   @HttpCode(200)
+  @ApiOperation({ summary: 'Delete Favorite' })
+  @ApiResponse({ status: 200, description: 'OK.' })
   public delete(@Param('id') id: number | string) {
     return this.favoritesService.delete(id);
   }
