3d0d432f1f0618c20ebc152eb717307685cf2f27
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2021-11-23 11:50:24 -0300

    updated app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { AccountStatusModule } from './account-status/account-status.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { NotificationModule } from './notification/notification.module';
@@ -12,8 +11,7 @@ import { ProfileModule } from './profile/profile.module';
 import { ReviewModule } from './review/review.module';
 import { ShippingMethodsModule } from './shipping-methods/shipping-methods.module';
 import { UsersModule } from './users/users.module';
-import { PhotosEntity } from './photos/models/photos.entity';
-import { Profile } from './profile/profile.entity';
+import { PhotosModule } from './photos/photos.module';
 
 @Module({
   imports: [
@@ -25,14 +23,14 @@ import { Profile } from './profile/profile.entity';
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      entities: [PhotosEntity, Profile],
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
     }),
 
     AccountStatusModule,
     FavoritesModule,
     NotificationModule,
+    PhotosModule,
     PlatformModule,
     ProfileModule,
     ReviewModule,
