a69a0850c7a820970468bc26e1d1a1430b492c4a
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-23 17:01:56 -0300

    Add TypeOrmModules

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './services/notification.service';
 import { NotificationController } from './controllers/notification.controller';
+import { Notification } from './notification.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Notification])],
   providers: [NotificationService],
   controllers: [NotificationController],
 })
