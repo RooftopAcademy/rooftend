34f754a194001231ba78e17a62f627da34f2c7e4
ChachoPacho <bordonnet@hotmail.com>
2021-11-24 16:08:34 -0300

    Items services created

---
+++ src/items/services/items.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Items } from 'src/items/models/item.entity';
+import { ItemsInterface } from 'src/items/models/item.interface';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ItemsService {
+  constructor(
+    @InjectRepository(Items)
+    private readonly ItemsRepo: Repository<Items>,
+  ) { }
+
+  findAll(): Promise<ItemsInterface[]> {
+    return this.ItemsRepo.find();
+  }
+
+  findOne(id: number): Promise<ItemsInterface> {
+    return this.ItemsRepo.findOne(id);
+  }
+
+  create(body: any): Promise<ItemsInterface> {
+    return this.ItemsRepo.save(body);
+  }
+
+  async update(id: number, body: any): Promise<ItemsInterface> {
+    const item = await this.findOne(id);
+    this.ItemsRepo.merge(item, body);
+    return this.ItemsRepo.save(item);
+  }
+
+  async delete(id: number): Promise<boolean> {
+    await this.ItemsRepo.delete(id);
+    return true;
+  }
+}
