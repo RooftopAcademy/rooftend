c7bc7b39b47273c7f8e653438db31f399722efdf
SantiagoMansilla <mansillasanti1200@gmail.com>
2021-11-24 14:39:13 -0300

    added documentation to brands files

--- src/brands/entities/brands.entity.ts
+++ src/brands/entities/brands.entity.ts
@@ -1,19 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity({name: 'brands'})
 export class Brand {
   @PrimaryGeneratedColumn({
     type: 'bigint'
   })
-  @ApiProperty({example: 1, description: 'The id of the brand'})
+  @ApiProperty({example: 1, description: 'The id of the brand', type: BigInt})
   id: number;
 
   @Column()
-  @ApiProperty({example: 'nike', description: 'The name of the brand'})
+  @ApiProperty({example: 'nike', description: 'The name of the brand', type: String})
   name: string;
 
   @Column()
-  @ApiProperty({example: '', description: 'The logo of the brand'})
+  @ApiProperty({example: '', description: 'The logo of the brand', type: String})
   photoId: string; 
 }
--- src/brands/entities/create-brands-dto.entity.ts
+++ src/brands/entities/create-brands-dto.entity.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 
 export class createBrandDTO{
     @IsString()
+    @ApiProperty({example: '', description: 'The name of the brand', type: String})
     name: string;
 
     @IsString()
+    @ApiProperty({example: '', description: 'The logo of the brand', type: String})
     photoId: string;
 }
