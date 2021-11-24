5af3d58de411de65072fec4dab17d3c6c28d97ae
gbangertes <gbangertes@gmail.com>
2021-11-24 15:18:58 -0300

    Modified stores interface.

--- src/stores/models/stores.interface.ts
+++ src/stores/models/stores.interface.ts
@@ -1,8 +1,12 @@
+import { Banner } from "src/banners/entities/banners.entity";
+import { Brand } from "src/brands/entities/brands.entity";
+import { User } from "src/users/entities/user.entity";
+
 export interface StoresInterface {
   id?: number;
   createdAt?: Date;
   updatedAt?: Date;
-  brandId?: number;
-  userId?: number;
-  bannerId?: number;
+  brandId?: Brand;
+  userId?: User;
+  bannerId?: Banner;
 }
