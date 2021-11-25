e4371964d2a51fdc75701301f48c9e6185b645b6
alejandro <info@alezvi.com.ar>
2021-11-25 16:44:36 -0300

    fix type

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,7 +31,7 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(<string>process.env.DB_HOST),
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
