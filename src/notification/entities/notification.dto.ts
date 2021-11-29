98d33d3c4ff5521adb56ae45b3f764fb74d0ea71
Ayrton <ayrtoncravero26@gmail.com>
2021-11-29 19:57:40 -0300

    the one of how the class was exploited was changed

--- src/notification/entities/notification.dto.ts
+++ src/notification/entities/notification.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-export class CreateNotificationDto {
+export class NotificationDto {
     @ApiProperty({
         description: 'The id of user.',
         type: Number
