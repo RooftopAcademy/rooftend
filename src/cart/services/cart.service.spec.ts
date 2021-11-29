86deec53747ed72dd5cc23734ea0588d871fdb36
Sebastian Arroyo <larroyo@fi.uba.ar>
2021-11-28 21:43:28 -0300

    cart.service testing for update and merge methods

--- src/cart/services/cart.service.spec.ts
+++ src/cart/services/cart.service.spec.ts
@@ -7,12 +7,37 @@ describe('CartService', () => {
     let service: CartService;
     const mockCartRepository = {
         create: jest.fn().mockImplementation(cart => cart),
-        save: jest.fn().mockImplementation(cart => Promise.resolve({id: Date.now(), created_at: Date.now(), updated_at: Date.now(), ...cart}))
+        save: jest.fn().mockImplementation(cart => Promise.resolve({ 
+            id: Date.now(), 
+            created_at: Date.now(), 
+            updated_at: Date.now(), 
+            ...cart 
+        })),
+        findOne: jest.fn().mockImplementation((id) => Promise.resolve({ 
+            id, 
+            created_at: Date.now(), 
+            updated_at: Date.now(), 
+            amount: 555, 
+            currencyCode: 'abc', 
+            userId: 1 
+        })),
+        merge: jest.fn().mockImplementation((newCart, oldCart) => Promise.resolve({ 
+            id: Date.now(), 
+            created_at: Date.now(), 
+            updated_at: Date.now(), 
+            ...oldCart 
+        })),
+        update: jest.fn().mockImplementation((id, cart) => Promise.resolve({
+            id,
+            created_at: Date.now(),
+            updated_at: Date.now(),
+            ...cart
+        })),
     }
 
-    beforeEach(async () =>{
+    beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [CartService,{
+            providers: [CartService, {
                 provide: getRepositoryToken(Cart),
                 useValue: mockCartRepository
             }],
@@ -21,12 +46,12 @@ describe('CartService', () => {
         service = module.get<CartService>(CartService);
     });
 
-    it('should be defined', () =>{
+    it('should be defined', () => {
         expect(service).toBeDefined();
     });
 
-    it('should create a new cart and return it', async () =>{
-        const cart = {currencyCode: 'ab1', amount: 555, userId : 1};
+    it('should create a new cart and return it', async () => {
+        const cart = { currencyCode: 'ab1', amount: 555, userId: 1 };
         expect(await service.create(cart)).toEqual({
             id: expect.any(Number),
             currencyCode: cart.currencyCode,
@@ -35,5 +60,28 @@ describe('CartService', () => {
             created_at: expect.any(Number),
             updated_at: expect.any(Number),
         })
-    })
+    });
+    it('should find a cart with the given id and return it', async () => {
+        expect(await service.findOne(10)).toEqual({
+            id: 10,
+            currencyCode: expect.any(String),
+            userId: expect.any(Number),
+            amount: expect.any(Number),
+            created_at: expect.any(Number),
+            updated_at: expect.any(Number),
+
+        })
+    });
+    it('should update a cart and return it', async () => {
+        const cart = { currencyCode: 'abc', amount: 555, userId: 1 };
+        expect(await service.update(10, cart)).toEqual({
+            id: 10,
+            created_at: expect.any(Number),
+            updated_at: expect.any(Number),
+            amount: cart.amount,
+            currencyCode : cart.currencyCode,
+            userId: cart.userId,
+        })
+    });
+
 });
