bbaf9d60d6e88ee533c579f89dc117b71ccd5c66
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-12-01 00:40:09 -0300

    Improve and modified favorite entity, bettered documentation

--- src/favorites/entities/favorite.entity.ts
+++ src/favorites/entities/favorite.entity.ts
@@ -9,44 +9,56 @@ import { ManyToOne } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { User } from '../../users/entities/user.entity';
 
-@Entity()
+@Entity('favorite')
 export class Favorite {
+  @ApiProperty({
+    type: "integer",
+    description: 'The ID of the Favorite record.',
+    example: 1,
+    readOnly: true,
+  })
   @PrimaryGeneratedColumn({
+    name: 'id',
     unsigned: true,
     type: 'bigint',
   })
-  @ApiProperty({ example: 1, description: 'The record ID' })
   id: number;
 
+  @ApiProperty({
+    type: "integer",
+    description: 'The user ID who added the item to favorites',
+    example: 8,
+    readOnly: true,
+  })
   @Column({
+    name: 'user_id',
     unsigned: true,
     type: 'bigint',
-  })
-  @ApiProperty({
-    example: '8',
-    description: 'The user ID who added the item to favorites',
   })
   user_id: number;
 
+  @ApiProperty({
+    type: "integer",
+    description: 'The Item ID that was added to favorites',
+    example: 3,
+    readOnly: true,
+  })
   @Column({
+    name: 'item_id',
     unsigned: true,
     type: 'bigint',
   })
-  @ApiProperty({
-    example: '3',
-    description: 'The Item ID that was added to favorites',
-  })
   item_id: number;
 
+  @ApiProperty({
+    format: 'date',
+    description: 'The date the record was last updated',
+    example: '2021-11-15 17:32:19.537+00',
+  })
   @UpdateDateColumn({
     name: 'updated_at',
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
-  })
-  @ApiProperty({
-    example: '2021-11-15 17:32:19.537+00',
-    description: 'The date the record was last updated',
-    format: 'date',
   })
   updatedAt: Date;
 
