47680c62d3b895c85c457e7f5ec80a9f5d75abdc
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-12-01 00:42:16 -0300

    Deleted useless methods and improved responses for endpoints

--- src/favorites/services/favorites.service.ts
+++ src/favorites/services/favorites.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Favorite } from '../entities/favorite.entity';
+import { CreateFavoriteDto } from '../dto/create-favorite.dto';
 
 import {
   paginate,
   Pagination,
   IPaginationOptions,
 } from 'nestjs-typeorm-paginate';
+import { ResCreateFavoriteDto } from '../dto/res-create-favorite.dto';
 
 @Injectable()
 export class FavoritesService {
@@ -21,9 +23,17 @@ export class FavoritesService {
     return paginate<Favorite>(this.favoritesRepo, options);
   }
 
-  create(body: any): Promise<Favorite[]> {
-    const newFavorite = this.favoritesRepo.create(body);
-    return this.favoritesRepo.save(newFavorite);
+  create(createFavoriteDto: CreateFavoriteDto): Promise<ResCreateFavoriteDto> {
+    const newFavorite = this.favoritesRepo.create(createFavoriteDto);
+    const res = this.favoritesRepo.save(newFavorite);
+    let response: ResCreateFavoriteDto
+    return res.then(i => 
+      response = {
+        "id": i.id,
+        "item_id": i.item_id,
+        "updatedAt": i.updatedAt
+      }
+    )
   }
 
   async delete(id: number | string): Promise<void> {
