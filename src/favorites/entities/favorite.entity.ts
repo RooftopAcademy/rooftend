92ebb299469d3ac5ad684dd5a340b443e273d784
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-12-05 23:57:51 -0300

    Improved the documentation and relationships of the Favorite entity

--- src/favorites/entities/favorite.entity.ts
+++ src/favorites/entities/favorite.entity.ts
@@ -3,68 +3,64 @@ import {
   Column,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
+  JoinColumn,
 } from 'typeorm';
 
 import { ManyToOne } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { User } from '../../users/entities/user.entity';
+import { Item } from '../../items/entities/items.entity';
 
 @Entity('favorite')
 export class Favorite {
   @ApiProperty({
-    type: "integer",
+    type: 'integer',
     description: 'The ID of the Favorite record.',
-    example: 1,
+    nullable: false,
     readOnly: true,
+    example: 1,
   })
   @PrimaryGeneratedColumn({
     name: 'id',
     unsigned: true,
-    type: 'bigint',
+    type: 'integer',
   })
   id: number;
 
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
   @ApiProperty({
-    type: "integer",
+    type: 'integer',
     description: 'The user ID who added the item to favorites',
-    example: 8,
+    nullable: false,
     readOnly: true,
-  })
-  @Column({
-    name: 'user_id',
-    unsigned: true,
-    type: 'bigint',
+    example: 8,
   })
   user_id: number;
 
+  @ManyToOne(() => Item)
+  @JoinColumn({ name: 'item_id' })
   @ApiProperty({
-    type: "integer",
+    type: 'integer',
     description: 'The Item ID that was added to favorites',
-    example: 3,
+    nullable: false,
     readOnly: true,
-  })
-  @Column({
-    name: 'item_id',
-    unsigned: true,
-    type: 'bigint',
+    example: 3,
   })
   item_id: number;
 
   @ApiProperty({
+    type: Date,
     format: 'date',
+    default: 'now()',
     description: 'The date the record was last updated',
     example: '2021-11-15 17:32:19.537+00',
   })
   @UpdateDateColumn({
     name: 'updated_at',
     type: 'timestamptz',
+    nullable: false,
     default: () => 'CURRENT_TIMESTAMP',
   })
-  updatedAt: Date;
-
-  // @ManyToOne(type => Item, item => item.favorites)
-  // item: Item;
-
-  @ManyToOne((type) => User, (user) => user.favorites)
-  user: User;
+  updated_at: Date;
 }
