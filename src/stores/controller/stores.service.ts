28c23b4eb3534f59ea7b2c1bae36765a37e0de62
gbangertes <gbangertes@gmail.com>
2021-12-06 14:58:52 -0300

    Fixed stores services.

--- src/stores/controller/stores.service.ts
+++ src/stores/controller/stores.service.ts
@@ -1,36 +1,56 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, UpdateResult } from 'typeorm';
-import { StoresInterface } from '../models/stores.interface';
-import { StoresEntity } from '../models/stores.entity';
+import { Store } from '../models/stores.entity';
 import { DeleteResult } from 'typeorm/browser';
 import {
   IPaginationOptions,
-  Pagination,
-  paginate,
+  paginateRaw,
 } from 'nestjs-typeorm-paginate';
+import { ReadStoreDto } from '../models/read-store.dto';
+import { CreateStoreDto } from '../models/create-store.dto';
 
 @Injectable()
 export class StoresService {
   constructor(
-    @InjectRepository(StoresEntity)
-    private readonly storesRepository: Repository<StoresEntity>,
+    @InjectRepository(Store)
+    private readonly storesRepository: Repository<Store>,
   ) {}
 
-  // async paginate(options: IPaginationOptions) {
-  //   return paginate<StoresInterface>(this.storesRepository, options);
-  // }
-
-  getOne(id: string | number): Promise<StoresEntity> {
-    return this.storesRepository.findOne(id);
+  async paginate(options: IPaginationOptions) {
+    return paginateRaw<Store>(
+      this.storesRepository.createQueryBuilder('store')
+        .leftJoinAndSelect('store.user', 'user')
+        .leftJoinAndSelect('store.brand', 'brand')
+        .select([
+          'store.id::integer AS "id"',
+          'user.username AS "username"',
+          'brand.name AS "brand"',
+        ]),
+      options,
+    );
   }
 
-  create(store: StoresInterface): Promise<StoresInterface> {
-    return this.storesRepository.save(store);
+  async getOne(id: string | number): Promise<ReadStoreDto> {
+    return await this.storesRepository.createQueryBuilder('store')
+      .leftJoinAndSelect('store.brand', 'brand')
+      .select([
+        'brand.name AS "brand"',
+      ]).where('store.id = :id', { id })
+      .getRawOne()
   }
 
-  async update(id: number, store: StoresInterface): Promise<UpdateResult> {
-    return this.storesRepository.update(id, store);
+  async create(createStoreDto: CreateStoreDto): Promise<CreateStoreDto> {
+    return this.storesRepository.save(createStoreDto).then((res) => {
+      return {
+        userId: res.userId,
+        brandId: res.brandId,
+      }
+    });
+  }
+
+  async update(id: number, createStoreDto: CreateStoreDto): Promise<UpdateResult> {
+    return this.storesRepository.update(id, createStoreDto);
   }
 
   delete(id: number): Promise<DeleteResult> {
