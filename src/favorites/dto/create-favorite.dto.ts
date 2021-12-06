626e69b72b4d7e02d9a44a16cff019e4863997fb
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-12-06 00:00:52 -0300

    A DTO was created for the cases of creating favorites

--- src/favorites/dto/create-favorite.dto.ts
+++ src/favorites/dto/create-favorite.dto.ts
@@ -2,6 +2,6 @@ import { OmitType, PartialType } from "@nestjs/swagger";
 import { FavoriteDto } from "./favorite.dto";
 
 export class CreateFavoriteDto extends PartialType(
-  OmitType(FavoriteDto, ['id', 'updatedAt'] as const),
+  OmitType(FavoriteDto, ['id', 'user_id', 'updatedAt'] as const),
 ) {
 }
