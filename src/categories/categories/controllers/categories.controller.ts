20dff597cdd563e70c3257c4ee33527bfa3683f0
MariaDelCarmenLuque <maritcarmn@gmail.com>
2021-12-06 11:26:53 -0300

    Improves in documentation

--- src/categories/categories/controllers/categories.controller.ts
+++ src/categories/categories/controllers/categories.controller.ts
@@ -17,14 +17,27 @@ import { CategoriesService } from '../services/categories.service';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { Category } from '../../categories.entity';
 import { Response } from 'express';
-import { ApiOperation, ApiTags ,ApiParam, ApiOkResponse,ApiNotFoundResponse} from '@nestjs/swagger';
-import { send } from 'process';
+import { ApiOperation, ApiTags ,ApiParam, ApiOkResponse,ApiNotFoundResponse, ApiBody, ApiResponse} from '@nestjs/swagger';
 
-@ApiTags('categories')
+
+@ApiTags('Categories')
 @Controller('categories')
 export class CategoriesController {
   public constructor(private readonly categoriesService: CategoriesService) {}
     
+  @Get()
+  @ApiOperation({summary: 'Returns categories available'})
+  @ApiOkResponse({
+    type:Category,
+    description: 'The categories available',
+    isArray: true
+  })
+  @ApiNotFoundResponse({
+    description: 'No categories available',
+    schema: {
+      example: new NotFoundException('No categories available').getResponse(),
+    },
+  })
     async index(
       @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
       @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
@@ -39,11 +52,11 @@ export class CategoriesController {
   
     @Get(':id')
     @HttpCode(200)
-    @ApiOperation({summary: 'List categories'})
+    @ApiOperation({summary: ' categories'})
     @ApiParam({ name: 'id', type: Number, required: true, example: 1 })
     @ApiOkResponse({
       type: Category,
-      description: 'The found review.',
+      description: 'The category found.',
     })
     @ApiNotFoundResponse({
       description: 'Category not found.',
@@ -54,26 +67,36 @@ export class CategoriesController {
     async findOne(@Param('id') id:number, @Res() res:Response){
       const data = await this.categoriesService.findOne(id);
       if (data) return res.status(200).send(data).end();
-      return res.status(404).end('Status Not Found')
+      res.status(404).end('Status Not Found')
     }
   
     @Post()
-    @HttpCode(201)
-    create(@Body() body: any) {
-        return this.categoriesService.create(body);
+    @ApiResponse({
+      status: 403,
+      description:'Forbidden'
+    })
+    @HttpCode(403)
+    create(@Res() res:Response) {
+        res.status(403).end();
     }
 
-    @Patch(':id')
-    @HttpCode(200)
-    @ApiParam({ name: 'id', type: Number, required: true, example: 1 })
-    update(@Param('id') id: number, @Body() body: any) {
-        return this.categoriesService.update(id, body);
+    @Patch()
+    @ApiResponse({
+      status: 403,
+      description:'Forbidden'
+    })
+    @HttpCode(403)
+    update(@Res() res: Response) {
+        res.status(403).end();
     }
     
-    @Delete(':id')
-    @HttpCode(204)
-    @ApiParam({ name: 'id', type: Number, required: true, example: 1 })
-    delete(@Param('id') id: number) {
-        return this.categoriesService.delete(id);
+    @Delete()
+    @ApiResponse({
+      status: 403,
+      description:'Forbidden'
+    })
+    @HttpCode(403)
+    delete(@Res() res: Response) {
+        res.status(403).end();
     }
 }
