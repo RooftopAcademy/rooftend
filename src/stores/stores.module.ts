1567f31b25f5be7fadcd0271f8bc51913b012595
gbangertes <gbangertes@gmail.com>
2021-12-06 14:56:38 -0300

    Changed entity name and fixed relations.

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StoresController } from './controller/stores.controller';
 import { StoresService } from './controller/stores.service';
-import { StoresEntity } from './models/stores.entity';
+import { Store } from './models/stores.entity';
 
 @Module({
   controllers: [StoresController],
   providers: [StoresService],
-  imports: [TypeOrmModule.forFeature([StoresEntity])],
+  imports: [TypeOrmModule.forFeature([Store])],
 })
 export class StoresModule {}
