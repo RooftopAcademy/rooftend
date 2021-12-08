f4cd428e520fbcea5c6048815109b006cdd46faa
GSaucedo <g.saucedo.kd@gmail.com>
2021-12-08 03:00:32 -0600

    Implemented basic test for controller

--- src/saved-items/controllers/saved-items.controller.spec.ts
+++ src/saved-items/controllers/saved-items.controller.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SavedItemsController } from './saved-items.controller';
+import { SavedItemsService } from '../services/saved-items.service';
 
 describe('SavedItemsController', () => {
   let controller: SavedItemsController;
 
+  const SavedItemsServiceMock = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [SavedItemsService],
       controllers: [SavedItemsController],
-    }).compile();
+    })
+      .overrideProvider(SavedItemsService)
+      .useValue(SavedItemsServiceMock)
+      .compile();
 
     controller = module.get<SavedItemsController>(SavedItemsController);
   });
