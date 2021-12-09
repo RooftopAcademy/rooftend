e53afc9c5a57314e8d280c63073c444c733f1ec3
Sebastian Arroyo <larroyo@fi.uba.ar>
2021-12-09 14:23:07 -0300

    added soldOffersPercentage swagger documentation

--- src/offers/controllers/offers.controller.ts
+++ src/offers/controllers/offers.controller.ts
@@ -8,7 +8,9 @@ import {
 } from '@nestjs/common';
 import { OffersService } from '../services/offers.service';
 import { PromotionTypeDto } from '../dto/promotion-type.dto';
+import { ApiForbiddenResponse, ApiNotFoundResponse, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Offers')
 @Controller('offers')
 export class OffersController {
   constructor(private readonly offersService: OffersService) {}
@@ -26,9 +28,19 @@ export class OffersController {
     }, promotionType)
   }
 
-  @Get(":id")
-  async getSoldOffers(@Param('id') id: number,):Promise<number>{
+  @Get(':id')
+    @ApiParam({
+        name: "id",
+        type: "integer",
+        required: true
+    })
+    @ApiOperation({summary: 'Gets percentage of items sold from and offer that matches given id'})
+    @ApiResponse({status: 201, description: 'Offer succesfully found'})
+    @ApiForbiddenResponse({ status: 403, description: 'Forbidden.'})
+    @ApiNotFoundResponse({status: 404, description: 'No Offer was found that matches that id'})
+  async getSoldOffers(@Param('id') id: number,):Promise<string>{
     const soldOffersPercentage = await this.offersService.getOffer(id);
-    return soldOffersPercentage.initialStock -soldOffersPercentage.soldStock
+    let percentage = (soldOffersPercentage.initialStock -soldOffersPercentage.soldStock)*100/soldOffersPercentage.initialStock
+    return percentage + '%'
   }
 }
