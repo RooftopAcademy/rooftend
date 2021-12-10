7fe763caee438916c7e0c1dd439fef076c6b04e2
gbangertes <gbangertes@gmail.com>
2021-12-10 13:13:25 -0300

    Added controller unit tests.

--- src/offers/controllers/offers.controller.spec.ts
+++ src/offers/controllers/offers.controller.spec.ts
@@ -1,20 +1,63 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import {
+  Test,
+  TestingModule,
+} from '@nestjs/testing';
+
 import { OffersController } from './offers.controller';
 import { OffersService } from '../services/offers.service';
+import { PromotionType } from '../entities/offer.entity';
 
 describe('OffersController', () => {
   let controller: OffersController;
+
+  const mockOffersService = {
+    paginate: jest.fn(),
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OffersController],
       providers: [OffersService],
-    }).compile();
+    })
+      .overrideProvider(OffersService)
+      .useValue(mockOffersService)
+      .compile();
 
     controller = module.get<OffersController>(OffersController);
   });
 
-  it('should be defined', () => {
+  it('Should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Service should be called with default parameters', () => {
+    const paginateOptions = {
+      page: 1,
+      limit: 50,
+      route: '/offers',
+    };
+    controller.findAll();
+    expect(mockOffersService.paginate).toBeCalledWith(paginateOptions);
+  });
+
+  it('Sevice should be called with a specifict value for page parameter', () => {
+    const paginateOptions = {
+      page: 2,
+      limit: 50,
+      route: '/offers',
+    };
+    controller.findAll(2);
+    expect(mockOffersService.paginate).toBeCalledWith(paginateOptions);
+  });
+
+  it('Sevice should be called with promotion type parameter', () => {
+    const paginateOptions = {
+      page: 2,
+      limit: 50,
+      route: '/offers',
+    };
+    const promotionType: PromotionType = PromotionType.DEAL_OF_THE_DAY;
+    controller.findAll(2, promotionType);
+    expect(mockOffersService.paginate).toBeCalledWith(paginateOptions, promotionType);
   });
 });
