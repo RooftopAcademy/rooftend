010a5fe0c2bc51b99c7ec4aaa4b7c884186516f6
agustintosco <agustintosco.92@gmail.com>
2021-12-14 23:48:25 +0100

    changed dir

---
+++ src/payment-methods/models/create-payment-method.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+/* eslint-disable prettier/prettier */
+export default class PaymentMethodDto {
+  @ApiProperty({
+    example: 'CASH',
+    description: 'The name of the payment method',
+    type: String,
+  })
+  name: string;
+
+  @ApiProperty({
+    example: 'Cash',
+    description: 'The type of the payment method, usually similar to the name',
+    type: String,
+  })
+  type: string;
+}
