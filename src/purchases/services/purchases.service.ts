453dcb9b5209593288759a84aa2f9f173fd391fd
Marcos Giordano <marcos_l14@hotmail.com>
2021-12-16 08:54:11 -0300

    Remove comments and modify logic for the findOneById method to return a PurrchaseDetails object.

--- src/purchases/services/purchases.service.ts
+++ src/purchases/services/purchases.service.ts
@@ -5,11 +5,11 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IsNull, Not, Repository } from 'typeorm';
-import { CartItem } from '../../cart-item/entities/cart-item.entity';
 import { Cart } from '../../cart/entities/cart.entity';
 import { Delivery } from '../../deliveries/entities/delivery.entity';
-import { Item } from '../../items/entities/items.entity';
 import { PhotosEntity } from '../../photos/models/photos.entity';
+import { ItemDetails } from '../entities/item-details.entity';
+import { PurchaseDetails } from '../entities/purchase-details.entity';
 
 @Injectable()
 export class PurchasesService {
@@ -27,7 +27,10 @@ export class PurchasesService {
     });
   }
 
-  async findOneById(id: number | string, userId: number) {
+  async findOneById(
+    id: number | string,
+    userId: number,
+  ): Promise<PurchaseDetails> {
     const purchase = await this.cartRepository.findOne({
       where: {
         id,
@@ -43,44 +46,41 @@ export class PurchasesService {
       throw new ForbiddenException('This cart has not been purchased');
     }
 
-    // const newQuery = this.cartRepository.query(`
-    //   SELECT Ca.purchased_at, Ph.url, I.title, I.price, CI.quantity, De.status
-    //   FROM carts AS Ca
-    //   INNER JOIN cart_item AS CI ON Ca.id = CI.cart_id
-    //   INNER JOIN items AS I ON CI.item_id = I.id
-    //   LEFT JOIN photos AS Ph ON Ph.subject_id = CI.item_id
-    //   LEFT JOIN deliveries AS De ON De.cart_id = Ca.id
-    //   WHERE Ca.id = ${id}`);
-
     const newQuery = await this.cartRepository
       .createQueryBuilder('carts')
       .where('carts.id = :id')
       .setParameter('id', id)
       .andWhere('carts.user_id =:userId')
       .setParameter('userId', userId)
-      .innerJoin(CartItem, 'cart_item', 'carts.id = cart_item.cart_id')
-      .innerJoin(Item, 'items', 'cart_item.item_id = items.id')
+      .leftJoin('carts.cartItemsId', 'cart_item')
+      .leftJoin('cart_item.itemId', 'items')
       .leftJoin(PhotosEntity, 'photos', 'cart_item.item_id = photos.subject_id')
       .leftJoin(Delivery, 'deliveries', 'carts.id = deliveries.cart_id')
-      .select('carts.purchased_at')
+      .select('carts.purchased_at AS "purchasedAt"')
       .addSelect([
-        'items.title',
-        'items.price',
-        'cart_item.quantity',
-        'photos.url',
-        'deliveries.status',
+        'items.title AS title',
+        'items.price AS price',
+        'cart_item.quantity AS quantity',
+        'photos.url AS photo',
+        'deliveries.status AS "deliveryStatus"',
       ])
       .getRawMany();
 
-    // .innerJoin('carts.cartItemsId', 'cart_items')
+    const purchaseDetails: PurchaseDetails = new PurchaseDetails();
+    purchaseDetails.purchasedAt = newQuery[0].purchasedAt;
+    purchaseDetails.deliveryStatus = newQuery[0].deliveryStatus;
+    purchaseDetails.itemDetails = [];
 
-    // .innerJoinAndMapOne(
-    //   'cart_item.item_id',
-    //   Item,
-    //   'items',
-    //   'cart_item.item_id = items.id',
-    // )
+    for (let index = 0; index < newQuery.length; index++) {
+      const itemDetails = new ItemDetails();
+      itemDetails.title = newQuery[index].title;
+      itemDetails.quantity = newQuery[index].quantity;
+      itemDetails.price = newQuery[index].price;
+      itemDetails.photo = newQuery[index].photo;
 
-    return newQuery;
+      purchaseDetails.itemDetails.push(itemDetails);
+    }
+
+    return purchaseDetails;
   }
 }
