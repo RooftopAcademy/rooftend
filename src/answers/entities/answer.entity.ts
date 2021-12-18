777a0fb3739a456a6ff1b403c931a1d38ccd7ba1
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-18 18:43:40 -0300

    Inproved entity answer

--- src/answers/entities/answer.entity.ts
+++ src/answers/entities/answer.entity.ts
@@ -1,32 +1,49 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "../../users/entities/user.entity";
 
 @Entity()
 export class Answer {
 
-    @PrimaryGeneratedColumn({
-        unsigned: true,
-        type: 'bigint',
+    @ApiProperty({
+        type: Number,
+        description: 'Answer Id',
+        readOnly: true,
+        example: 1,
     })
+    @PrimaryGeneratedColumn({ type: 'bigint' })
+    @Type(() => Number)
     id: number;
 
-    @Column({
-        name: 'content',
-        type: 'character varying',
-        nullable: false,
+    @ApiProperty({
+        type: String,
+        maxLength: 500,
+        description: 'Answer content',
+        example: 'Gracias por preguntar, aun contamos con stock!',
     })
+    @Column({ name: 'content', type: 'varchar', length: 500 })
     content: string;
 
-    @CreateDateColumn({
-        name: 'created_at',
-        type: 'timestamptz',
-        default: () => 'CURRENT_TIMESTAMP',
-    })
-    created_at: Date;
-
     @OneToOne(() => User)
-    @JoinColumn({
-        name: 'user_id',
+    @JoinColumn()
+    @ApiProperty({
+        type: Number,
+        description: 'Id of user that answer',
+        nullable: false,
+        readOnly: true,
+        example: 2,
     })
-    user: User;
+    user_id: number;
+
+
+    @ApiProperty({
+        type: Date,
+        default: 'now()',
+        example: '2021-11-18T01:46:52.589Z',
+        description: 'The timestamp of the created answer',
+    })
+    @Column({ name: 'created_at', type: 'timestamp with time zone' })
+    @Type(() => Date)
+    createdAt: Date;
 }
