b543e7daa6e8d9e1529fe347917eb5faa999339c
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-19 18:36:51 -0300

    Added softdelete and documentation

--- src/answers/controllers/answers.controller.ts
+++ src/answers/controllers/answers.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { ApiBody, ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { Body, Controller, Delete, HttpCode, Param, Post } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiBody, ApiCreatedResponse, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+import { DeleteResult } from 'typeorm';
 import { AnswerDTO } from '../entities/answer.dto';
 import { AnswersService } from '../services/answers.service';
 
@@ -23,5 +24,25 @@ export class AnswersController {
     @ApiBody({ type: AnswerDTO })
     create(@Body() answer: AnswerDTO, userId: number) {
         return this.answersService.create(answer, userId);
+    }
+
+
+    @Delete(':id')
+    @HttpCode(204)
+    @ApiOperation({ summary: 'Delete answer' })
+    @ApiResponse({
+        status: 204,
+        description: 'Answer was deled.',
+    })
+    @ApiParam({
+        name: 'id',
+        example: 1,
+        type: Number,
+    })
+    @ApiBadRequestResponse({
+        description: 'Error, the deletion was not completed',
+    })
+    async delete(@Param('id') id: number): Promise<DeleteResult> {
+        return await this.answersService.deleteAnswer(id);
     }
 }
