4e63d7503247dea989ebb400f653dcd8c21dc6f6
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-19 16:11:54 -0300

    Inprove create and delete, changed name of entity

--- src/questions/services/questions.service.ts
+++ src/questions/services/questions.service.ts
@@ -1,42 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Repository } from 'typeorm';
-import { QuestionsEntity } from '../models/questions.entity';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Question } from '../entities/question.entity';
 import {
   paginate,
   Pagination,
   IPaginationOptions,
 } from 'nestjs-typeorm-paginate';
+import { QuestionDTO } from '../entities/questions.dto';
 
 @Injectable()
 export class QuestionsService {
   constructor(
-    @InjectRepository(QuestionsEntity)
-    private readonly questionsRepository: Repository<QuestionsEntity>,
-  ) {}
+    @InjectRepository(Question)
+    private readonly questionsRepository: Repository<Question>,
+  ) { }
 
   async paginateBy(
     relation: 'user' | 'item',
     options: IPaginationOptions,
     itemOrUserId: number,
-  ): Promise<Pagination<QuestionsEntity>> {
-    return await paginate<QuestionsEntity>(this.questionsRepository, options, {
+  ): Promise<Pagination<Question>> {
+    return await paginate<Question>(this.questionsRepository, options, {
       relations: [relation],
-      order: { created_at: 'DESC' },
+      order: { createdAt: 'DESC' },
       where: { relation: itemOrUserId },
     });
   }
 
-  createQuestion(question: QuestionsEntity): Promise<QuestionsEntity> {
-    return this.questionsRepository.save(question);
+  createQuestion(question: QuestionDTO, userId: number): Promise<Question> {
+    const questionEntity = this.questionsRepository.create({ ...question, "userId": userId });
+    return this.questionsRepository.save(questionEntity);
   }
 
-  deleteQuestion(id: number): Promise<DeleteResult> {
-    return this.questionsRepository.delete(id);
+  async deleteQuestion(id: number): Promise<UpdateResult> {
+    const deleteResponse = await this.questionsRepository.softDelete(id);
+    if (!deleteResponse.affected) {
+      throw new NotFoundException(id, 'Error, the deletion was not completed');
+    }
+    return deleteResponse;
   }
 }
-
-// async paginate(options: IPaginationOptions, item_id: number): Promise<Pagination<QuestionsEntity>> {
-//     const questions = this.questionsRepository.find(item_id)
-//     return paginate<QuestionsEntity>(questions, options);
-// }
