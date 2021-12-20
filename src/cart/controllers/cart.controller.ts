3c0a555bc6ffc9cf4051745422529622468fa830
Sebastian Arroyo <larroyo@fi.uba.ar>
2021-12-20 17:03:40 -0300

    Removed unnecessary endpoints and behavior carts module implements

--- src/cart/controllers/cart.controller.ts
+++ src/cart/controllers/cart.controller.ts
@@ -1,15 +1,8 @@
 import {
   Controller,
   Get,
-  Param,
-  Body,
-  Post,
-  Put,
-  Delete,
-  Patch,
 } from '@nestjs/common';
-import { ApiBody, ApiForbiddenResponse, ApiHeader, ApiNotFoundResponse, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CartItem } from '../../cart-item/entities/cart-item.entity';
+import { ApiForbiddenResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Cart } from '../entities/cart.entity';
 import { CartService } from '../services/cart.service';
 
@@ -27,47 +20,6 @@ export class CartController {
         return this.cartService.findAll();
     }
 
-    @ApiBody({
-        type: Cart
-    })
-    @Post()
-    @ApiOperation({summary: 'Creates cart'})
-    @ApiResponse({status: 201, description: 'Cart succesfully created'})
-    @ApiForbiddenResponse({ status: 403, description: 'Forbidden.'})
-    create (@Body() body : any): Promise<Cart>{
-        return this.cartService.create(body);
-    }
-
-    @Patch(':id')
-    @ApiParam({
-        name: "id",
-        type: "integer",
-        required: true
-    })
-    @ApiBody({
-        type: Cart
-    })
-    @ApiOperation({summary: 'Updates cart'})
-    @ApiResponse({status: 201, description: 'Cart succesfully updated'})
-    @ApiForbiddenResponse({ status: 403, description: 'Forbidden.'})
-    @ApiNotFoundResponse({status: 404, description: 'No Cart was found that matches that id'})
-    update(@Param('id') id : number, @Body() body : any): Promise<Cart>{
-        return this.cartService.update(id, body);
-    }
-
-    @Delete(':id')
-    @ApiParam({
-        name: "id",
-        type: "integer",
-        required: true
-    })
-    @ApiOperation({summary: 'Deletes a cart that matches the given id'})
-    @ApiResponse({status: 204, description: 'Cart succesfully deleted'})
-    @ApiForbiddenResponse({ status: 403, description: 'Forbidden.'})
-    @ApiNotFoundResponse({status: 404, description: 'No Cart was found that matches that id'})
-    delete(@Param('id') id : number ): Promise<void>{
-        return this.cartService.delete(id);
-    }
 
 }
 
