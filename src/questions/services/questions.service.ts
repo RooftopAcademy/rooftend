357a9d3e2acf268a8cbbe72356f16fc41ef79aee
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-27 21:13:45 -0300

    Fixed all methods, inproved throw exceptions, created querys for gets methods

--- src/questions/services/questions.service.ts
+++ src/questions/services/questions.service.ts
@@ -1,13 +1,18 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
-import { Question } from '../entities/question.entity';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException
+} from '@nestjs/common';
 import {
   paginate,
   Pagination,
   IPaginationOptions,
 } from 'nestjs-typeorm-paginate';
-import { QuestionDTO } from '../entities/question.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Question } from '../entities/question.entity';
+import { CreateQuestionDTO } from '../entities/create-question-dto';
+
 
 @Injectable()
 export class QuestionsService {
@@ -17,27 +22,76 @@ export class QuestionsService {
   ) { }
 
   async paginateBy(
-    relation: 'user' | 'item',
     options: IPaginationOptions,
-    itemOrUserId: number,
+    itemId: number,
   ): Promise<Pagination<Question>> {
-    return await paginate<Question>(this.questionsRepository, options, {
-      relations: [relation],
-      order: { createdAt: 'DESC' },
-      where: { relation: itemOrUserId },
-    });
+    try {
+      let questions = this.questionsRepository.createQueryBuilder('questions')
+        .where('questions.itemId = :itemId', { itemId })
+        .leftJoinAndMapOne('questions.user', 'questions.userId', 'user')
+        .leftJoinAndMapOne('questions.answer', 'questions.answerId', 'answer')
+        .select(['questions.content'])
+        .addSelect(['answer.content', 'answer.createdAt'])
+        .where('questions.answerId is not null');
+
+      return await paginate<Question>(questions, options);
+    }
+    catch (err) {
+      throw new NotFoundException();
+    }
   }
 
-  createQuestion(question: QuestionDTO, userId: number): Promise<Question> {
-    const questionEntity = this.questionsRepository.create({ ...question, "userId": userId });
-    return this.questionsRepository.save(questionEntity);
+  async paginateRecived(
+    options: IPaginationOptions,
+    userId: number,
+  ): Promise<Pagination<Question>> {
+    try {
+      let questions = this.questionsRepository.createQueryBuilder('questions')
+        .leftJoinAndMapOne('questions.user', 'questions.userId', 'user')
+        .leftJoinAndMapOne('questions.item', 'questions.itemId', 'item')
+        .select(['questions.content', 'questions.createdAt'])
+        .addSelect(['user.username', 'item.title'])
+        .where('questions.answerId is null')
+        .andWhere('item.userId = :userId', { userId })
+      return await paginate<Question>(questions, options);
+    }
+    catch (err) {
+      throw new NotFoundException();
+    }
   }
 
-  async deleteQuestion(id: number): Promise<UpdateResult> {
+  async paginateSent(
+    options: IPaginationOptions,
+    userId: number,
+  ): Promise<Pagination<Question>> {
+    try {
+      let questions = this.questionsRepository.createQueryBuilder('questions')
+        .leftJoinAndMapOne('questions.item', 'questions.itemId', 'item')
+        .leftJoinAndMapOne('questions.answer', 'questions.answerId', 'answer')
+        .select(['questions.id', 'questions.content', 'questions.createdAt'])
+        .addSelect(['item.title', 'item.description', 'item.price', 'item.stock', 'answer.content'])
+        .where('questions.userId = :userId', { userId })
+      return await paginate<Question>(questions, options);
+    }
+    catch (err) {
+      throw new NotFoundException();
+    }
+  }
+
+  async createQuestion(question: CreateQuestionDTO, userId: number): Promise<void> {
+    try {
+      const questionEntity = this.questionsRepository.create({ ...question, 'userId': userId, 'createdAt': new Date() });
+      await this.questionsRepository.save(questionEntity);
+    }
+    catch (err) {
+      throw new UnprocessableEntityException();
+    }
+  }
+
+  async deleteQuestion(id: number): Promise<void> {
     const deleteResponse = await this.questionsRepository.softDelete(id);
     if (!deleteResponse.affected) {
-      throw new NotFoundException(id, 'Error, the deletion was not completed');
+      throw new NotFoundException();
     }
-    return deleteResponse;
   }
 }
