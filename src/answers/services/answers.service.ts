7ef6b6c51f9bc82354106434ddd6c9560ac66ed0
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-27 21:03:33 -0300

    Fixed create method, injected questions repository

--- src/answers/services/answers.service.ts
+++ src/answers/services/answers.service.ts
@@ -1,29 +1,43 @@
-import { Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
+import {
+    Injectable,
+    NotFoundException,
+    UnprocessableEntityException
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, UpdateResult } from 'typeorm';
+import { Repository } from 'typeorm';
+import { Question } from '../../questions/entities/question.entity';
 import { AnswerDTO } from '../entities/answer.dto';
 import { Answer } from '../entities/answer.entity';
-
 @Injectable()
 export class AnswersService {
     constructor(
-        @InjectRepository(Answer) private AnswersRepository: Repository<Answer>,
+        @InjectRepository(Answer) private answersRepository: Repository<Answer>,
+        @InjectRepository(Question) private questionsRepository: Repository<Question>,
     ) { }
 
     async create(answer: AnswerDTO): Promise<void> {
         try {
-            const answerEntity = this.AnswersRepository.create({ ...answer, 'createdAt': new Date() });
-            await this.AnswersRepository.save(answerEntity)
+            const partialAnswer = this.answersRepository.create({ ...answer, 'createdAt': new Date() });
+            const answerEntity = await this.answersRepository.save(partialAnswer);
+            await this.saveAnswer(answer.questionId, Number(answerEntity.id));
         }
         catch (err) {
             throw new UnprocessableEntityException();
         }
     }
 
+    async saveAnswer(questionId: number, answerId: number): Promise<void> {
+        const updateResponse = await this.questionsRepository.update(questionId, { 'answerId': answerId });
+        if (!updateResponse.affected) {
+            throw new NotFoundException();
+        }
+    }
+
     async deleteAnswer(id: number): Promise<void> {
-        const deleteResponse = await this.AnswersRepository.softDelete(id);
+        const deleteResponse = await this.answersRepository.softDelete(id);
         if (!deleteResponse.affected) {
             throw new NotFoundException();
         }
     }
+
 }
