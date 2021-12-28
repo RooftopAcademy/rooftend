6bc2a1af80cde95d97df5c8c7990b15fd36a5305
Pablo Canale <pablocanale.exa@gmail.com>
2021-12-27 21:04:57 -0300

    imported Question entity in answer module

--- src/answers/answers.module.ts
+++ src/answers/answers.module.ts
@@ -3,11 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Answer } from './entities/answer.entity';
 import { AnswersService } from './services/answers.service';
 import { AnswersController } from './controllers/answers.controller';
-
-
+import { Question } from '../questions/entities/question.entity';
 @Module({
   providers: [AnswersService],
-  imports: [TypeOrmModule.forFeature([Answer])],
+  imports: [TypeOrmModule.forFeature([Answer, Question])],
   controllers: [AnswersController],
 })
 export class AnswersModule { }
