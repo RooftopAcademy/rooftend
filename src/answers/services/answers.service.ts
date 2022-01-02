397743f29a63b8a4cfc0f4e026f6ef799859634f
Pablo Canale <pablocanale.exa@gmail.com>
2022-01-02 20:09:28 -0300

    Refactored methods, used try/catch in delete, status returned now from here

--- src/answers/services/answers.service.ts
+++ src/answers/services/answers.service.ts
@@ -6,8 +6,10 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Question } from '../../questions/entities/question.entity';
+import Status from '../../statusCodes';
 import { AnswerDTO } from '../entities/answer.dto';
 import { Answer } from '../entities/answer.entity';
+
 @Injectable()
 export class AnswersService {
     constructor(
@@ -15,29 +17,27 @@ export class AnswersService {
         @InjectRepository(Question) private questionsRepository: Repository<Question>,
     ) { }
 
-    async create(answer: AnswerDTO): Promise<void> {
+    async create(answer: AnswerDTO): Promise<Status> {
         try {
             const partialAnswer = this.answersRepository.create({ ...answer, 'createdAt': new Date() });
             const answerEntity = await this.answersRepository.save(partialAnswer);
-            await this.saveAnswer(answer.questionId, Number(answerEntity.id));
+            await this.questionsRepository.update(answerEntity.questionId, { 'answerId': Number(answerEntity.id) });
+            return Status.CREATED
         }
         catch (err) {
             throw new UnprocessableEntityException();
         }
     }
 
-    async saveAnswer(questionId: number, answerId: number): Promise<void> {
-        const updateResponse = await this.questionsRepository.update(questionId, { 'answerId': answerId });
-        if (!updateResponse.affected) {
-            throw new NotFoundException();
+    async delete(id: number): Promise<Status> {
+        try {
+            await this.answersRepository.softDelete(id);
+            return Status.DELETED
         }
-    }
+        catch (err) {
+            throw new NotFoundException()
+        }
 
-    async deleteAnswer(id: number): Promise<void> {
-        const deleteResponse = await this.answersRepository.softDelete(id);
-        if (!deleteResponse.affected) {
-            throw new NotFoundException();
-        }
     }
 
 }
