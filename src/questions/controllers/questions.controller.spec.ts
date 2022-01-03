065e267750f711c91fc2160737a21c780e2e9ac8
Pablo Canale <pablocanale.exa@gmail.com>
2022-01-03 01:04:26 -0300

    Created test for questions.controller

--- src/questions/controllers/questions.controller.spec.ts
+++ src/questions/controllers/questions.controller.spec.ts
@@ -4,6 +4,8 @@ import { QuestionsService } from '../services/questions.service'
 import Status from '../../statusCodes';
 import { NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { CreateQuestionDTO } from '../entities/create-question-dto';
+import { IPaginationMeta, Pagination } from 'nestjs-typeorm-paginate';
+import { Question } from '../entities/question.entity';
 
 describe('QuestionController test', () => {
     let controllerQuestions: QuestionsController
@@ -38,58 +40,62 @@ describe('QuestionController test', () => {
             )
         }),
         paginateRecived: jest.fn().mockImplementation((options, userId) => {
-            [
-                {
-                    "content": "Soft kitty, little kitty song?",
-                    "createdAt": "2021-12-27T18:19:14.893Z",
-                    "user": {
-                        "username": "CatOwner1",
+            return Promise.resolve(
+                [
+                    {
+                        "content": "Soft kitty, little kitty song?",
+                        "createdAt": "2021-12-27T18:19:14.893Z",
+                        "user": {
+                            "username": "CatOwner1",
+                        },
+                        "item": {
+                            "title": "Balls of wool",
+                        }
                     },
-                    "item": {
-                        "title": "Balls of wool",
-                    }
-                },
-                {
-                    "content": "Are you allergic to cats?",
-                    "createdAt": "2021-12-27T18:23:59.363Z",
-                    "user": {
-                        "username": "CatOwner2",
+                    {
+                        "content": "Are you allergic to cats?",
+                        "createdAt": "2021-12-27T18:23:59.363Z",
+                        "user": {
+                            "username": "CatOwner2",
+                        },
+                        "item": {
+                            "title": "Candies for your cats :3",
+                        }
                     },
-                    "item": {
-                        "title": "Candies for your cats :3",
-                    }
-                },
-            ]
+                ]
+            )
         }),
         paginateSent: jest.fn().mockImplementation((options, userId) => {
-            [
-                {
-                    "id": "14",
-                    "content": "Why you did you said that cats are not included? Batman has a cat?",
-                    "createdAt": "2021-12-27T18:23:59.363Z",
-                    "item": {
-                        "title": "Batman toy, cats are not included",
-                        "description": "Dieta a base de platanos",
-                        "price": 1872,
-                        "stock": 21
+            return Promise.resolve(
+                [
+                    {
+                        "id": "14",
+                        "content": "Why you did you said that cats are not included? Batman has a cat?",
+                        "createdAt": "2021-12-27T18:23:59.363Z",
+                        "item": {
+                            "title": "Batman toy, cats are not included",
+                            "description": "Dieta a base de platanos",
+                            "price": 1872,
+                            "stock": 21
+                        },
+                        "answer": null
                     },
-                    "answer": null
-                },
-                {
-                    "id": "4",
-                    "content": "Do you have one of Catwoman?",
-                    "createdAt": "2021-12-27T18:20:42.255Z",
-                    "item": {
-                        "title": "Batman toy, cats are not included",
-                        "description": "DC character",
-                        "price": 1872,
-                        "stock": 21
+                    {
+                        "id": "4",
+                        "content": "Do you have one of Catwoman?",
+                        "createdAt": "2021-12-27T18:20:42.255Z",
+                        "item": {
+                            "title": "Batman toy, cats are not included",
+                            "description": "DC character",
+                            "price": 1872,
+                            "stock": 21
+                        },
+                        "answer": {
+                            "content": "Search my others publiCATions, maybe in cat toys CATegory"
+                        }
                     },
-                    "answer": {
-                        "content": "Search my others publiCATions, maybe in cat toys CATegory"
-                    }
-                },
-            ]
+                ]
+            )
         })
     }
 
@@ -154,5 +160,120 @@ describe('QuestionController test', () => {
                 expect(err).toEqual(new NotFoundException())
             }
         })
+    })
+
+    describe('find method', () => {
+        it('should get all questions answerd of publication with item id 1', async () => {
+            expect(await controllerQuestions.find(1, 1, 1)).toEqual(
+                [
+                    {
+                        "content": "Cats, cats, cats... Can u see them?",
+                        "answer":
+                        {
+                            "content": "They are everywhere!",
+                            "createdAt": "2021-12-27T20:06:45.424Z"
+                        }
+                    },
+                    {
+                        "content": "THE CAT IS UNDER THE TABLE!?",
+                        "answer":
+                        {
+                            "content": "At the moment, yes...",
+                            "createdAt": "2021-12-27T20:06:53.314Z"
+                        }
+                    },
+                ]
+            )
+            expect(mockQuestionsService.paginateBy).toHaveBeenCalled()
+        })
+        it('should return null', async () => {
+            jest.spyOn(mockQuestionsService, 'paginateBy')
+                .mockImplementationOnce(() => {
+                    return null;
+                })
+            expect(await controllerQuestions.find(1, 1, 10)).toEqual(null)
+        })
+
+    })
+
+    describe('findRecived method', () => {
+        it('should get all questions that user with id 2 recived', async () => {
+            expect(await controllerQuestions.findRecived(1, 10)).toEqual(
+                [
+                    {
+                        "content": "Soft kitty, little kitty song?",
+                        "createdAt": "2021-12-27T18:19:14.893Z",
+                        "user": {
+                            "username": "CatOwner1",
+                        },
+                        "item": {
+                            "title": "Balls of wool",
+                        }
+                    },
+                    {
+                        "content": "Are you allergic to cats?",
+                        "createdAt": "2021-12-27T18:23:59.363Z",
+                        "user": {
+                            "username": "CatOwner2",
+                        },
+                        "item": {
+                            "title": "Candies for your cats :3",
+                        }
+                    },
+                ]
+            )
+            expect(mockQuestionsService.paginateBy).toHaveBeenCalled()
+        })
+        it('should return null', async () => {
+            jest.spyOn(mockQuestionsService, 'paginateRecived')
+                .mockImplementationOnce(() => {
+                    return null;
+                })
+            expect(await controllerQuestions.findRecived(1, 10)).toEqual(null)
+        })
+
+    })
+    describe('findSent method', () => {
+        it('should get all questions wrote by the user', async () => {
+            expect(await controllerQuestions.findSent(1, 10)).toEqual(
+                [
+                    {
+                        "id": "14",
+                        "content": "Why you did you said that cats are not included? Batman has a cat?",
+                        "createdAt": "2021-12-27T18:23:59.363Z",
+                        "item": {
+                            "title": "Batman toy, cats are not included",
+                            "description": "Dieta a base de platanos",
+                            "price": 1872,
+                            "stock": 21
+                        },
+                        "answer": null
+                    },
+                    {
+                        "id": "4",
+                        "content": "Do you have one of Catwoman?",
+                        "createdAt": "2021-12-27T18:20:42.255Z",
+                        "item": {
+                            "title": "Batman toy, cats are not included",
+                            "description": "DC character",
+                            "price": 1872,
+                            "stock": 21
+                        },
+                        "answer": {
+                            "content": "Search my others publiCATions, maybe in cat toys CATegory"
+                        }
+                    },
+                ]
+            )
+            expect(mockQuestionsService.paginateBy).toHaveBeenCalled()
+        })
+        it('should return null', async () => {
+            jest.spyOn(mockQuestionsService, 'paginateSent')
+                .mockImplementationOnce(() => {
+                    return null;
+                })
+            expect(await controllerQuestions.findSent(1, 10)).toEqual(null)
+        })
+
     })
 });
