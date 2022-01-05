d15a6625fe22849e9a769c7cad03eb3dd4267da9
ghnoob <ptnchk.rodrigo@gmail.com>
2022-01-05 11:14:25 -0300

    import casl module into items module

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CaslModule } from '../auth/casl/casl.module';
 import { ItemsController } from './controllers/items.controller';
 import { Item } from './entities/items.entity';
 import { ItemsService } from './services/items.service';
@@ -7,8 +8,6 @@ import { ItemsService } from './services/items.service';
 @Module({
   controllers: [ItemsController],
   providers: [ItemsService],
-  imports: [
-    TypeOrmModule.forFeature([Item])
-  ]
+  imports: [TypeOrmModule.forFeature([Item]), CaslModule],
 })
 export class ItemsModule { }
