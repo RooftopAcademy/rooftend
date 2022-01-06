72f3b516df6e8cc81c3091050f11d82c787438b2
Pablo Canale <pablocanale.exa@gmail.com>
2022-01-06 08:01:46 -0300

    create method changed, using QuestionsService istead of inyect a questionsRepository

---
+++ src/questions/services/answers.service.ts
@@ -0,0 +1,47 @@
+import {
+    Injectable,
+    NotFoundException,
+    UnprocessableEntityException
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Question } from '../entities/question.entity';
+import STATUS from '../../statusCodes/statusCodes';
+import Status from '../../statusCodes/status.interface'
+import { AnswerDTO } from '../entities/answer.dto';
+import { Answer } from '../entities/answer.entity';
+import { QuestionsService } from './questions.service';
+
+@Injectable()
+export class AnswersService {
+    constructor(
+        @InjectRepository(Answer) private answersRepository: Repository<Answer>,
+        private questionsService: QuestionsService
+    ) { }
+
+    async create(answer: AnswerDTO, questionId: number) {
+        try {
+
+            await this.questionsService.findUnanswered(questionId)
+            const partialAnswer = this.answersRepository.create({ ...answer, 'createdAt': new Date() });
+            const answerEntity = await this.answersRepository.save(partialAnswer);
+            await this.questionsService.addAnswer(questionId, answerEntity.id)
+            return STATUS.CREATED
+
+        }
+        catch (err) {
+            throw new UnprocessableEntityException();
+        }
+    }
+
+    async delete(id: number): Promise<Status> {
+        try {
+            await this.answersRepository.softDelete(id);
+            return STATUS.DELETED
+        }
+        catch (err) {
+            throw new NotFoundException()
+        }
+    }
+
+}
