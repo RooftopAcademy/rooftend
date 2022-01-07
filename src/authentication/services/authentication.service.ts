51e646c15401347944a94bafee5e48182b320453
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-07 20:12:14 -0300

    Add features to authentication service

--- src/authentication/services/authentication.service.ts
+++ src/authentication/services/authentication.service.ts
@@ -2,14 +2,17 @@ import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { UserService } from '../../users/services/user.service';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDTO } from '../../users/entities/create-user-dto.entity';
-import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async checkEmail(user: CreateUserDTO) {
-    const userExists = await this.userService.findOneByEmail(
+    const userExists = await this.usersService.findOneByEmail(
       user.email.toLowerCase(),
     );
 
@@ -24,6 +27,26 @@ export class AuthenticationService {
   async create(user: CreateUserDTO) {
     user.email = user.email.toLowerCase();
     user.password = await bcrypt.hash(user.password, 10);
-    await this.userService.create(user);
+    await this.usersService.create(user);
+  }
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOneByEmail(email.toLowerCase());
+
+    const passwordMatch = await bcrypt.compare(pass, user.password);
+
+    if (user && passwordMatch) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, id: user.id };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
