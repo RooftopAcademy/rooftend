bbd03ed5d2c0ba215d1ebc496551ffbd78939714
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-07 20:12:26 -0300

    Create passport strategies

---
+++ src/authentication/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from '../constants/constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.id, email: payload.email };
+  }
+}
