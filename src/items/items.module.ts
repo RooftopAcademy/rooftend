3c5a74ce74f13f5793345b873c807d739f09148f
ghnoob <ptnchk.rodrigo@gmail.com>
2022-01-06 22:56:55 -0300

    restore items module

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CaslModule } from '../auth/casl/casl.module';
 import { ItemsController } from './controllers/items.controller';
 import { Item } from './entities/items.entity';
 import { ItemsService } from './services/items.service';
@@ -8,6 +7,8 @@ import { ItemsService } from './services/items.service';
 @Module({
   controllers: [ItemsController],
   providers: [ItemsService],
-  imports: [TypeOrmModule.forFeature([Item])],
+  imports: [
+    TypeOrmModule.forFeature([Item])
+  ]
 })
 export class ItemsModule { }
