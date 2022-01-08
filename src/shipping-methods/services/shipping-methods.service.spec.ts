e6b0987ea991e5fac520e7e31ce4caedfd441d1b
ChachoPacho <bordonnet@hotmail.com>
2022-01-08 11:26:33 -0300

    Tests created

--- src/shipping-methods/services/shipping-methods.service.spec.ts
+++ src/shipping-methods/services/shipping-methods.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ShippingMethod } from '../entities/shipping-method.entity';
 import { ShippingMethodsService } from './shipping-methods.service';
 
 describe('ShippingMethodsService', () => {
   let service: ShippingMethodsService;
 
+  const id = 1;
+  const mockShippingMethodsRepository = {
+    findAll: jest.fn().mockImplementation(() => Promise.resolve([{
+      id: 3,
+      name: "Name",
+      photoId: 1,
+    },])),
+    findOne: jest.fn().mockImplementation((id) => Promise.resolve({
+      id,
+      name: "Name",
+      photoId: 1,
+    })),
+    getCount: jest.fn().mockImplementation(() => Promise.resolve(1)),
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ShippingMethodsService],
+      providers: [ShippingMethodsService, {
+        provide: getRepositoryToken(ShippingMethod),
+        useValue: mockShippingMethodsRepository
+      }],
     }).compile();
 
     service = module.get<ShippingMethodsService>(ShippingMethodsService);
@@ -14,5 +34,27 @@ describe('ShippingMethodsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return an array of Shipping Methods', async () => {
+    expect(await service.findAll()).toEqual(expect.arrayContaining([
+      {
+        id: expect.any(Number),
+        name: expect.any(String),
+        photoId: expect.any(Number),
+      },
+    ]));
+  });
+
+  it('should return a Shipping Method with the given id', async () => {
+    expect(await service.findOne(id)).toEqual({
+      id,
+      name: expect.any(String),
+      photoId: expect.any(Number),
+    })
+  });
+
+  it('should return the quantity of existing Shipping Methods', async () => {
+    expect(await service.getCount()).toEqual(expect.any(Number))
   });
 });
