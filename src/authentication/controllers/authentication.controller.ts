08cd139b10f4f1b9d3d6fb690dbdef4c4c4b6bf1
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-09 17:55:53 -0300

    Login and register logic for authentication

--- src/authentication/controllers/authentication.controller.ts
+++ src/authentication/controllers/authentication.controller.ts
@@ -1,49 +1,38 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Req,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthenticationService } from '../services/authentication.service';
 import { CreateUserDTO } from '../../users/entities/create-user-dto.entity';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
-import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { Public } from '../decorators/public.decorator';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private authService: AuthenticationService) {}
 
+  @Public()
   @Post('register')
   async register(@Body() user: CreateUserDTO) {
-    this.authService.checkEmail(user);
+    await this.authService.checkEmail(user);
 
+    await this.authService.checkPass(user.password);
     //verificar max, min caracteres y alfanumerico y simbolos (regExp)
 
-    //en las columnas de las tablas se permiten nulos????
-
-    this.authService.create(user);
+    await this.authService.create(user);
 
     //crear evento para crear carrito
 
-    //implementar passport y devolver token
-
-    //id, mail, password
-
-    return 'retornar token';
+    return this.authService.login(user);
   }
 
   @UseGuards(LocalAuthGuard)
+  @Public()
   @Post('login')
   async login(@Req() req) {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Get('profile')
+  // getProfile(@Request() req) {
+  //   return req.user;
+  // }
 }
