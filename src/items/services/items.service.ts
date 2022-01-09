8e3705538bb6d926bcd6182f45ba7a8c403a808d
ChachoPacho <bordonnet@hotmail.com>
2022-01-09 11:47:18 -0300

    Permissions Implemented; Item DTO

--- src/items/services/items.service.ts
+++ src/items/services/items.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Item } from '../entities/items.entity';
 import { Repository } from 'typeorm';
+
+import { Item } from '../entities/items.entity';
+import { User } from '../../users/entities/user.entity';
+
+import { CreateItemDTO } from '../entities/create.item.dto';
+
+import { CaslAbilityFactory } from '../../auth/casl/casl-ability.factory';
+import { Permission } from '../../auth/permission.enum';
+import { ForbiddenError } from '@casl/ability';
+
 
 @Injectable()
 export class ItemsService {
   constructor(
     @InjectRepository(Item)
     private readonly ItemsRepo: Repository<Item>,
+    private readonly caslAbilityFactory: CaslAbilityFactory
   ) { }
 
   findAll(): Promise<Item[]> {
@@ -15,20 +25,36 @@ export class ItemsService {
   }
 
   findOne(id: number): Promise<Item> {
-    return this.ItemsRepo.findOne(id);
+    const item = this.ItemsRepo.findOne(id);
+
+    if (!item) throw new NotFoundException('Item Not Found');
+
+    return item;
   }
 
-  create(body: any): Promise<Item> {
-    return this.ItemsRepo.save(body);
+  create(user: User, body: CreateItemDTO): Promise<Item> {
+    const item = this.ItemsRepo.create(body);
+    item.userId = user.id;
+
+    return this.ItemsRepo.save(item);
   }
 
-  async update(id: number, body: any): Promise<Item> {
+  async update(user: User, id: number, body: any): Promise<Item> {
     const item = await this.findOne(id);
+    const ability = this.caslAbilityFactory.createForUser(user);
+
+    ForbiddenError.from(ability).throwUnlessCan(Permission.Update, Object.assign(new Item(), item));
+
     this.ItemsRepo.merge(item, body);
     return this.ItemsRepo.save(item);
   }
 
-  async delete(id: number): Promise<boolean> {
+  async delete(user: User, id: number): Promise<boolean> {
+    const item = await this.findOne(id);
+    const ability = this.caslAbilityFactory.createForUser(user);
+
+    ForbiddenError.from(ability).throwUnlessCan(Permission.Delete, Object.assign(new Item(), item));
+
     await this.ItemsRepo.delete(id);
     return true;
   }
