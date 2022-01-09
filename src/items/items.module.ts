484e4ce9a31518722910ad2743e7d082e78b8a28
ChachoPacho <bordonnet@hotmail.com>
2022-01-09 11:47:45 -0300

    CASL imported

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CaslModule } from '../auth/casl/casl.module';
 import { ItemsController } from './controllers/items.controller';
 import { Item } from './entities/items.entity';
 import { ItemsService } from './services/items.service';
@@ -8,7 +9,8 @@ import { ItemsService } from './services/items.service';
   controllers: [ItemsController],
   providers: [ItemsService],
   imports: [
-    TypeOrmModule.forFeature([Item])
+    TypeOrmModule.forFeature([Item]),
+    CaslModule
   ]
 })
 export class ItemsModule { }
