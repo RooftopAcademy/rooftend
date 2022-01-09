c9145d2d6774df82a110331bec25ee2f0fdf69f2
Sebastian Arroyo <larroyo@fi.uba.ar>
2022-01-09 17:34:02 -0300

    Added getOne and create to cart controller

--- src/cart/controllers/cart.controller.ts
+++ src/cart/controllers/cart.controller.ts
@@ -1,8 +1,11 @@
 import {
+  Body,
   Controller,
   Get,
+  Param,
+  Post,
 } from '@nestjs/common';
-import { ApiForbiddenResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiForbiddenResponse, ApiNotFoundResponse, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Cart } from '../entities/cart.entity';
 import { CartService } from '../services/cart.service';
 
@@ -10,16 +13,40 @@ import { CartService } from '../services/cart.service';
 @Controller('carts')
 export class CartController {
 
-    constructor( private cartService : CartService ){}
+  constructor(private cartService: CartService) { }
 
-    @Get()
-    @ApiOperation({summary: 'Gets all carts'})
-    @ApiResponse({status: 201, description: 'Listing all Carts'})
-    @ApiForbiddenResponse({ status: 403, description: 'Forbidden.'})
-    getAll(): Promise<Cart[]>{
-        return this.cartService.findAll();
-    }
+  @Get()
+  @ApiOperation({ summary: 'Gets all carts' })
+  @ApiResponse({ status: 201, description: 'Listing all Carts' })
+  @ApiForbiddenResponse({ status: 403, description: 'Forbidden.' })
+  getAll(): Promise<Cart[]> {
+    return this.cartService.findAll();
+  }
 
+  @Get(':id')
+  @ApiParam({
+    name: "id",
+    type: "integer",
+    required: true
+  })
+  @ApiOperation({ summary: 'Gets one cart by Id' })
+  @ApiResponse({ status: 201, description: 'Cart succesfully found' })
+  @ApiForbiddenResponse({ status: 403, description: 'Forbidden.' })
+  @ApiNotFoundResponse({ status: 404, description: 'No Cart was found that matches that id' })
+  getOne(@Param('id') id: number): Promise<Cart> {
+    return this.cartService.findOne(id);
+  }
+
+  @ApiBody({
+    type: Cart
+  })
+  @Post()
+  @ApiOperation({ summary: 'Creates cart' })
+  @ApiResponse({ status: 201, description: 'Cart succesfully created' })
+  @ApiForbiddenResponse({ status: 403, description: 'Forbidden.' })
+  create(@Body() body: any): Promise<Cart> {
+    return this.cartService.create(body);
+  }
 
 }
 
