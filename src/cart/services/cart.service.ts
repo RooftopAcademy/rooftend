a086ce7b15be164de61214827d8ceb1d9d8186c4
Sebastian Arroyo <larroyo@fi.uba.ar>
2022-01-09 17:40:57 -0300

    Added findOne and create to cart service

--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -10,10 +10,19 @@ export class CartService {
     constructor(@InjectRepository(Cart) private cartRepo: Repository<Cart>) {
     }
 
-    findAll() : Promise<Cart[]>{
+    findAll(): Promise<Cart[]> {
         return this.cartRepo.find({
-            select: ["id", "amount", "currencyCode"] ,
-            where:{ purchasedAt: null}});
+            select: ["id", "amount", "currencyCode"],
+            where: { purchasedAt: null }
+        });
+    }
+
+    findOne(id: number): Promise<Cart> {
+        return this.cartRepo.findOne(id);
+    }
+
+    create(body: any): Promise<Cart> {
+        return this.cartRepo.save(body);
     }
 
 }
