905230df20e9636c3297b4c7a0afdc49419c5252
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-10 12:51:08 -0300

    Modified Page and Limit parameters an not required

--- src/categories/controllers/categories.controller.ts
+++ src/categories/controllers/categories.controller.ts
@@ -17,20 +17,28 @@ import { CategoriesService } from '../services/categories.service';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { Category } from '../entities/categories.entity';
 import { Response } from 'express';
-import { ApiOperation, ApiTags ,ApiParam, ApiOkResponse,ApiNotFoundResponse, ApiBody, ApiResponse} from '@nestjs/swagger';
-
+import {
+  ApiOperation,
+  ApiTags,
+  ApiParam,
+  ApiOkResponse,
+  ApiNotFoundResponse,
+  ApiBody,
+  ApiResponse,
+  ApiQuery,
+} from '@nestjs/swagger';
 
 @ApiTags('Categories')
 @Controller('categories')
 export class CategoriesController {
   public constructor(private readonly categoriesService: CategoriesService) {}
-    
+
   @Get()
-  @ApiOperation({summary: 'Returns all available categories'})
+  @ApiOperation({ summary: 'Returns all available categories' })
   @ApiOkResponse({
-    type:Category,
+    type: Category,
     description: 'The categories available',
-    isArray: true
+    isArray: true,
   })
   @ApiNotFoundResponse({
     description: 'No categories available',
@@ -38,65 +46,79 @@ export class CategoriesController {
       example: new NotFoundException('No categories available').getResponse(),
     },
   })
-    async index(
-      @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
-      @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
-    ): Promise <Pagination<Category>>{
-      limit = limit > 100 ? 100 : limit;
-      return this.categoriesService.paginate({
-        page,
-        limit,
-        route: '/categories',
-      });
-    }
-  
-    @Get(':id')
-    @HttpCode(200)
-    @ApiOperation({summary: ' Gets one category by id'})
-    @ApiParam({ name: 'id', type: Number, required: true, example: 1 })
-    @ApiOkResponse({
-      type: Category,
-      description: 'The category found.',
-    })
-    @ApiNotFoundResponse({
-      description: 'Category not found.',
-      schema: {
-        example: new NotFoundException('Category not found').getResponse(),
-      },
-    })
-    async findOne(@Param('id') id:number, @Res() res:Response){
-      const data = await this.categoriesService.findOne(id);
-      if (data) return res.status(200).send(data).end();
-      res.status(404).end('Status Not Found')
-    }
-  
-    @Post()
-    @ApiResponse({
-      status: 403,
-      description:'Forbidden'
-    })
-    @HttpCode(403)
-    create(@Res() res:Response) {
-        res.status(403).end();
-    }
+  @ApiQuery({
+    name: 'page',
+    type: Number,
+    required: false,
+    description: 'Current page number',
+    example: 1,
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: Number,
+    required: false,
+    description: 'Limit of paginated categories',
+    example: 10,
+  })
+  async index(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+  ): Promise<Pagination<Category>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.categoriesService.paginate({
+      page,
+      limit,
+      route: '/categories',
+    });
+  }
 
-    @Patch()
-    @ApiResponse({
-      status: 403,
-      description:'Forbidden'
-    })
-    @HttpCode(403)
-    update(@Res() res: Response) {
-        res.status(403).end();
-    }
-    
-    @Delete()
-    @ApiResponse({
-      status: 403,
-      description:'Forbidden'
-    })
-    @HttpCode(403)
-    delete(@Res() res: Response) {
-        res.status(403).end();
-    }
+  @Get(':id')
+  @HttpCode(200)
+  @ApiOperation({ summary: ' Gets one category by id' })
+  @ApiParam({ name: 'id', type: Number, required: true, example: 1 })
+  @ApiOkResponse({
+    type: Category,
+    description: 'The category found.',
+  })
+  @ApiNotFoundResponse({
+    description: 'Category not found.',
+    schema: {
+      example: new NotFoundException('Category not found').getResponse(),
+    },
+  })
+  async findOne(@Param('id') id: number, @Res() res: Response) {
+    const data = await this.categoriesService.findOne(id);
+    if (data) return res.status(200).send(data).end();
+    res.status(404).end('Status Not Found');
+  }
+
+  @Post()
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @HttpCode(403)
+  create(@Res() res: Response) {
+    res.status(403).end();
+  }
+
+  @Patch()
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @HttpCode(403)
+  update(@Res() res: Response) {
+    res.status(403).end();
+  }
+
+  @Delete()
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @HttpCode(403)
+  delete(@Res() res: Response) {
+    res.status(403).end();
+  }
 }
