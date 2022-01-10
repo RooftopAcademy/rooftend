e0c0a70e524b1778824df69f3778ecd5cf2799aa
pabloroque1 <pabloroque1995@gmail.com>
2022-01-10 09:52:25 -0300

    Creation methods HTTP and documentation in controller

--- src/addresses/controllers/addresses.controller.ts
+++ src/addresses/controllers/addresses.controller.ts
@@ -1,46 +1,70 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
   HttpCode,
   Param,
   Patch,
   Post,
+  UnprocessableEntityException
 } from '@nestjs/common';
+import { ApiParam, ApiTags, ApiOperation, ApiResponse, ApiBadRequestResponse } from '@nestjs/swagger';
 import { AddressesService } from '../services/addresses.service';
 
+@ApiTags('Addresses')
 @Controller('adresses')
 export class AddressesController {
-  constructor(private readonly addressesService: AddressesService) {}
+  constructor(private readonly addressesService: AddressesService) { }
 
-  @Get()
-  @HttpCode(200)
-  public findAll() {
-    this.addressesService.findAll();
-  }
-
+  @ApiOperation({ summary: 'Get address by Id' })
   @Get(':id')
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    required: true,
+    description: 'Id of address',
+    example: 1
+  })
   @HttpCode(200)
-  public find(@Param('id') id) {
-    return this.addressesService.find(id);
+  findOne(@Param('id') id: number) {
+    return this.addressesService.findOne(id);
   }
 
-  @Patch()
-  @HttpCode(200)
-  public edit(@Param('id') id: string) {
-    return this.addressesService.edit(id);
-  }
-
-  @Delete('id')
-  @HttpCode(203)
-  public delete(@Param('id') id: string) {
-    return this.addressesService.delete(id);
+  @Patch(':id')
+  @HttpCode(204)
+  @ApiResponse({
+    status: 204,
+    description: 'Address updated succesfully',
+  })
+  @ApiBadRequestResponse({
+    description: 'The address could not be updated',
+  })
+  @ApiParam({
+    name: 'address_id',
+    type: Number,
+    required: true,
+    description: 'Id of address',
+    example: 1
+  })
+  async edit(@Param('id') id: number, @Body() body) {
+    return await this.addressesService.update(id,body);
   }
 
   @Post()
   @HttpCode(201)
-  public created(@Body() body) {
-    return this.addressesService.create(body);
+  @ApiOperation({ summary: 'Create an Address' })
+  @ApiResponse({
+    status: 201,
+    description: 'The Address has been created successfully.',
+  })
+  @ApiBadRequestResponse({
+    description: 'The address could not be created',
+  })
+  async created(@Body() body) {
+    try {
+      return await this.addressesService.create(body)
+    } catch (error) {
+      throw new UnprocessableEntityException(error)
+    }
   }
 }
