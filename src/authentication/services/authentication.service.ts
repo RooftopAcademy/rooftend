d27f129c8c6b914a6de035f3f8be722d2810b442
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-10 17:00:52 -0300

    Correct a typo

--- src/authentication/services/authentication.service.ts
+++ src/authentication/services/authentication.service.ts
@@ -75,7 +75,7 @@ export class AuthenticationService {
     const payload = { email: user.email, id: user.id };
 
     return {
-      access_token: this.jwtService.sign(payload),
+      accessToken: this.jwtService.sign(payload),
     };
   }
 }
