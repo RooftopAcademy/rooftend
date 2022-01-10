e7cb77e8280dc707192537dedb57d96ffcbf2091
pabloroque1 <pabloroque1995@gmail.com>
2022-01-10 09:30:50 -0300

    Create services methods create-find-update

--- src/addresses/services/addresses.service.ts
+++ src/addresses/services/addresses.service.ts
@@ -1,24 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Address } from '../entities/address.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { getConnection, Repository } from 'typeorm';
 
 @Injectable()
 export class AddressesService {
-  findAll(): [] {
-    return [];
+
+  constructor(@InjectRepository(Address) private adressRepo: Repository<Address>) {
+
   }
 
-  public find(id: string): string {
-    return `item ${id} found`;
+  async findOne(idAddress: number): Promise<Address> {
+    const address = getConnection().createQueryBuilder()
+      .select(["address.country_code", "address.country_state",
+        "address.city_name", "address.street_name", "address.street_number",
+        "address.zip_code", "address.floor", "address.office"])
+      .from(Address, "address")
+      .where("address.id = :id", { id: idAddress });
+
+    return address[0];
   }
 
-  edit(id: string): string {
-    return `item ${id} edited`;
+  async update(id: number, body: any): Promise<Address> {
+    const address = await this.adressRepo.findOne(id);
+    this.adressRepo.merge(address, body);
+    return this.adressRepo.save(address);
   }
 
-  delete(id: string): string {
-    return `item ${id} removed`;
-  }
-
-  create(body: string): string {
-    return 'item created';
-  }
+  create(body: any) {
+    const newAddress = this.adressRepo.create(body);
+    return this.adressRepo.save(newAddress);
+}
 }
