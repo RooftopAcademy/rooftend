2afbe715ef7657ffe5d4990cc8eacf3b9b517942
Pablo Canale <pablocanale.exa@gmail.com>
2022-01-11 11:58:12 -0300

    add nulleable option in swagger doc

--- src/platform/entities/platform.entity.ts
+++ src/platform/entities/platform.entity.ts
@@ -30,11 +30,13 @@ export class Platform {
     type: 'date',
     format: 'date-time',
     example: Date.now(),
+    nullable: false,
   })
   @CreateDateColumn({
     name: 'created_at',
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
+    nullable: false,
   })
   createdAt: Date;
 
@@ -44,11 +46,13 @@ export class Platform {
     type: 'date',
     format: 'date-time',
     example: Date.now(),
+    nullable: false,
   })
   @UpdateDateColumn({
     name: 'updated_at',
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
+    nullable: false,
   })
   updatedAt: Date;
 
@@ -58,22 +62,27 @@ export class Platform {
     type: 'date',
     format: 'date-time',
     example: '2021-12-16',
+    nullable: true,
   })
   @DeleteDateColumn({
     name: 'deleted_at',
     type: 'timestamptz',
     default: null,
+    nullable: true,
   })
   deletedAt?: Date;
   @ApiProperty({
     description: '3 digits ISO country code (Example: ARG)',
     type: 'string',
     example: 'ARG',
+    nullable: false,
+    maxLength: 3,
   })
   @Column({
     name: 'country_code',
     type: 'char',
     length: 3,
+    nullable: false,
   })
   countryCode: string;
 
@@ -81,11 +90,14 @@ export class Platform {
     description: '3 digits ISO currency code',
     type: 'string',
     example: 'ARG',
+    maxLength: 3,
+    nullable: false,
   })
   @Column({
     name: 'currency_code',
     type: 'char',
     length: 3,
+    nullable: false,
   })
   currencyCode: string;
 
@@ -93,11 +105,14 @@ export class Platform {
     description: '5 digits ISO language code',
     type: 'string',
     example: 'es_AR',
+    nullable: false,
+    maxLength: 5,
   })
   @Column({
     name: 'lang_code',
     type: 'char',
     length: 5,
+    nullable: false,
   })
   langCode: string;
 
@@ -105,11 +120,14 @@ export class Platform {
     description: 'Up to 5 digits phone code',
     type: 'string',
     example: '++549',
+    nullable: false,
+    maxLength: 5,
   })
   @Column({
     name: 'phone_country_code',
     type: 'char',
     length: 5,
+    nullable: false,
   })
   phoneCountryCode: string;
 }
