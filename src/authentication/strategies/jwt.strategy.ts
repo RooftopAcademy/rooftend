cef0aeb5559f4ce29f5bc0733b1a0935ce2f15af
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-11 16:27:27 -0300

    Refactor jwtStrategy to pass an entire user to req.user (except password)

--- src/authentication/strategies/jwt.strategy.ts
+++ src/authentication/strategies/jwt.strategy.ts
@@ -14,6 +14,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { id: payload.id, email: payload.email };
+    const { iat, exp, ...result } = payload;
+
+    return { result };
   }
 }
