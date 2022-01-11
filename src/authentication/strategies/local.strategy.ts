dc5781900efc2690791be37eae2bc9f36301d934
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-11 18:07:11 -0300

    Separate pass and email validation

--- src/authentication/strategies/local.strategy.ts
+++ src/authentication/strategies/local.strategy.ts
@@ -10,10 +10,10 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(email: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(email, password);
-    if (!user) {
-      throw new UnauthorizedException();
-    }
+    const user = await this.authService.validateUser(email);
+
+    await this.authService.validatePassword(user, password);
+
     return user;
   }
 }
