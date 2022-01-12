3bd3f972ba72292ef08a883eba390402b628121b
Ayrton <ayrtoncravero26@gmail.com>
2022-01-12 01:14:25 -0300

    adding 'caslModule' in the 'historyModule'

--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -3,10 +3,11 @@ import { HistoryController } from './controllers/history/history.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { History } from './models/history.entity';
 import { HistoryService } from './services/history/history.service';
+import { CaslModule } from '../auth/casl/casl.module';
 
 @Module({
   providers: [HistoryService],
   controllers: [HistoryController],
-  imports: [TypeOrmModule.forFeature([History])],
+  imports: [TypeOrmModule.forFeature([History]), CaslModule],
 })
 export class HistoryModule {}
