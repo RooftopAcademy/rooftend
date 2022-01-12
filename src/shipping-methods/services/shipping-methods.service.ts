58b8bda77e2c2efc5212ce567a7f9ad73344fda8
gbangertes <gbangertes@gmail.com>
2022-01-12 10:17:24 -0300

    Removed unnecessary methods in service.

--- src/shipping-methods/services/shipping-methods.service.ts
+++ src/shipping-methods/services/shipping-methods.service.ts
@@ -11,53 +11,10 @@ export class ShippingMethodsService {
   ) { }
 
   findAll() {
-    //return this.shippingMethodsRepo.find();
-    return [
-      {
-        id: 1,
-        name: '',
-        photoId: 1,
-      },
-      {
-        id: 2,
-        name: '',
-        photoId: 2,
-      },
-    ];
+    return this.shippingMethodsRepo.find();
   }
 
   findOne(id: number) {
-    //return this.shippingMethodsRepo.findOne(id);
-    return {
-      id: id,
-      name: '',
-      photoId: 1,
-    };
-  }
-
-  getCount() {
-    //return this.shippingMethodsRepo.query("SELECT COUNT(*) FROM shipping_methods");
-    return 10;
-  }
-
-  create(body: any) {
-    // const newShippingMethod = new ShippingMethod();
-    // newShippingMethod.name = body.name;
-    // newShippingMethod.photoId = body.photoId;
-
-    // return this.shippingMethodsRepo.save(newShippingMethod);
-    return 'Método de Envío Creado';
-  }
-
-  async update(id: number, body: any) {
-    //const shippingMethod = await this.findOne(id);
-    //this.shippingMethodsRepo.merge(shippingMethod, body);
-    //return this.shippingMethodsRepo.save(shippingMethod);
-    return `Se Actualizó el Método de Envío ${id}`;
-  }
-
-  async delete(id: number) {
-    //await this.shippingMethodsRepo.delete(id);
-    return 'Eliminado';
+    return this.shippingMethodsRepo.findOne(id);
   }
 }
