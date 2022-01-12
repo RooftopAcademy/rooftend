bd4ad5a8243faa2651a57b29fdc1099a2ebcc6ea
Ayrton <ayrtoncravero26@gmail.com>
2022-01-12 15:58:40 -0300

    adding permisions of 'History'

--- src/auth/casl/casl-ability.factory.ts
+++ src/auth/casl/casl-ability.factory.ts
@@ -10,9 +10,10 @@ import { Permission } from '../enums/permission.enum';
 import { User } from '../../users/entities/user.entity';
 import { Item } from '../../items/entities/items.entity';
 import { FlatClass } from '../types/flat-class.type';
+import { History } from '../../history/models/history.entity';
 
 // TODO: add classes to InferSubjects -> InferSubjects<typeof Item | typeof Review ...>
-type Subjects = InferSubjects<typeof Item> | 'all';
+type Subjects = InferSubjects<typeof Item | typeof History> | 'all';
 
 export type AppAbility = Ability<[Permission, Subjects]>;
 
@@ -26,6 +27,9 @@ export class CaslAbilityFactory {
     // can<FlatClass<[CLASE]>>(Permission[PERMISO], [CLASE], { 'user.id': user.id });
     can([Permission.Create, Permission.Read], Item);
     can<FlatClass<Item>>([Permission.Delete, Permission.Update], Item, {
+      'user.id': user.id,
+    });
+    can<FlatClass<History>>([Permission.Read, Permission.Delete], History, { 
       'user.id': user.id,
     });
 
