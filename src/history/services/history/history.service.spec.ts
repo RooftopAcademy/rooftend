43907a5f477bbf4f56a5e10a4120d03d0e40154d
Ayrton <ayrtoncravero26@gmail.com>
2022-01-12 01:17:01 -0300

    changes were added and all unnecessary methods / imports were removed

--- src/history/services/history/history.service.spec.ts
+++ src/history/services/history/history.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { History } from '../../models/history.entity';
 import { HistoryService } from './history.service';
 
 describe('HistoryService', () => {
   let service: HistoryService;
 
+  let user = {
+    id: 1,
+    user_id: 1,
+    createAt: Date.now(),
+  };
+
+  const mockHistoryRepository = {
+    find: jest.fn(() => Promise.resolve([
+      {
+        id: 1,
+        user_id: 1,
+        createdAt: new Date(),
+      },
+    ]),
+    ),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HistoryService],
+      providers: [
+        HistoryService,
+        {
+          provide: getRepositoryToken(History),
+          useValue: mockHistoryRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<HistoryService>(HistoryService);
@@ -15,4 +40,10 @@ describe('HistoryService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+    it('should return a list of history', async () => {
+      expect(await service.getAll(user))
+    });
+  })
 });
