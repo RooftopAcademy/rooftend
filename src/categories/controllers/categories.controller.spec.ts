cdbe109278238a8f649a8c7c2f2270f129d0bd7f
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-12 00:16:23 -0300

    Added controller test

--- src/categories/controllers/categories.controller.spec.ts
+++ src/categories/controllers/categories.controller.spec.ts
@@ -1,18 +1,137 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CategoriesService } from '../services/categories.service';
 import { CategoriesController } from './categories.controller';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
+  const mockCategoriesService = {
+    paginate: jest.fn().mockImplementation(() => {
+      return [
+        {
+          items: [
+            {
+              id: 1,
+              name: 'TECHNOLOGY',
+              subCategories: [
+                {
+                  id: 12,
+                  name: 'CELL PHONES AND TELEPHONES',
+                },
+                {
+                  id: 13,
+                  name: 'COMPUTING',
+                },
+              ],
+            },
+            {
+              id: 2,
+              name: 'REAL STATE',
+              subCategories: [
+                {
+                  id: 14,
+                  name: 'DEPARTMENTS',
+                },
+                {
+                  id: 15,
+                  name: 'HOUSES',
+                },
+              ],
+            },
+          ],
+        },
+      ];
+    }),
+
+    findOneById: jest.fn().mockImplementation((id) => {
+      return {
+        id,
+        name: 'TECHNOLOGY',
+        subCategories: [
+          {
+            id: 12,
+            name: 'CELL PHONES AND TELEPHONES',
+          },
+          {
+            id: 13,
+            name: 'COMPUTING',
+          },
+        ],
+      };
+    }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoriesController],
-    }).compile();
+      providers: [CategoriesService],
+    })
+      .overrideProvider(CategoriesService)
+      .useValue(mockCategoriesService)
+      .compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('paginate', () => {
+    it('should return a list of categories', () => {
+      expect(controller.index()).toEqual([
+        {
+          items: [
+            {
+              id: 1,
+              name: 'TECHNOLOGY',
+              subCategories: [
+                {
+                  id: 12,
+                  name: 'CELL PHONES AND TELEPHONES',
+                },
+                {
+                  id: 13,
+                  name: 'COMPUTING',
+                },
+              ],
+            },
+            {
+              id: 2,
+              name: 'REAL STATE',
+              subCategories: [
+                {
+                  id: 14,
+                  name: 'DEPARTMENTS',
+                },
+                {
+                  id: 15,
+                  name: 'HOUSES',
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+    });
+  });
+  describe('findOne', () => {
+    it('should return a category by its id', () => {
+      expect(controller.findOne(1)).toEqual({
+        id: 1,
+        name: 'TECHNOLOGY',
+        subCategories: [
+          {
+            id: 12,
+            name: 'CELL PHONES AND TELEPHONES',
+          },
+          {
+            id: 13,
+            name: 'COMPUTING',
+          },
+        ],
+      });
+      expect(mockCategoriesService.findOneById).toHaveBeenCalled();
+      expect(mockCategoriesService.findOneById).toHaveBeenCalledWith(1);
+    });
   });
 });
--- src/categories/services/categories.service.ts
+++ src/categories/services/categories.service.ts
@@ -7,8 +7,6 @@ import {
   IPaginationOptions,
 } from 'nestjs-typeorm-paginate';
 import { Category } from '../entities/categories.entity';
-import { isNull } from 'util';
-
 @Injectable()
 export class CategoriesService {
   public constructor(
@@ -16,7 +14,7 @@ export class CategoriesService {
   ) {}
 
   async paginate(options: IPaginationOptions): Promise<Pagination<Category>> {
-    return paginate<Category>(this.repository, options,{
+    return paginate<Category>(this.repository, options, {
       where: {
         parentCategory: { id: IsNull() },
       },
@@ -32,23 +30,5 @@ export class CategoriesService {
       throw new NotFoundException(`Category with id ${id} not found.`);
     }
     return category;
-  }
-  create(body: any): Promise<Category> {
-    const newCategory = this.repository.create({
-      id: body.id,
-      name: body.name,
-    });
-    return this.repository.save(newCategory);
-  }
-
-  async update(id: number, body: any): Promise<Category> {
-    const category = await this.repository.findOne(id);
-    this.repository.merge(category, body);
-    return this.repository.save(category);
-  }
-
-  async delete(id: number): Promise<boolean> {
-    await this.repository.delete(id);
-    return true;
   }
 }
