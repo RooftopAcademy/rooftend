3b91f60bcf401f8159a8527d03dc179a5ba292ef
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-12 16:42:31 -0300

    Removed unnecessary endpoints

--- src/brands/controllers/brands.controllers.ts
+++ src/brands/controllers/brands.controllers.ts
@@ -1,25 +1,14 @@
 import {
-  Body,
   Controller,
   DefaultValuePipe,
-  Delete,
   Get,
-  Param,
   ParseIntPipe,
-  Patch,
-  Post,
   Query,
 } from '@nestjs/common';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { Brand } from '../entities/brands.entity';
 import { BrandsService } from '../services/brands.serveces';
-import { createBrandDTO } from '../entities/create-brands-dto.entity';
-import {
-  ApiBadRequestResponse,
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Brands')
 @Controller('brands')
@@ -34,8 +23,8 @@ export class BrandsController {
   })
   @Get()
   async index(
-    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
-    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
   ): Promise<Pagination<Brand>> {
     limit = limit > 100 ? 100 : limit;
     return this.brandService.paginate({
@@ -43,55 +32,5 @@ export class BrandsController {
       limit,
       route: '/brands',
     });
-  }
-
-  @ApiOperation({ summary: 'Get a brand by id' })
-  @ApiResponse({
-    status: 200,
-    description: 'Shows the search result for a brand by id',
-    type: Brand,
-  })
-  @Get(':id')
-  getOne(@Param('id') id: number) {
-    return this.brandService.findOne(id);
-  }
-
-  @ApiOperation({ summary: 'Create a brands' })
-  @ApiResponse({
-    status: 201,
-    description: 'The brand has been created successfully',
-  })
-  @ApiBadRequestResponse({
-    description: 'The brand could not be created',
-  })
-  @Post()
-  create(@Body() createBrandDTO: createBrandDTO) {
-    return this.brandService.create(createBrandDTO);
-  }
-
-  @ApiOperation({ summary: 'Update a brands' })
-  @ApiResponse({
-    status: 200,
-    description: 'The brand has been updated successfully',
-  })
-  @ApiBadRequestResponse({
-    description: 'The brand could not be updated',
-  })
-  @Patch()
-  update(@Param() id: number, @Body() createBrandDTO: createBrandDTO) {
-    return this.brandService.update(id, createBrandDTO);
-  }
-
-  @ApiOperation({ summary: 'Delete a brands' })
-  @ApiResponse({
-    status: 200,
-    description: 'The brand has been deleted successfully',
-  })
-  @ApiBadRequestResponse({
-    description: 'The brand could not be deleted',
-  })
-  @Delete()
-  delete(@Param('id') id: number) {
-    return this.brandService.delete(id);
   }
 }
