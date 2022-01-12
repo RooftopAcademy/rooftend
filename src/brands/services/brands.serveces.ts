661acfbdd50c2902e50098522dee5887647d7357
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-12 19:44:33 -0300

    Removed unused methods in service

--- src/brands/services/brands.serveces.ts
+++ src/brands/services/brands.serveces.ts
@@ -14,31 +14,6 @@ export class BrandsService {
     @InjectRepository(Brand)
     private brandRepo: Repository<Brand>,
   ) {}
-
-  findOne(id: number) {
-    return this.brandRepo.findOne(id);
-  }
-
-  findAll() {
-    return this.brandRepo.find();
-  }
-
-  create(body: any) {
-    const newBrand = this.brandRepo.create(body);
-    return this.brandRepo.save(newBrand);
-  }
-
-  async update(id: number, body: any) {
-    const brand = await this.brandRepo.findOne(id);
-    this.brandRepo.merge(brand, body);
-    return this.brandRepo.save(brand);
-  }
-
-  async delete(id: number) {
-    await this.brandRepo.delete(id);
-    return true;
-  }
-
   async paginate(options: IPaginationOptions): Promise<Pagination<Brand>> {
     return paginate<Brand>(this.brandRepo, options);
   }
