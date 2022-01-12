16921d9f37822b9a213d67861222cc435b09c61b
ChachoPacho <bordonnet@hotmail.com>
2022-01-12 15:26:32 -0300

    CustomMesage Permissions

--- src/auth/casl/casl-ability.factory.ts
+++ src/auth/casl/casl-ability.factory.ts
@@ -8,6 +8,8 @@ import {
 import { Injectable } from '@nestjs/common';
 import { Permission } from '../permission.enum';
 import { User } from '../../users/entities/user.entity';
+import { FlatClass } from '../types/flat-class.type';
+import { CustomMessage } from '../../custom-messages/entities/custom-messages.entity';
 
 // TODO: replace any with classes
 type Subjects = InferSubjects<any> | 'all';
@@ -21,6 +23,8 @@ export class CaslAbilityFactory {
       Ability<[Permission, Subjects]>
     >(Ability as AbilityClass<AppAbility>);
     // can<FlatClass<[CLASE]>>(Permission[PERMISO], [CLASE], { "user.id": user.id });
+
+    can<FlatClass<CustomMessage>>([Permission.Read, Permission.Delete, Permission.Update], CustomMessage, { "user.id": user.id });
 
     return build({
       // Read https://casl.js.org/v5/en/guide/subject-type-detection#use-classes-as-subject-types for details
