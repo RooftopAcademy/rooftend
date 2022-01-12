c7f0c29c582f286d28f9d3de41b44095b39d0053
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-12 14:43:34 -0300

    Add throttler guard to implement rate limiting

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { ThrottlerGuard } from '@nestjs/throttler';
 import { UsersModule } from '../users/users.module';
 import { jwtConstants } from './constants/constants';
 import { AuthenticationController } from './controllers/authentication.controller';
@@ -26,6 +27,10 @@ import { LocalStrategy } from './strategies/local.strategy';
     {
       provide: 'APP_GUARD',
       useClass: JwtAuthGuard,
+    },
+    {
+      provide: 'APP_GUARD',
+      useClass: ThrottlerGuard,
     },
   ],
 })
