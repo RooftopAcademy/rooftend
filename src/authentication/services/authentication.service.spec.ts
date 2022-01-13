cd378b57d7eeb22699ecac6cb51e5d55dc44fe70
Fabio Martín Trucco <85174494+FabioTrucco@users.noreply.github.com>
2022-01-13 13:12:43 -0300

    minor refactor

--- src/authentication/services/authentication.service.spec.ts
+++ src/authentication/services/authentication.service.spec.ts
@@ -27,6 +27,7 @@ describe('AuthenticationService', () => {
   const user: CreateUserDTO = {
     email: 'userEmail@gmail.com',
     password: 'anyvalidpass123_',
+    passwordConfirmation: 'anyvalidpass123_',
   };
 
   const mockAuthService = {
@@ -62,7 +63,7 @@ describe('AuthenticationService', () => {
         return null;
       }),
     login: jest.fn().mockImplementation((user: CreateUserDTO) => {
-      let foundUser: CreateUserDTO;
+      let foundUser;
       for (let i = 0; i < registeredUsers.length; i++) {
         if (registeredUsers[i].email == user.email) {
           foundUser = registeredUsers[i];
@@ -79,7 +80,7 @@ describe('AuthenticationService', () => {
 
   const mockUserService = {
     findOneByEmail: jest.fn().mockImplementation((email: string) => {
-      let regUser: CreateUserDTO;
+      let regUser;
       for (let i = 0; i < registeredUsers.length; i++) {
         if (registeredUsers[i].email == email) {
           regUser = registeredUsers[i];
@@ -121,29 +122,33 @@ describe('AuthenticationService', () => {
   });
 
   afterEach(jest.clearAllMocks);
+
   describe('checkEmail', () => {
     it('should return void if the email address has not been used', async () => {
       const check = await service.checkEmail(user);
       expect(check).toBeUndefined();
       expect(mockAuthService.checkEmail).toHaveBeenCalledTimes(1);
     });
+
     it('should throw an error if the address is already being used', () => {
       expect(() => {
         service.checkEmail({
           email: 'email1@gmail.com',
           password: 'anyValidPass123:',
+          passwordConfirmation: 'anyValidPass123:',
         });
       }).toThrowError(/The user is already registered/);
       expect(mockAuthService.checkEmail).toHaveBeenCalledTimes(1);
-      // expect(mockAuthService.checkEmail).toHaveBeenCalledWith();
     });
   });
+
   describe('create', () => {
     it('should create a new user', () => {
       const newUser = service.create(user);
       expect(newUser).toBeUndefined;
     });
   });
+
   describe('validate user with email', () => {
     it('should throw an error during validation', async () => {
       expect(() => {
@@ -151,6 +156,7 @@ describe('AuthenticationService', () => {
       }).toThrowError(/User not found/);
       expect(mockAuthService.validateUser).toHaveBeenCalledTimes(1);
     });
+
     it('should validate the user email', () => {
       expect(service.validateUser('email1@gmail.com')).toEqual(
         'email1@gmail.com',
@@ -168,6 +174,7 @@ describe('AuthenticationService', () => {
       );
       expect(mockAuthService.validatePassword).toHaveBeenCalledTimes(1);
     });
+
     it('should return null if the password is wrong', () => {
       const wrongUser: User = new User();
       wrongUser.email = 'email1@gmail.com';
@@ -176,6 +183,7 @@ describe('AuthenticationService', () => {
       expect(mockAuthService.validatePassword).toHaveBeenCalledTimes(1);
     });
   });
+
   describe('login', () => {
     it('should login a user', () => {
       expect(
@@ -188,6 +196,7 @@ describe('AuthenticationService', () => {
       });
       expect(mockAuthService.login).toHaveBeenCalledTimes(1);
     });
+
     it('should throw an error for invalid email or password', () => {
       expect(() => {
         mockAuthService.login({
