edb42d1a14d54fd4ff4a3a69275d38d0f438e300
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-13 18:09:55 -0300

    Added relation in brands entity

--- src/brands/entities/brands.entity.ts
+++ src/brands/entities/brands.entity.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Store } from '../../stores/entities/stores.entity';
 
 @Entity({ name: 'brands' })
 export class Brand {
@@ -7,13 +8,12 @@ export class Brand {
     unsigned: true,
     type: 'bigint',
   })
-  @ApiProperty(
-    {
-      example: 1,
-      description: 'The id of the brand',
-      type: Number,
-      readOnly: true,
-    })
+  @ApiProperty({
+    example: 1,
+    description: 'The id of the brand',
+    type: Number,
+    readOnly: true,
+  })
   id: number;
 
   @Column({
@@ -37,10 +37,13 @@ export class Brand {
     nullable: false,
   })
   @ApiProperty({
-    example: 'https://logos-marcas.com/wp-content/uploads/2020/04/Nike-Logo.png',
+    example:
+      'https://logos-marcas.com/wp-content/uploads/2020/04/Nike-Logo.png',
     description: 'The logo of the brand',
     type: String,
     nullable: false,
   })
   photoUrl: string;
+  @ManyToOne(() => Store, (store) => store.brand)
+  store?: Store;
 }
