1b526479dc193f4bff634cd74cfb9a94ac897f72
ChachoPacho <bordonnet@hotmail.com>
2022-01-14 16:56:00 -0300

    Documentated Entities

--- src/items/entities/create.item.dto.ts
+++ src/items/entities/create.item.dto.ts
@@ -1,11 +1,47 @@
-import { Brand } from '../../brands/entities/brands.entity';
-import { Category } from '../../categories/entities/categories.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateItemDTO {
+  @IsString()
+  @ApiProperty({
+    type: String,
+    description: "Item's title"
+  })
   title: string;
+
+  @IsString()
+  @ApiProperty({
+    type: String,
+    description: "Item's description"
+  })
   description: string;
+
+  @IsNumber()
+  @ApiProperty({
+    type: Number,
+    description: "Item's price"
+  })
   price: number;
+
+  @IsNumber()
+  @ApiProperty({
+    type: Number,
+    description: "Item's current stock"
+  })
   stock: number;
-  brandId: Brand;
-  categoryId: Category;
+
+  @IsNumber()
+  @IsOptional()
+  @ApiProperty({
+    type: Number,
+    description: "Item's brand"
+  })
+  brand?: number;
+
+  @IsNumber()
+  @ApiProperty({
+    type: Number,
+    description: "Item's category"
+  })
+  category: number;
 }
