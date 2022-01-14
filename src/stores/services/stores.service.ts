440c85871c8b28f35656a44f3d010abd04b0343b
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-14 01:11:26 -0300

    Removed unnecessary methods in service

--- src/stores/services/stores.service.ts
+++ src/stores/services/stores.service.ts
@@ -1,25 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, UpdateResult } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Store } from '../entities/stores.entity';
-import { DeleteResult } from 'typeorm/browser';
-import {
-  IPaginationOptions,
-  paginateRaw,
-} from 'nestjs-typeorm-paginate';
+import { IPaginationOptions, paginateRaw } from 'nestjs-typeorm-paginate';
 import { ReadStoreDto } from '../entities/read-store.dto';
-import { CreateStoreDto } from '../entities/create-store.dto';
 
 @Injectable()
 export class StoresService {
   constructor(
     @InjectRepository(Store)
     private readonly storesRepository: Repository<Store>,
-  ) { }
+  ) {}
 
   async paginate(options: IPaginationOptions) {
     return paginateRaw<Store>(
-      this.storesRepository.createQueryBuilder('store')
+      this.storesRepository
+        .createQueryBuilder('store')
         .leftJoinAndSelect('store.user', 'user')
         .leftJoinAndSelect('store.brand', 'brand')
         .select([
@@ -32,28 +28,11 @@ export class StoresService {
   }
 
   async getOne(id: string | number): Promise<ReadStoreDto> {
-    return await this.storesRepository.createQueryBuilder('store')
+    return await this.storesRepository
+      .createQueryBuilder('store')
       .leftJoinAndSelect('store.brand', 'brand')
-      .select([
-        'brand.name AS "brand"',
-      ]).where('store.id = :id', { id })
-      .getRawOne()
-  }
-
-  async create(createStoreDto: CreateStoreDto): Promise<CreateStoreDto> {
-    return this.storesRepository.save(createStoreDto).then((res) => {
-      return {
-        userId: res.userId,
-        brandId: res.brandId,
-      }
-    });
-  }
-
-  async update(id: number, createStoreDto: CreateStoreDto): Promise<UpdateResult> {
-    return this.storesRepository.update(id, createStoreDto);
-  }
-
-  delete(id: number): Promise<DeleteResult> {
-    return this.storesRepository.delete(id);
+      .select(['brand.name AS "brand"'])
+      .where('store.id = :id', { id })
+      .getRawOne();
   }
 }
