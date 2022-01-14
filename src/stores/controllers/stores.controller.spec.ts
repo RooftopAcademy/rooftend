c949efaeba906b1e25d2f1a56040a04412fd4e01
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-14 12:36:45 -0300

    added unit test in controller

--- src/stores/controllers/stores.controller.spec.ts
+++ src/stores/controllers/stores.controller.spec.ts
@@ -1,18 +1,81 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { StoresService } from '../services/stores.service';
 import { StoresController } from './stores.controller';
 
 describe('StoresController', () => {
   let controller: StoresController;
-
+  const mockStoresService = {
+    paginate: jest.fn().mockImplementation(() => {
+      return [
+        {
+          items: [
+            {
+              id: 1,
+              username: 'miUsuario',
+              brand: 'Xiaomi',
+            },
+            {
+              id: 2,
+              username: 'miUsuario',
+              brand: 'Nike',
+            },
+          ],
+        },
+      ];
+    }),
+    getOne: jest.fn().mockImplementation((id) => {
+      return {
+        id: 1,
+        username: 'miUsuario',
+        brand: 'Xiaomi',
+      };
+    }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StoresController],
-    }).compile();
+      providers: [StoresService],
+    })
+      .overrideProvider(StoresService)
+      .useValue(mockStoresService)
+      .compile();
 
     controller = module.get<StoresController>(StoresController);
   });
 
-  it('should be defined', () => {
+  it('Should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('paginate', () => {
+    it('should return a list of stores', async () => {
+      expect(await controller.getAll()).toEqual([
+        {
+          items: [
+            {
+              id: 1,
+              username: 'miUsuario',
+              brand: 'Xiaomi',
+            },
+            {
+              id: 2,
+              username: 'miUsuario',
+              brand: 'Nike',
+            },
+          ],
+        },
+      ]);
+    });
+  });
+  describe('getOne', () => {
+    it('Should return a store by its id', async () => {
+      expect(await controller.getOne(1)).toEqual({
+        id: 1,
+        username: 'miUsuario',
+        brand: 'Xiaomi',
+      });
+      expect(mockStoresService.getOne).toHaveBeenCalled();
+      expect(mockStoresService.getOne).toHaveBeenCalledWith(1);
+    });
   });
 });
