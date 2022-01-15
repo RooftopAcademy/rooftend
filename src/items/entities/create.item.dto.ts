288d05b6c835f601f4793aecf5c489072a817498
ghnoob <ptnchk.rodrigo@gmail.com>
2022-01-14 22:54:05 -0300

    correct create item dto

--- src/items/entities/create.item.dto.ts
+++ src/items/entities/create.item.dto.ts
@@ -1,11 +1,60 @@
-import { Brand } from '../../brands/entities/brands.entity';
-import { Category } from '../../categories/entities/categories.entity';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+  Max,
+  Min,
+} from 'class-validator';
 
-export class CreateItemDTO {
+export class CreateItemDto {
+  @IsString()
+  @Length(1, 100)
+  @ApiProperty({
+    example: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+    description: 'Item title',
+  })
   title: string;
+
+  @IsString()
+  @Length(1, 1000)
+  @ApiProperty({
+    example: 'Imitaciones de varitas de sauco para el mago coleccionista',
+    description: 'Item description',
+  })
   description: string;
+
+  @IsNumber()
+  @Min(0)
+  @Max(100000000)
+  @ApiProperty({
+    example: 1050.99,
+    description: 'Item Price',
+  })
   price: number;
+
+  @IsNumber()
+  @Min(1)
+  @Max(100000000)
+  @ApiProperty({
+    example: 10,
+    description: 'Item Stock',
+  })
   stock: number;
-  brandId: Brand;
-  categoryId: Category;
+
+  @IsNumber()
+  @ApiProperty({
+    description: 'Id of the item category',
+    example: 1,
+  })
+  categoryId: number;
+
+  @IsOptional()
+  @IsNumber()
+  @ApiPropertyOptional({
+    description: 'Id of the item brand',
+    example: 1,
+  })
+  brandId?: number;
 }
