8919dd5e55f5166d5671565733146e879a34626f
ghnoob <ptnchk.rodrigo@gmail.com>
2022-01-15 23:57:43 -0300

    fix item creation

--- src/items/services/items.service.ts
+++ src/items/services/items.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 
 import { InjectRepository } from '@nestjs/typeorm';
 import { Item } from '../entities/items.entity';
@@ -12,12 +16,18 @@ import { User } from '../../users/entities/user.entity';
 import { CreateItemDto } from '../entities/create.item.dto';
 import { ItemSearchOptions } from '../interfaces/item-search-options.interface';
 import { UpdateItemDto } from '../entities/update.item.dto';
+import { Category } from '../../categories/entities/categories.entity';
+import { Brand } from '../../brands/entities/brands.entity';
 
 @Injectable()
 export class ItemsService {
   constructor(
     @InjectRepository(Item)
     private readonly itemsRepo: Repository<Item>,
+    @InjectRepository(Category)
+    private readonly categoriesRepo: Repository<Category>,
+    @InjectRepository(Brand)
+    private readonly brandsRepo: Repository<Brand>,
   ) {}
 
   /**
@@ -78,9 +88,29 @@ export class ItemsService {
     return item;
   }
 
-  create(user: User, body: CreateItemDto): Promise<Item> {
+  async create(user: User, body: CreateItemDto): Promise<Item> {
     const item = this.itemsRepo.create(body);
     item.user = user;
+
+    if (body.brandId) {
+      const brand: Brand = await this.brandsRepo.findOne(body.brandId);
+
+      if (!brand) {
+        throw new UnprocessableEntityException('Brand does not exist');
+      }
+
+      item.brand = brand;
+    }
+
+    const category: Category = await this.categoriesRepo.findOne(
+      body.categoryId,
+    );
+
+    if (!category) {
+      throw new UnprocessableEntityException('Category does not exist');
+    }
+
+    item.category = category;
 
     return this.itemsRepo.save(item);
   }
