ed77c801590dbee4687d55c0b61c3063b70206dc
Ayrton <ayrtoncravero26@gmail.com>
2022-01-18 11:25:59 -0300

    changes

--- src/history/models/create-history.dto.ts
+++ src/history/models/create-history.dto.ts
@@ -1,7 +1,6 @@
-import { OmitType, PartialType } from "@nestjs/swagger";
-import { HistoryDto } from "./history.dto";
+import { OmitType, PartialType } from '@nestjs/swagger';
+import { HistoryDto } from './history.dto';
 
 export class CreateHistoryDto extends PartialType(
   OmitType(HistoryDto, ['id', 'user_id', 'created_at'] as const),
-) {
-}
+) {}
