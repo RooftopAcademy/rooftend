938fad352cf07258a8004291ecdba16383a691bb
agustintosco <agustintosco.92@gmail.com>
2022-01-20 10:09:54 -0600

    added constant bc of refactor

--- src/mail/constants/constants.ts
+++ src/mail/constants/constants.ts
@@ -1,9 +1,10 @@
 import { HexBase64BinaryEncoding, Utf8AsciiBinaryEncoding } from "crypto";
 
-export const CryptoSecurityKey = {
-  algorithm: 'aes-256-cbc',
-  key: Buffer.alloc(32).fill('jaksfn jafkn jafnsafskajlwqjr'),
-  initialVector: Buffer.alloc(16).fill('jaksfn jafkn jafnsafskajlwqjr'),
-  inputEncoding: 'utf8' as Utf8AsciiBinaryEncoding,
-  outputEncoding: 'hex' as HexBase64BinaryEncoding,
+export const cryptoConstants = {
+  ALGORITHM: 'aes-256-cbc',
+  KEY: Buffer.alloc(32).fill('jaksfn jafkn jafnsafskajlwqjr'),
+  INITIAL_VECTOR: Buffer.alloc(16).fill('jaksfn jafkn jafnsafskajlwqjr'),
+  INPUT_ENCODING: 'utf8' as Utf8AsciiBinaryEncoding,
+  OUTPUT_ENCODING: 'hex' as HexBase64BinaryEncoding,
+  VALIDITY_TIME: 1000 * 60 * 60 * 24,
 };
