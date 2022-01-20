b63c23f2fce0e4b445c81d0c98097af4498b86f3
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-20 09:01:15 -0300

    Imported Casl Module

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Question } from './entities/question.entity';
 import { Answer } from './entities/answer.entity';
 import { AnswersService } from './services/answers.service';
+import { CaslModule } from '../auth/casl/casl.module';
 
 @Module({
   providers: [QuestionsService, AnswersService],
   controllers: [QuestionsController],
-  imports: [TypeOrmModule.forFeature([Question, Answer])],
+  imports: [TypeOrmModule.forFeature([Question, Answer]), CaslModule],
 })
-export class QuestionsModule { }
+export class QuestionsModule {}
