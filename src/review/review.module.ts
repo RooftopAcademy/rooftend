bedfa937938cec47b8c2166b3e78ec6213cbb55e
Pablo Canale <pablocanale.exa@gmail.com>
2022-01-22 13:31:11 -0300

    Changed module file

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ReviewController } from './controllers/review.controller';
-import { ReviewRepository } from './repositories/review.repository';
-import { Review } from './review.entity';
-import { ReviewService } from './services/review.service';
+import { ItemReviewsController } from './controllers/itemReviews.controller';
+
+import { ItemReviewsService } from './services/itemReviews.service';
 import { CartModule } from '../cart/cart.module';
+import { UserReviews } from './entities/userReviews.entity';
+import { ItemReviews } from './entities/itemReviews.entity';
+import { UserReviewsService } from './services/userReviews.service';
+import { CaslModule } from '../auth/casl/casl.module';
+import { UserReviewsController } from './controllers/userReviews.controller';
+import { ItemsModule } from '../items/items.module';
+import { ItemsService } from '../items/services/items.service';
+import { Item } from '../items/entities/items.entity';
 
 @Module({
-  controllers: [ReviewController],
-  providers: [ReviewService],
-  imports: [TypeOrmModule.forFeature([Review, ReviewRepository]), CartModule],
+  controllers: [ItemReviewsController, UserReviewsController],
+  providers: [ItemReviewsService, UserReviewsService, ItemsService],
+  imports: [TypeOrmModule.forFeature([UserReviews, ItemReviews, Item]), CartModule, CaslModule, ItemsModule],
 })
-export class ReviewModule {}
+export class ReviewModule { }
