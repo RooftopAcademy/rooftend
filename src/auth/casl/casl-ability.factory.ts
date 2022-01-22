60a4a74819f05feab01f734dc23366405efd6291
Pablo Canale <pablocanale.exa@gmail.com>
2022-01-22 13:16:22 -0300

    Added permissions for Questions module

--- src/auth/casl/casl-ability.factory.ts
+++ src/auth/casl/casl-ability.factory.ts
@@ -10,9 +10,12 @@ import { Permission } from '../enums/permission.enum';
 import { User } from '../../users/entities/user.entity';
 import { Item } from '../../items/entities/items.entity';
 import { FlatClass } from '../types/flat-class.type';
+import { Reviews } from '../../review/entities/reviews';
+import { ItemReviews } from '../../review/entities/itemReviews.entity';
+import { UserReviews } from '../../review/entities/userReviews.entity';
 
 // TODO: add classes to InferSubjects -> InferSubjects<typeof Item | typeof Review ...>
-type Subjects = InferSubjects<typeof Item> | 'all';
+type Subjects = InferSubjects<typeof Item | typeof ItemReviews | typeof UserReviews> | 'all';
 
 export type AppAbility = Ability<[Permission, Subjects]>;
 
@@ -23,7 +26,16 @@ export class CaslAbilityFactory {
       Ability<[Permission, Subjects]>
     >(Ability as AbilityClass<AppAbility>);
 
+    can([Permission.Create, Permission.Read], ItemReviews)
+    can<FlatClass<ItemReviews>>([Permission.Create, Permission.Read], ItemReviews, {
+      'user.id': Number(user.id),
+    })
+    can([Permission.Create, Permission.Read], UserReviews)
+    can<FlatClass<UserReviews>>([Permission.Create, Permission.Read], UserReviews, {
+      'user.id': Number(user.id),
+    })
     // can<FlatClass<[CLASE]>>(Permission[PERMISO], [CLASE], { 'user.id': user.id });
+
     can([Permission.Create, Permission.Read], Item);
     can<FlatClass<Item>>([Permission.Delete, Permission.Update], Item, {
       'user.id': Number(user.id),
