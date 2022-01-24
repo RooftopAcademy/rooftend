91249cf0d2e41b50dfeceebd2fab4ef3ec4785c3
Ayrton <ayrtoncravero26@gmail.com>
2022-01-24 15:33:10 -0300

    change in the test, the deletedes missing

--- src/history/controllers/history.controller.spec.ts
+++ src/history/controllers/history.controller.spec.ts
@@ -1,12 +1,14 @@
-import { ForbiddenException } from '@nestjs/common';
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { response } from 'express';
 import { CaslModule } from '../../auth/casl/casl.module';
+import { Brand } from '../../brands/entities/brands.entity';
+import { Category } from '../../categories/entities/categories.entity';
+import { Item } from '../../items/entities/items.entity';
 import STATUS from '../../statusCodes/statusCodes';
 import { User } from '../../users/entities/user.entity';
 import { HistoryService } from '../services/history.service';
 import { HistoryController } from './history.controller';
-
 
 describe('HistoryController', () => {
   let controller: HistoryController;
@@ -23,20 +25,40 @@ describe('HistoryController', () => {
           ],
         },
       ),
-    delete: jest.fn().mockImplementation(() => STATUS.DELETED),
+    delete: jest.fn(),
     findHistory: jest.fn().mockImplementation(() => {
-      return {
-        id: 1,
-        user_id: 1,
-        createdAt: new Date(),
-      };
+      const user = new User()
+      user.id = 1;
+      return Promise.resolve(user);
     }),
   };
 
+  const mockUser = new User();
+  mockUser.id = 1;
+
   const request: any = {
-    user: new User(),
+    user: mockUser,
   };
   request.user.id = 1;
+
+
+  const mockUser2 = new User();
+  mockUser2.id = 2;
+
+  const request2: any = { user: mockUser2 };
+
+  const mockItem = {
+    id: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    title: 'Name',
+    description: 'Des',
+    price: 1,
+    stock: 1,
+    brand: new Brand(),
+    user: request.user,
+    category: new Category(),
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -84,27 +106,39 @@ describe('HistoryController', () => {
     })
   });
 
-  describe('delete', () => {
+  /* describe('delete', () => {
     const id = 1;
 
     it('should deleted history', async () => {
-      expect(await controller.delete(request, id)).toEqual(true);
+      expect(await controller.delete(request, id)).toEqual(STATUS.DELETED);
 
-      expect.any({ 
+      expect(mockHistoryService.delete).toHaveBeenLastCalledWith(id);
+    });
+  }); */
+  describe('delete', () => {
+    /* it('should remove a History', async () => {
+      expect(await controller.delete(request, 1)).toEqual(STATUS.DELETED);
+
+      expect(mockHistoryService.delete).toHaveBeenCalledWith({
         id: 1,
-        user_id: 1,
-        createdAt: expect.any(Date), 
-      }),0;
+        user: 1,
+        item: expect.any(Item),
+        createdAt: new Date(),
+      });
+    }); */
+
+    it('should return a ForbiddenException', async () => {
+      await expect(controller.delete(request2, 1)).rejects.toThrowError(
+        ForbiddenException,
+      );
+    });
+
+    it('should return a NotFoundException', async () => {
+      mockHistoryService.findHistory.mockRejectedValueOnce(new NotFoundException());
+  
+      await expect(controller.delete(request, 1)).rejects.toThrowError(
+        NotFoundException,
+      );
     });
   });
-
-  /* it('should return a ForbiddenError message', async () => {
-    mockHistoryService.delete.mockImplementationOnce(() => { throw new ForbiddenException() });
-
-    try {
-      expect(await controller.delete(response, id)).toThrow(ForbiddenException);
-    } catch(error) {
-      expect(error.message).toEqual('Forbidden');
-    };
-  }); */
 });
