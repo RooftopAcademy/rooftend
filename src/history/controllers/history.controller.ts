bfaa62feb0a1cf7157ef4da13b1199ebadc3a8be
Ayrton <ayrtoncravero26@gmail.com>
2022-01-24 12:37:07 -0300

    adding documentation

--- src/history/controllers/history.controller.ts
+++ src/history/controllers/history.controller.ts
@@ -10,6 +10,7 @@ import {
   ParseIntPipe,
   Query,
   Req,
+  UnauthorizedException,
 } from '@nestjs/common';
 import {
   ApiBearerAuth,
@@ -27,7 +28,6 @@ import { subject } from '@casl/ability';
 import { User } from '../../users/entities/user.entity';
 import { Request } from 'express';
 import STATUS from '../../statusCodes/statusCodes';
-import { identity } from 'rxjs';
 
 @ApiTags('history')
 @Controller('history')
@@ -42,14 +42,61 @@ export class HistoryController {
     status: 200,
     description: 'A list with all the History',
     type: History,
+    schema: {
+      example: {
+        history: [
+          {
+            id: '1',
+            user: {
+              id: '1',
+              username: 'kdougharty1r',
+              email: 'modriscole1r@cnn.com',
+              account_status: 2,
+              deletedAt: null,
+              completed: false,
+            },
+            item: {
+              id: '1',
+              createdAt: '2022-01-15T21:56:42.157Z',
+              updatedAt: '2022-01-15T21:56:42.157Z',
+              title: 'GherkinSour',
+              description: 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.',
+              price: 4271.85,
+              stock: 1176,
+              brand: {
+                id: '66',
+                name: 'functionalities',
+                photoUrl: 'http://dummyimage.com/100x100.png/cc0000/ffffff',
+              },
+              user: {
+                id: '64',
+                username: 'kdougharty1r',
+                email: 'modriscole1r@cnn.com',
+                account_status: 2,
+                deletedAt: null,
+                completed: false,
+              },
+              category: {
+                id: 65,
+                name: 'Clothing',
+              },
+            }
+          }
+        ]
+      }
+    }
   })
-  //Agregar la docu del schema
   @ApiForbiddenResponse({
     description: 'Forbidden.',
-    //Agregar los ejemplos
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
   })
   @ApiUnauthorizedResponse({
     description: 'Not Authorized',
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
   })
   @ApiBearerAuth()
   @Get()
@@ -76,12 +123,21 @@ export class HistoryController {
   @ApiResponse({
     status: 200,
     description: 'The history has been removed successfully.',
+    schema: {
+      example: STATUS.DELETED,
+    },
   })
   @ApiForbiddenResponse({
     description: 'Forbidden.',
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
   })
   @ApiUnauthorizedResponse({
     description: 'Not Authorized',
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
   })
   @ApiBearerAuth()
   @HttpCode(200)
