9f2f6574e7c9f39911cb2bc278648b1fa9035d85
Ayrton <ayrtoncravero26@gmail.com>
2022-01-25 14:18:43 -0300

    change dto

--- src/history/models/create-history.dto.ts
+++ src/history/models/create-history.dto.ts
@@ -1,6 +1,11 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
-import { HistoryDto } from './history.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber } from "class-validator";
 
-export class CreateHistoryDto extends PartialType(
-  OmitType(HistoryDto, [] as const),
-) {}
+export class CreateHistoryDto {
+  @IsNumber()
+  @ApiProperty({
+    description: 'Unique identifier of the item',
+    example: '1',
+  })
+  itemId: number
+}
