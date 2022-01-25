c8359029a2f374459c63990bc2070e5fc30ac445
Ayrton <ayrtoncravero26@gmail.com>
2022-01-24 21:32:17 -0300

    deleted information unnecesary

--- src/history/models/create-history.dto.ts
+++ src/history/models/create-history.dto.ts
@@ -2,5 +2,5 @@ import { OmitType, PartialType } from '@nestjs/swagger';
 import { HistoryDto } from './history.dto';
 
 export class CreateHistoryDto extends PartialType(
-  OmitType(HistoryDto, ['id', 'user_id', 'created_at'] as const),
+  OmitType(HistoryDto, [] as const),
 ) {}
