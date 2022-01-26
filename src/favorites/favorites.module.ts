fb086357520be420019475a3280b565ec230b8c7
Ayrton <ayrtoncravero26@gmail.com>
2022-01-26 09:50:22 -0300

    add import of caslModule

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -4,9 +4,10 @@ import { FavoritesController } from './controllers/favorites.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Favorite } from './entities/favorite.entity';
+import { CaslModule } from '../auth/casl/casl.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Favorite])],
+  imports: [TypeOrmModule.forFeature([Favorite]), CaslModule],
   providers: [FavoritesService],
   controllers: [FavoritesController],
 })
