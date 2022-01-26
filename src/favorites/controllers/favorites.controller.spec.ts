0d5a9b8c2f72b18dfc889f81e7b1ec4e1f339de4
Ayrton <ayrtoncravero26@gmail.com>
2022-01-26 09:55:49 -0300

    completed tests

--- src/favorites/controllers/favorites.controller.spec.ts
+++ src/favorites/controllers/favorites.controller.spec.ts
@@ -20,15 +20,9 @@ describe('FavoritesController', () => {
         createdAt: new Date(),
       },
     ]),
-    /* create: jest.fn(dto => {
-      return {
-        ...dto,
-        user_id: 24
-      }
-    }), */
     create: jest.fn((user: User, body: CreateFavoriteDto) => {
       Promise.resolve({
-        item_id: body.item_id,
+        item_id: 1,
       });
     }),
     delete: jest.fn(),
@@ -57,35 +51,26 @@ describe('FavoritesController', () => {
   });
 
   describe('paginate', () => {
-    it('should get 10 favorites records.', () => {
+    it('should get 10 favorites records.', async () => {
       const page = 1;
       const limit = 10;
+      const route = '/favorites';
   
-      expect(controller.paginate(response, page, limit)).not.toBeUndefined();
+      await expect(controller.paginate(response, page, limit)).resolves.toEqual([
+        {
+          id: 1,
+          user: 1,
+          item_id: 1,
+          createdAt: expect.any(Date),
+        },
+      ]);
 
-      //expect(mockFavoriteService.paginate).toHaveBeenCalled(); -> Va este test?
-    });
-
-    it('should return a ForbiddenError message', async () => {
-      mockFavoriteService.paginate.mockImplementationOnce(() => {
-        throw new ForbiddenException();
-      });
-
-      try {
-        expect(await controller.paginate(response, 1)).toThrow(ForbiddenException);
-      } catch (error) {
-        expect(error.message).toEqual('Forbidden');
-      };
+      expect(mockFavoriteService.paginate).toHaveBeenCalledWith({page, limit, route}, response.user);
     });
   });
 
   describe('create', () => {
     it('should create a favorite.', async () => {
-
-      const dto: CreateFavoriteDto = {
-        item_id: 1,
-      };
-
       const mockUser = new User();
       mockUser.id = 1;
 
@@ -93,14 +78,16 @@ describe('FavoritesController', () => {
         user: mockUser,
       };
       request.user.id = 1;
+      
+      const dto: CreateFavoriteDto = {
+        itemId: 1,
+      };
 
-      expect(await controller.create(request, dto)).toEqual({
-        item_id: dto.item_id,
-      });
+      expect(await controller.create(request, dto)).toEqual(
+        STATUS.CREATED
+      );
   
-      //VIEJO -> expect(mockFavoriteService.create).toHaveBeenCalledWith(data, token)
-
-      expect(mockFavoriteService.create).toHaveBeenCalledWith(request.user, dto);
+      expect(mockFavoriteService.create).toHaveBeenCalledWith(dto, request.user);
     });
 
     it('should return a ForbiddenError message', async () => {
