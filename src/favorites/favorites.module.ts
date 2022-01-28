06e481d1bf0ab04470d9cfbdad5ba9987fc98ea9
Ayrton <ayrtoncravero26@gmail.com>
2022-01-28 11:18:19 -0300

    import 'Item' module

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Favorite } from './entities/favorite.entity';
 import { CaslModule } from '../auth/casl/casl.module';
+import { Item } from '../items/entities/items.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Favorite]), CaslModule],
+  imports: [TypeOrmModule.forFeature([Favorite, Item]), CaslModule],
   providers: [FavoritesService],
   controllers: [FavoritesController],
 })
