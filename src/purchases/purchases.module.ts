b534ab08b84bcc82157c08e0ffc475815ce22533
Ayrton <ayrtoncravero26@gmail.com>
2022-01-29 10:13:24 -0300

    import caslModule

--- src/purchases/purchases.module.ts
+++ src/purchases/purchases.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CaslModule } from '../auth/casl/casl.module';
 import { Cart } from '../cart/entities/cart.entity';
 import { Delivery } from '../deliveries/entities/delivery.entity';
 import { PurchasesController } from './controllers/purchases.controller';
 import { PurchasesService } from './services/purchases.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cart, Delivery])],
+  imports: [TypeOrmModule.forFeature([Cart, Delivery]), CaslModule],
   providers: [PurchasesService],
   controllers: [PurchasesController],
 })
