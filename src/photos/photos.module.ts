2a7a71293f05578b3a6f5d6f04c2d4c302cf6124
Ayrton <ayrtoncravero26@gmail.com>
2022-01-30 17:25:08 -0300

    add dependecy

--- src/photos/photos.module.ts
+++ src/photos/photos.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PhotosController } from './controllers/photos.controller';
-import { PhotosEntity } from './models/photos.entity';
+import { Photos } from './models/photos.entity';
+import { CaslModule } from '../auth/casl/casl.module';
 import { PhotosService } from './services/photos.service';
-import { PhotosRepository } from './repositories/photos.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PhotosEntity, PhotosRepository])],
   controllers: [PhotosController],
   providers: [PhotosService],
+  imports: [TypeOrmModule.forFeature([Photos]), CaslModule],
 })
 export class PhotosModule {}
