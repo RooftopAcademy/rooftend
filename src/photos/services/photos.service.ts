07af1ee9b84a6d0e9e5d5f0050e780a89cbe5ee6
Ayrton <ayrtoncravero26@gmail.com>
2022-01-30 15:46:21 -0300

    add method 'findOne'

--- src/photos/services/photos.service.ts
+++ src/photos/services/photos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeleteResult, UpdateResult } from 'typeorm';
-import { PhotosEntity } from '../models/photos.entity';
+import { Photos } from '../models/photos.entity';
 import { PhotosInterface } from '../models/photos.interface';
 import { Observable, from } from 'rxjs';
 import {
@@ -14,25 +14,33 @@ import { PhotosRepository } from '../repositories/photos.repository';
 @Injectable()
 export class PhotosService {
   constructor(
-    @InjectRepository(PhotosEntity)
+    @InjectRepository(Photos)
     private readonly photosRepository: PhotosRepository,
   ) {}
 
   async paginate(
     options: IPaginationOptions,
-  ): Promise<Pagination<PhotosEntity>> {
-    return paginate<PhotosEntity>(this.photosRepository, options);
+  ): Promise<Pagination<Photos>> {
+    return paginate<Photos>(this.photosRepository, options);
   }
 
   create(photo: PhotosInterface): Observable<PhotosInterface> {
     return from(this.photosRepository.save(photo));
   }
 
-  findAll(): Observable<PhotosEntity[]> {
+  findAll(): Observable<Photos[]> {
     return from(this.photosRepository.find());
   }
 
-  delete(id: number): Observable<DeleteResult> {
-    return from(this.photosRepository.delete(id));
+  async findOne(id: number) {
+    const photo = await this.photosRepository.findOne(id, {
+      relations: ['user'],
+    });
+
+    return photo;
+  }
+
+  delete(photo: Photos): Observable<DeleteResult> {
+    return from(this.photosRepository.delete(photo));
   }
 }
