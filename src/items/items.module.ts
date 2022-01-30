d4e40b6fe1dc021e6e3ff5cc08488cea5dca6192
gbangertes <gbangertes@gmail.com>
2022-01-30 18:44:29 -0300

    Added services imports.

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CaslModule } from '../auth/casl/casl.module';
 import { Brand } from '../brands/entities/brands.entity';
+import { BrandsService } from '../brands/services/brands.serveces';
 import { Category } from '../categories/entities/categories.entity';
+import { CategoriesService } from '../categories/services/categories.service';
 import { ItemsController } from './controllers/items.controller';
 import { Item } from './entities/items.entity';
 import { ItemsService } from './services/items.service';
@@ -10,6 +12,11 @@ import { ItemsService } from './services/items.service';
 @Module({
   controllers: [ItemsController],
   providers: [ItemsService],
-  imports: [TypeOrmModule.forFeature([Item, Category, Brand]), CaslModule],
+  imports: [TypeOrmModule.forFeature(
+    [Item, Category, Brand]),
+    CaslModule,
+    CategoriesService,
+    BrandsService,
+  ],
 })
 export class ItemsModule {}
