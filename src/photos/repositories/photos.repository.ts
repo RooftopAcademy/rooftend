475f727c73e9ebf3517e68491c1c209d8a00fe10
Ayrton <ayrtoncravero26@gmail.com>
2022-01-30 17:25:45 -0300

    change imports

--- src/photos/repositories/photos.repository.ts
+++ src/photos/repositories/photos.repository.ts
@@ -1,6 +1,6 @@
 import { EntityRepository } from 'typeorm';
 import { AbstractPolymorphicRepository } from 'typeorm-polymorphic';
-import { PhotosEntity } from '../models/photos.entity';
+import { Photos } from '../models/photos.entity';
 
-@EntityRepository(PhotosEntity)
-export class PhotosRepository extends AbstractPolymorphicRepository<PhotosEntity> {}
+@EntityRepository(Photos)
+export class PhotosRepository extends AbstractPolymorphicRepository<Photos> {}
