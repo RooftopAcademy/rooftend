8e822cda55b6acb934d30a5a82c990b54a7d3719
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-31 09:53:33 -0300

    Added authorization in Delete endopoint

--- src/saved-items/controllers/saved-items.controller.ts
+++ src/saved-items/controllers/saved-items.controller.ts
@@ -2,99 +2,126 @@ import {
   Body,
   Controller,
   Delete,
+  ForbiddenException,
   Get,
+  HttpCode,
   HttpStatus,
+  NotFoundException,
   Param,
   Post,
+  Req,
   Res,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { CreateSavedItemDto } from '../dto/CreateSavedItemDto';
 import { SavedItemsService } from '../services/saved-items.service';
 import {
+  ApiBearerAuth,
   ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
   ApiOperation,
   ApiResponse,
   ApiTags,
+  ApiUnauthorizedResponse,
   getSchemaPath,
 } from '@nestjs/swagger';
+import STATUS from '../../statusCodes/statusCodes';
+import { CaslAbilityFactory } from '../../auth/casl/casl-ability.factory';
+import Status from '../../statusCodes/status.interface';
+import { User } from '../../users/entities/user.entity';
+import { subject } from '@casl/ability';
+import { Permission } from '../../auth/enums/permission.enum';
 
 @ApiTags('Saved')
+// @ApiBearerAuth()
 @Controller('saved')
 export class SavedItemsController {
-  constructor(private readonly savedItemsService: SavedItemsService) {}
+  constructor(
+    private readonly caslAbilityFactory: CaslAbilityFactory,
+    private readonly savedItemsService: SavedItemsService,
+  ) {}
 
-  @Get()
-  @ApiOperation({ summary: 'Get all saved items' })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'Returns all saved items',
-    type: [CreateSavedItemDto],
-  })
-  async findAll(
-    @Res({ passthrough: true }) res: Response,
-  ): Promise<CreateSavedItemDto[]> {
-    const rawData = await this.savedItemsService.getAllSavedItems();
-    res.status(HttpStatus.OK);
-    const data: CreateSavedItemDto[] = rawData.map((item) => {
-      return {
-        itemId: item.itemId,
-        userId: item.userId,
-        quantity: item.quantity,
-        price: item.price,
-      };
-    });
-    return data;
-  }
+  // @Get()
+  // @ApiOperation({ summary: 'Get all saved items' })
+  // @ApiResponse({
+  //   status: 200,
+  //   description: 'Returns all saved items',
+  //   type: [CreateSavedItemDto],
+  //   schema: {
+  //     example: {
+  //       items: [{}],
+  //     },
+  //   },
+  // })
+  // async findAll(
+  //   @Res({ passthrough: true }) res: Response,
+  // ): Promise<CreateSavedItemDto[]> {
+  //   const rawData = await this.savedItemsService.getAllSavedItems();
+  //   res.status(HttpStatus.OK);
+  //   const data: CreateSavedItemDto[] = rawData.map((item) => {
+  //     return {
+  //       // itemId: item.itemId,
+  //       // userId: item.userId,
+  //       quantity: item.quantity,
+  //       price: item.price,
+  //     };
+  //   });
+  //   return data;
+  // }
 
-  @Post()
-  @ApiOperation({ summary: 'Add a new saved item' })
-  @ApiCreatedResponse({
-    description: 'The saved item has been successfully created.',
-    schema: {
-      example: {
-        message: 'Successfully created',
-        item: { userId: 1, quantity: 1, price: 100 },
-      },
-      properties: {
-        message: {
-          type: 'String',
-          description: 'message for successful creation',
-          example: 'Successfully created',
-        },
-        item: { $ref: getSchemaPath(CreateSavedItemDto) },
-      },
-    },
-  })
-  async create(
-    @Res({ passthrough: true }) res: Response,
-    @Body() createSavedItemDto: CreateSavedItemDto,
-  ) {
-    const rawSavedItem = await this.savedItemsService.createSavedItem(
-      createSavedItemDto,
-    );
-    res.status(HttpStatus.CREATED);
-    const savedItem: CreateSavedItemDto = {
-      itemId: rawSavedItem.itemId,
-      userId: rawSavedItem.userId,
-      quantity: rawSavedItem.quantity,
-      price: rawSavedItem.price,
-    };
-    return {
-      message: 'Successfully created',
-      item: savedItem,
-    };
-  }
+  // @Post()
+  // @ApiOperation({ summary: 'Add a new saved item' })
+  // @ApiCreatedResponse({
+  //   description: 'The saved item has been successfully created.',
+  //   schema: {
+  //     example: {
+  //       message: 'Successfully created',
+  //       // item: { userId: 1, quantity: 1, price: 100 },
+  //     },
+  //     properties: {
+  //       message: {
+  //         type: 'String',
+  //         description: 'message for successful creation',
+  //         example: 'Successfully created',
+  //       },
+  //       item: { $ref: getSchemaPath(CreateSavedItemDto) },
+  //     },
+  //   },
+  // })
+  // async create(
+  //   @Res({ passthrough: true }) res: Response,
+  //   @Body() createSavedItemDto: CreateSavedItemDto,
+  // ) {
+  //   const rawSavedItem = await this.savedItemsService.createSavedItem(
+  //     createSavedItemDto,
+  //   );
+  //   res.status(HttpStatus.CREATED);
+  //   const savedItem: CreateSavedItemDto = {
+  //     itemId: rawSavedItem.itemId,
+  //     userId: rawSavedItem.userId,
+  //     quantity: rawSavedItem.quantity,
+  //     price: rawSavedItem.price,
+  //   };
+  //   return {
+  //     message: 'Successfully created',
+  //     item: savedItem,
+  //   };
+  // }
 
   @Delete(':id')
-  @ApiOperation({ summary: 'Delete a saved item' })
+  @HttpCode(200)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete a saved item by ID' })
   @ApiResponse({
-    status: HttpStatus.OK,
+    status: 200,
     description: 'The saved item has been successfully deleted.',
     schema: {
-      example: {
-        message: 'item with id 5 deleted successfully',
-      },
+      example: STATUS.deleted,
+      // {
+      //   message: 'item with id 5 deleted successfully',
+      // },
       properties: {
         message: {
           type: 'String',
@@ -104,17 +131,49 @@ export class SavedItemsController {
       },
     },
   })
-  async remove(@Res({ passthrough: true }) res: Response, @Param() params) {
-    try {
-      const response = await this.savedItemsService.removeSavedItem(params.id);
-      if (response.affected == 0) {
-        throw new Error('Error: item with id ' + params.id + ' not found');
-      }
-      res.status(HttpStatus.OK);
-      return `Item with id ${params.id} deleted successfully`;
-    } catch (err) {
-      res.status(HttpStatus.NOT_FOUND);
-      return err.message;
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
+    description: 'User is not logged in',
+  })
+  @ApiForbiddenResponse({
+    description: 'User is not authorized to delete this saved item',
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Saved Item Not Found',
+    schema: {
+      example: new NotFoundException('Item not found').getResponse(),
+    },
+  })
+  // async remove(@Res({ passthrough: true }) res: Response, @Param() params) {
+  //   try {
+  //     const response = await this.savedItemsService.removeSavedItem(params.id);
+  //     if (response.affected == 0) {
+  //       throw new Error('Error: item with id ' + params.id + ' not found');
+  //     }
+  //     res.status(HttpStatus.OK);
+  //     return `Item with id ${params.id} deleted successfully`;
+  //   } catch (err) {
+  //     res.status(HttpStatus.NOT_FOUND);
+  //     return err.message;
+  //   }
+  // }
+  async remove(@Req() req:Request, @Param('id') id:number):Promise<Status> {
+    const user: User = <User>req.user;
+
+    const savedItem = await this.savedItemsService.findOneSavedItem(id);
+    const ability = this.caslAbilityFactory.createForUser(user);
+
+    if (ability.cannot(Permission.Delete, subject('SavedItem', savedItem))) {
+      throw new ForbiddenException();
     }
+
+    await this.savedItemsService.removeSavedItem(savedItem);
+
+   return STATUS.deleted;
   }
 }
