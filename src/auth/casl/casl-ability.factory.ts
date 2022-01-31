fdd3da4173291d7e8d000c5482ab50d0335753e0
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-31 09:47:54 -0300

    Added Delete Permission in Casl Ability Factory

--- src/auth/casl/casl-ability.factory.ts
+++ src/auth/casl/casl-ability.factory.ts
@@ -10,9 +10,10 @@ import { Permission } from '../enums/permission.enum';
 import { User } from '../../users/entities/user.entity';
 import { Item } from '../../items/entities/items.entity';
 import { FlatClass } from '../types/flat-class.type';
+import { SavedItem } from '../../saved-items/entities/SavedItems.entity';
 
 // TODO: add classes to InferSubjects -> InferSubjects<typeof Item | typeof Review ...>
-type Subjects = InferSubjects<typeof Item> | 'all';
+type Subjects = InferSubjects<typeof Item | typeof SavedItem> | 'all';
 
 export type AppAbility = Ability<[Permission, Subjects]>;
 
@@ -25,10 +26,13 @@ export class CaslAbilityFactory {
 
     // can<FlatClass<[CLASE]>>(Permission[PERMISO], [CLASE], { 'user.id': user.id });
     can([Permission.Create, Permission.Read], Item);
+    can([Permission.Read], SavedItem);
     can<FlatClass<Item>>([Permission.Delete, Permission.Update], Item, {
       'user.id': user.id,
     });
-
+    can<FlatClass<SavedItem>>([Permission.Delete], SavedItem, {
+      'user.id': user.id,
+    });
     return build({
       // Read https://casl.js.org/v5/en/guide/subject-type-detection#use-classes-as-subject-types for details
       detectSubjectType: (item) =>
