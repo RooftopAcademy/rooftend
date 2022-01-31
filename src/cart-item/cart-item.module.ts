beb0e214bf75965ce82f0014aba5484ad32a9af0
ghnoob <ptnchk.rodrigo@gmail.com>
2022-01-31 10:37:22 -0300

    fix module imports

--- src/cart-item/cart-item.module.ts
+++ src/cart-item/cart-item.module.ts
@@ -3,11 +3,18 @@ import { CartItemService } from './services/cart-item.service';
 import { CartItemController } from './controllers/cart-item.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartItem } from './entities/cart-item.entity';
-import { Item } from '../items/entities/items.entity';
+import { CartModule } from '../cart/cart.module';
+import { CaslModule } from '../auth/casl/casl.module';
+import { ItemsModule } from '../items/items.module';
 
 @Module({
   providers: [CartItemService],
   controllers: [CartItemController],
-  imports: [TypeOrmModule.forFeature([CartItem, Item])],
+  imports: [
+    TypeOrmModule.forFeature([CartItem]),
+    CartModule,
+    CaslModule,
+    ItemsModule,
+  ],
 })
 export class CartItemModule {}
