de3cae2c4f5e7b978788c661495b439899d79d1d
Pablo Canale <pablocanale.exa@gmail.com>
2022-01-31 12:49:11 -0300

    Created create method for userReviews

--- src/review/controllers/userReviews.controller.ts
+++ src/review/controllers/userReviews.controller.ts
@@ -1,7 +1,14 @@
-import { ClassSerializerInterceptor, Controller, UseInterceptors } from "@nestjs/common";
+import { subject } from "@casl/ability";
+import { Body, ClassSerializerInterceptor, Controller, ForbiddenException, HttpCode, Param, Post, Req, UseInterceptors } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { CaslAbilityFactory } from "../../auth/casl/casl-ability.factory";
+import { Permission } from "../../auth/enums/permission.enum";
+import { User } from "../../users/entities/user.entity";
+import { ItemReviews } from "../entities/itemReviews.entity";
+import userReviewDTO from "../entities/userReview.create.dto";
 import { UserReviewsService } from "../services/userReviews.service";
+import { CartService } from "../../cart/services/cart.service";
+import { UserReviews } from "../entities/userReviews.entity";
 
 @ApiTags('User Reviews')
 @Controller('reviews')
@@ -10,5 +17,28 @@ export class UserReviewsController {
     constructor(
         private readonly userReviewsService: UserReviewsService,
         private readonly caslAbilityFactory: CaslAbilityFactory,
+        private cartService: CartService
     ) { }
+
+
+    @Post('profile/:userId')
+    @HttpCode(201)
+    async create(
+        @Req() req,
+        @Body() reviewDTO: userReviewDTO,
+        @Param() cartId: number,
+
+    ) {
+        const user: User = <User>req.user;
+        const ability = this.caslAbilityFactory.createForUser(user);
+
+        if (ability.cannot(Permission.Create, subject('UserReviews', UserReviews))) {
+            throw new ForbiddenException();
+        }
+        const purchase = await this.cartService.findOneFromUser(cartId, user);
+        if (!purchase) throw new ForbiddenException();
+
+        return this.userReviewsService.create(reviewDTO, user);
+    }
+
 }
