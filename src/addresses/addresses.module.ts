87b6476c01021bd7dbe08942ac6b7a9d87941f8e
Ayrton <ayrtoncravero26@gmail.com>
2022-01-31 10:20:31 -0300

    import 'caslModule'

--- src/addresses/addresses.module.ts
+++ src/addresses/addresses.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CaslModule } from '../auth/casl/casl.module';
 import { AddressesController } from './controllers/addresses.controller';
 import { Address } from './entities/address.entity';
 import { AddressesService } from './services/addresses.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Address])],
+  imports: [TypeOrmModule.forFeature([Address]), CaslModule],
   providers: [AddressesService],
   controllers: [AddressesController],
 })
