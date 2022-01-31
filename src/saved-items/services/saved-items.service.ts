d1e2001d2af05e03b6cf857298097bae296ee6f6
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-31 09:55:03 -0300

    Updated removeSavedItem method in service

--- src/saved-items/services/saved-items.service.ts
+++ src/saved-items/services/saved-items.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { CreateSavedItemDto } from '../dto/CreateSavedItemDto';
-import { SavedItemsEntity } from '../entities/SavedItems.entity';
+import { SavedItem } from '../entities/SavedItems.entity';
 
 @Injectable()
 export class SavedItemsService {
   constructor(
-    @InjectRepository(SavedItemsEntity)
-    private savedItemsRepo: Repository<SavedItemsEntity>,
+    @InjectRepository(SavedItem)
+    private savedItemsRepo: Repository<SavedItem>,
   ) {}
 
   getAllSavedItems() {
     return this.savedItemsRepo.find();
   }
-
+  findOneSavedItem(id: number) {
+    return this.savedItemsRepo.findOne(id);
+  }
   createSavedItem(createSavedItemDto: CreateSavedItemDto) {
     const newSavedItem = this.savedItemsRepo.create(createSavedItemDto);
     return this.savedItemsRepo.save(newSavedItem);
   }
 
-  removeSavedItem(id: number) {
-    return this.savedItemsRepo.delete(id);
+  removeSavedItem(savedItem: SavedItem): Promise<DeleteResult> {
+    return this.savedItemsRepo.delete(savedItem);
   }
 }
