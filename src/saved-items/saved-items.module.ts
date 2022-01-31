570b309326afc0e8ec87f7a60693c798fd5a2b10
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-01-31 09:52:19 -0300

    Imported Casl module in saved items module

--- src/saved-items/saved-items.module.ts
+++ src/saved-items/saved-items.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SavedItemsService } from './services/saved-items.service';
 import { SavedItemsController } from './controllers/saved-items.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SavedItemsEntity } from './entities/SavedItems.entity';
+import { SavedItem } from './entities/SavedItems.entity';
+import { CaslModule } from '../auth/casl/casl.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SavedItemsEntity])],
+  imports: [TypeOrmModule.forFeature([SavedItem]), CaslModule],
   providers: [SavedItemsService],
   controllers: [SavedItemsController],
 })
