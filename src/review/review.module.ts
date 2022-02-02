6f8d251c55b9774177255ecedad47f6373ab7472
Pablo Canale <pablocanale.exa@gmail.com>
2022-02-02 16:26:23 -0300

    Fixed imports in module

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -10,17 +10,15 @@ import { UserReviewsService } from './services/userReviews.service';
 import { CaslModule } from '../auth/casl/casl.module';
 import { UserReviewsController } from './controllers/userReviews.controller';
 import { ItemsModule } from '../items/items.module';
-import { ItemsService } from '../items/services/items.service';
-import { Item } from '../items/entities/items.entity';
-import { Category } from '../categories/entities/categories.entity';
-import { Brand } from '../brands/entities/brands.entity';
-import { UserService } from '../../dist/users/services/user.service';
-import { UsersModule } from '../../dist/users/users.module';
-import { User } from '../users/entities/user.entity';
+
+import { UsersModule } from '../users/users.module';
+import { Likes } from './entities/likes.entity';
+import { LikesService } from './services/likes.service';
+
 
 @Module({
   controllers: [ItemReviewsController, UserReviewsController],
-  providers: [ItemReviewsService, UserReviewsService, ItemsService, UserService],
-  imports: [TypeOrmModule.forFeature([UserReviews, ItemReviews, Item, Category, Brand, User]), CartModule, CaslModule, ItemsModule, UsersModule],
+  providers: [ItemReviewsService, UserReviewsService, LikesService],
+  imports: [TypeOrmModule.forFeature([UserReviews, ItemReviews, Likes]), CartModule, CaslModule, ItemsModule, UsersModule],
 })
 export class ReviewModule { }
