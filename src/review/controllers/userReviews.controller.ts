6dce18dc3eca88c9d46c315eab3351914428cf18
Pablo Canale <pablocanale.exa@gmail.com>
2022-02-03 15:41:41 -0300

    Created method for finding reviews of user

--- src/review/controllers/userReviews.controller.ts
+++ src/review/controllers/userReviews.controller.ts
@@ -1,6 +1,6 @@
 import { subject } from "@casl/ability";
-import { Body, ClassSerializerInterceptor, Controller, ForbiddenException, HttpCode, Param, Post, Req, UseInterceptors } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Body, ClassSerializerInterceptor, Controller, DefaultValuePipe, ForbiddenException, Get, HttpCode, Param, ParseIntPipe, Post, Query, Req, UseInterceptors } from "@nestjs/common";
+import { ApiOperation, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { CaslAbilityFactory } from "../../auth/casl/casl-ability.factory";
 import { Permission } from "../../auth/enums/permission.enum";
 import { User } from "../../users/entities/user.entity";
@@ -10,6 +10,9 @@ import { CartService } from "../../cart/services/cart.service";
 import { UserReviews } from "../entities/userReviews.entity";
 import { CartItemService } from "../../cart-item/services/cart-item.service";
 import { ItemsService } from "../../items/services/items.service";
+import { Pagination } from "nestjs-typeorm-paginate";
+import { opinionsEnum } from "../enum/opinions.enum";
+import { Public } from "../../authentication/decorators/public.decorator";
 @ApiTags('User Reviews')
 @Controller('reviews')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -23,6 +26,54 @@ export class UserReviewsController {
 
     ) { }
 
+    @Get('user/:id')
+    @Public()
+    @HttpCode(200)
+    @ApiOperation({ summary: 'List reviews of seller' })
+    @ApiQuery({
+        name: 'filter',
+        type: String,
+        required: false,
+        description: 'filter positives or negavatives reviews',
+        example: 'REGULAR',
+    })
+    @ApiQuery({
+        name: 'page',
+        type: Number,
+        required: false,
+        description: 'Current page number. Defaults to 1.',
+        example: 1,
+    })
+    @ApiParam({
+        name: 'id',
+        type: Number,
+        required: true,
+        description: 'Id of reviewed user',
+        example: 1,
+    })
+    @ApiQuery({
+        name: 'limit',
+        type: Number,
+        required: false,
+        description: 'Maximum results per page. Max value: 100. Defaults to 10.',
+        example: 10,
+    })
+    async findAll(
+        @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+        @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+        @Param('id') id: number,
+        @Query('filter') filter: opinionsEnum,
+    ): Promise<Pagination<UserReviews>> {
+        limit = limit > 100 ? 100 : limit;
+        return this.userReviewsService.paginate(
+            {
+                page,
+                limit,
+                route: '/reviews',
+            },
+            id,
+            filter);
+    }
 
     @Post('/purchase/:carId/user/:reviewedId')
     @HttpCode(201)
