8af0b3bd3c42791b12c750efb8a538c7992b4d34
Pablo Canale <pablocanale.exa@gmail.com>
2022-02-08 16:01:26 -0300

    Added body in likeReview

--- src/review/controllers/likes.controller.ts
+++ src/review/controllers/likes.controller.ts
@@ -1,13 +1,13 @@
 import { subject } from "@casl/ability";
-import { ClassSerializerInterceptor, Controller, ForbiddenException, HttpCode, Param, Post, Req, UseInterceptors } from "@nestjs/common";
+import { Body, ClassSerializerInterceptor, Controller, ForbiddenException, HttpCode, Param, Post, Req, UseInterceptors } from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { Request } from "express";
 import { CaslAbilityFactory } from "../../auth/casl/casl-ability.factory";
 import { Permission } from "../../auth/enums/permission.enum";
-import { Public } from "../../authentication/decorators/public.decorator";
 import { User } from "../../users/entities/user.entity";
 import { Likes } from "../entities/likes.entity";
 import { LikesService } from "../services/likes.service";
+import LikeDTO from "../DTOs/like.dto";
 @ApiTags('Likes')
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('reviews')
@@ -18,11 +18,11 @@ export class LikesController {
     ) { }
 
 
-    @Post('/:id/:liked')
+    @Post('/:id')
     @HttpCode(201)
     @ApiOperation({ summary: 'Like a item review' })
     async likeReview(
-        @Param('liked') liked: boolean,
+        @Body() likeDTO: LikeDTO,
         @Param('id') reviewId: number,
         @Req() req: Request) {
 
@@ -32,6 +32,6 @@ export class LikesController {
         if (ability.cannot(Permission.Create, subject('Likes', Likes))) {
             throw new ForbiddenException();
         }
-        return this.likesService.react(liked, user, reviewId)
+        return this.likesService.react(likeDTO.liked, user, reviewId)
     }
 }
